d5a70a5baa17ac854a0ff0f6997c43f16121beaa
janghyunjun <buzz03312@naver.com>
2025-05-27 21:34:07 +0900

    [chore] 스프링 초기 세팅

---
+++ src/main/java/konkuk/thip/ThipServerApplication.java
@@ -0,0 +1,13 @@
+package konkuk.thip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThipServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konkuk/thip/ThipServerApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.thip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThipServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
