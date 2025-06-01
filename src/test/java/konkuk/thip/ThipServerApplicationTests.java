b102b32ddef3122d74202eb555377950add2359f
seongjunnoh <joon1754@naver.com>
2025-06-01 21:22:53 +0900

    refactor : test yml 사용 명시

--- src/test/java/konkuk/thip/ThipServerApplicationTests.java
+++ src/test/java/konkuk/thip/ThipServerApplicationTests.java
@@ -2,8 +2,10 @@ package konkuk.thip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ThipServerApplicationTests {
 
 	@Test
