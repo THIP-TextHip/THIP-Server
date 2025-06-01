52b5d90773b5aefc2c7ae9cd26f9d3fead57c39e
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:43:51 +0900

    [feat] Category 엔티티 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/Category.java
@@ -0,0 +1,33 @@
+package konkuk.thip.entity;
+
+import jakarta.persistence.*;
+import konkuk.thip.global.entity.BaseEntity;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "alias_id", nullable = false)
+    private Alias alias;
+
+    @Column(length = 50, nullable = false)
+    private String value;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tag> tags = new ArrayList<>();
+}
