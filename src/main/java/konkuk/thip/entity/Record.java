b10935afb0ca72dc9723d6535acfa85dbad09558
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:45:38 +0900

    [feat] Record 엔티티 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/Record.java
@@ -0,0 +1,33 @@
+package konkuk.thip.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "records")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Record {
+
+    @Id
+    @Column(name = "post_id")
+    private Long postId;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    private Integer page;
+
+    @Column(name = "is_overview",nullable = false)
+    private boolean isOverview;
+}
+
