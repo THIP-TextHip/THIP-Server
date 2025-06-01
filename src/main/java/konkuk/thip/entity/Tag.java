f6b449a86fb2e56623f9e153b1c8105e84609a8c
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:46:46 +0900

    [feat] Tag 엔티티 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/Tag.java
@@ -0,0 +1,30 @@
+package konkuk.thip.entity;
+
+import jakarta.persistence.*;
+import konkuk.thip.global.entity.BaseEntity;
+import lombok.*;
+
+@Entity
+@Table(name = "tag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Tag extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long tagId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(length = 50, nullable = false)
+    private String value;
+}
