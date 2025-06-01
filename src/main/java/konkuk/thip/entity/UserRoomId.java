395446ec6fc97c0da3daa1650e685cb0e4164149
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:47:38 +0900

    [feat] UserRoom 엔티티 복합키 UserRoomId 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/UserRoomId.java
@@ -0,0 +1,17 @@
+
+package konkuk.thip.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Embeddable
+@Getter
+public class UserRoomId implements Serializable {
+    private Long userId;
+    private Long roomId;
+}
