4974bf0c2143a8bc248ffbb49a69fcad21acbc89
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:40:07 +0900

    [feat] Alias 엔티티 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/Alias.java
@@ -0,0 +1,34 @@
+package konkuk.thip.entity;
+
+import jakarta.persistence.*;
+import konkuk.thip.global.entity.BaseEntity;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "alias")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Alias extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alias_id")
+    private Long aliasId;
+
+    @Column(length = 50, nullable = false)
+    private String value;
+
+    @OneToOne(mappedBy = "alias")
+    private User user;
+
+    @OneToMany(mappedBy = "alias", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> categories = new ArrayList<>();
+
+
+}
