eab7a0f553b5ad2f6bb4ec95bd3597a3b589df44
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:45:33 +0900

    [feat] RecentSearch 엔티티 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/RecentSearch.java
@@ -0,0 +1,31 @@
+
+package konkuk.thip.entity;
+
+import jakarta.persistence.*;
+import konkuk.thip.global.entity.BaseEntity;
+import lombok.*;
+
+
+@Entity
+@Table(name = "recent_search")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class RecentSearch extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recentSearchId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "search_term",length = 50, nullable = false)
+    private String searchTerm;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SearchType type;
+}
