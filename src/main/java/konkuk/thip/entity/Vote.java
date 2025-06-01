c967355f531a58f1cbc1a8286f17534b54a02ba3
강희진 <kanghuijin12@gmail.com>
2025-06-01 19:48:55 +0900

    [feat] Vote 엔티티 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/Vote.java
@@ -0,0 +1,32 @@
+package konkuk.thip.entity;
+
+import jakarta.persistence.*;
+import konkuk.thip.global.entity.BaseEntity;
+import lombok.*;
+
+@Entity
+@Table(name = "vote")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Vote extends BaseEntity {
+
+    @Id
+    @Column(name = "post_id")
+    private Long postId;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    private Integer page;
+
+    @Column(name = "is_overview",nullable = false)
+    private boolean isOverview;
+}
