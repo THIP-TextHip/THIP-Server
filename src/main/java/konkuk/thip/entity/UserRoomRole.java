caf656022cb7e83f6b06086ebeb147cb6237cc49
강희진 <kanghuijin12@gmail.com>
2025-06-02 14:13:34 +0900

    [chore] UserRoomRole enum 작성 (#11)

---
+++ src/main/java/konkuk/thip/entity/UserRoomRole.java
@@ -0,0 +1,30 @@
+
+package konkuk.thip.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRoomRole {
+
+    HOST("호스트"),
+    MEMBER("팀원");
+
+    private String type;
+
+    UserRoomRole(String type) {
+        this.type = type;
+    }
+
+    public static UserRoomRole from(String type) {
+        for (UserRoomRole userRoomRole : UserRoomRole.values()) {
+            if (userRoomRole.getType().equals(type)) {
+                return userRoomRole;
+            }
+        }
+        //컨트롤러 어드바이스 추가하고 예외처리
+        //throw new GlobalException(NO_SUCH_TYPE_USER);
+        return null;
+    }
+
+
+}
