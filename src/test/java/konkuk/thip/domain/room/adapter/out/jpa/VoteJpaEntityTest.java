916236b8fda8106b9273281b40d450925a0bbcbe
janghyunjun <buzz03312@naver.com>
2025-06-21 01:26:26 +0900

    [test] CI 시에 자동으로 Spring profile test로 제정하는거 삭 (#14)

--- src/test/java/konkuk/thip/domain/room/adapter/out/jpa/VoteJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/room/adapter/out/jpa/VoteJpaEntityTest.java
@@ -15,12 +15,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class VoteJpaEntityTest {
 
     @PersistenceContext
