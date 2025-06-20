291af770aff353b1a642c97a71c043723ef56cbd
janghyunjun <buzz03312@naver.com>
2025-06-21 01:20:47 +0900

    [test] CI 시에 자동으로 Spring profile test로 지정 (#14)

--- src/test/java/konkuk/thip/domain/room/adapter/out/jpa/RoomJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/room/adapter/out/jpa/RoomJpaEntityTest.java
@@ -9,12 +9,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 @DataJpaTest
-@ActiveProfiles("test")
 class RoomJpaEntityTest {
     @PersistenceContext
     private EntityManager em;
--- src/test/java/konkuk/thip/domain/user/adapter/out/jpa/UserJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/user/adapter/out/jpa/UserJpaEntityTest.java
@@ -8,12 +8,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ActiveProfiles("test")
 class UserJpaEntityTest {
 
     @PersistenceContext
