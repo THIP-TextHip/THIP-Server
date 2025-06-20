be28a91531efab23257d749fcdd50aa2b3ff5dc0
janghyunjun <buzz03312@naver.com>
2025-06-20 17:21:47 +0900

    [chore] @EnableJpaAuditing 추가 (#14)

--- src/main/java/konkuk/thip/ThipServerApplication.java
+++ src/main/java/konkuk/thip/ThipServerApplication.java
@@ -2,7 +2,9 @@ package konkuk.thip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ThipServerApplication {
 
