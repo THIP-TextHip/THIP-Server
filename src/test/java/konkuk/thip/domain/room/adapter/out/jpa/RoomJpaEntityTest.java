2729c2cb168526b81284f54a5ae086218cff718c
janghyunjun <buzz03312@naver.com>
2025-06-21 01:16:28 +0900

    [test] 모든 테스트 내장데이터베이스 H2 명시 제거 (#14)

--- src/test/java/konkuk/thip/domain/room/adapter/out/jpa/RoomJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/room/adapter/out/jpa/RoomJpaEntityTest.java
@@ -8,8 +8,6 @@ import konkuk.thip.domain.room.adapter.out.persistence.RoomJpaRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -17,7 +15,6 @@ import java.time.LocalDate;
 
 @DataJpaTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(connection= EmbeddedDatabaseConnection.H2)
 class RoomJpaEntityTest {
     @PersistenceContext
     private EntityManager em;
