905463b47f8f2bcc1f0982560bf575affd1fb872
janghyunjun <buzz03312@naver.com>
2025-06-21 01:15:01 +0900

    [test] 모든 테스트 내장데이터베이스 H2 명시 제거 (#14)

--- src/test/java/konkuk/thip/domain/feed/adapter/out/jpa/FeedJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/feed/adapter/out/jpa/FeedJpaEntityTest.java
@@ -13,8 +13,6 @@ import konkuk.thip.domain.user.adapter.out.persistence.UserJpaRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -22,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(connection= EmbeddedDatabaseConnection.H2)
 class FeedJpaEntityTest {
 
     @PersistenceContext
