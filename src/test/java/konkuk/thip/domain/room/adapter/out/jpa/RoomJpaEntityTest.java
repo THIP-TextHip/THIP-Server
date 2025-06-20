8425f93a8d120dc9e56b040fadf0c182f57d80c1
janghyunjun <buzz03312@naver.com>
2025-06-21 00:43:49 +0900

    [test] Spring Profile test로 지정 (#14)

--- src/test/java/konkuk/thip/domain/room/adapter/out/jpa/RoomJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/room/adapter/out/jpa/RoomJpaEntityTest.java
@@ -11,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(connection= EmbeddedDatabaseConnection.H2)
 class RoomJpaEntityTest {
     @PersistenceContext
--- src/test/java/konkuk/thip/domain/user/adapter/out/jpa/UserJpaEntityTest.java
+++ src/test/java/konkuk/thip/domain/user/adapter/out/jpa/UserJpaEntityTest.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(connection= EmbeddedDatabaseConnection.H2)
 class UserJpaEntityTest {
 
