211739cc63429a043b39e887e490faddd6846f15
seongjunnoh <joon1754@naver.com>
2025-06-24 01:42:51 +0900

    [feat] record 도메인 엔티티, mapper 추가 (#20)

---
+++ src/main/java/konkuk/thip/room/domain/Record.java
@@ -0,0 +1,22 @@
+package konkuk.thip.room.domain;
+
+import konkuk.thip.common.entity.BaseDomainEntity;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class Record extends BaseDomainEntity {
+
+    private Long id;
+
+    private String content;
+
+    private Long creatorId;
+
+    private Integer page;
+
+    private boolean isOverview;
+
+    private Long roomId;
+}
