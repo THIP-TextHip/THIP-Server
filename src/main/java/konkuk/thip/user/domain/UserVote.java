fe8d1f5bc0f829a64edd4e2dc533dcd72da18887
seongjunnoh <joon1754@naver.com>
2025-06-24 01:41:46 +0900

    [feat] user vote 도메인 엔티티, mapper 추가 (#20)

---
+++ src/main/java/konkuk/thip/user/domain/UserVote.java
@@ -0,0 +1,16 @@
+package konkuk.thip.user.domain;
+
+import konkuk.thip.common.entity.BaseDomainEntity;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class UserVote extends BaseDomainEntity {
+
+    private Long id;
+
+    private Long userId;
+
+    private Long voteItemId;
+}
