853b46210b840a4733e024fa1934417337c15882
seongjunnoh <joon1754@naver.com>
2025-06-24 01:39:47 +0900

    [feat] alias 도메인 엔티티, mapper 추가 (#20)

---
+++ src/main/java/konkuk/thip/user/domain/Alias.java
@@ -0,0 +1,14 @@
+package konkuk.thip.user.domain;
+
+import konkuk.thip.common.entity.BaseDomainEntity;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class Alias extends BaseDomainEntity {
+
+    private Long id;
+
+    private String value;
+}
