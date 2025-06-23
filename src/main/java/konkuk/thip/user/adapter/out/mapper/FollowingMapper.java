5b687bae9f97dab866d05dbe63aa796ece46350b
seongjunnoh <joon1754@naver.com>
2025-06-24 01:39:20 +0900

    [feat] following 도메인 엔티티, mapper 추가 (#20)

---
+++ src/main/java/konkuk/thip/user/adapter/out/mapper/FollowingMapper.java
@@ -0,0 +1,28 @@
+package konkuk.thip.user.adapter.out.mapper;
+
+import konkuk.thip.user.adapter.out.jpa.FollowingJpaEntity;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.domain.Following;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FollowingMapper {
+
+    public FollowingJpaEntity toJpaEntity(UserJpaEntity userJpaEntity, UserJpaEntity followingUserJpaEntity) {
+        return FollowingJpaEntity.builder()
+                .userJpaEntity(userJpaEntity)
+                .followingUserJpaEntity(followingUserJpaEntity)
+                .build();
+    }
+
+    public Following toDomainEntity(FollowingJpaEntity followingJpaEntity) {
+        return Following.builder()
+                .id(followingJpaEntity.getFollowingId())
+                .userId(followingJpaEntity.getUserJpaEntity().getUserId())
+                .followingUserId(followingJpaEntity.getFollowingUserJpaEntity().getUserId())
+                .createdAt(followingJpaEntity.getCreatedAt())
+                .modifiedAt(followingJpaEntity.getModifiedAt())
+                .status(followingJpaEntity.getStatus())
+                .build();
+    }
+}
