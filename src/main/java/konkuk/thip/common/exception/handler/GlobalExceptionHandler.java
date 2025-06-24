2239c618ae7375f78d3990c9d4389cfe1929dab0
janghyunjun <buzz03312@naver.com>
2025-06-24 16:45:33 +0900

    [chore] 에러 핸들러에서  HttpStatus 제거 및 에러 바인딩 (#18)

--- src/main/java/konkuk/thip/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/konkuk/thip/common/exception/handler/GlobalExceptionHandler.java
@@ -2,74 +2,91 @@ package konkuk.thip.common.exception.handler;
 
 import konkuk.thip.common.dto.ErrorResponse;
 import konkuk.thip.common.exception.BusinessException;
-import konkuk.thip.common.exception.code.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import static konkuk.thip.common.exception.code.ErrorCode.*;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    // 요청한 API가 없는 경우
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ErrorResponse noHandlerExceptionHandler(NoHandlerFoundException e) {
-        return ErrorResponse.of(ErrorCode.API_NOT_FOUND);
+    public ResponseEntity<ErrorResponse> noHandlerExceptionHandler(NoHandlerFoundException e) {
+        return ResponseEntity
+                .status(API_NOT_FOUND.getHttpStatus())
+                .body(ErrorResponse.of(API_NOT_FOUND));
     }
 
     // 허용되지 않은 HTTP 메소드로 요청한 경우
-    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ErrorResponse httpRequestMethodNotSupportedExceptionHandler(HttpRequestMethodNotSupportedException e) {
+    public ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedExceptionHandler(HttpRequestMethodNotSupportedException e) {
         log.error("[HttpRequestMethodNotSupportedExceptionHandler] {}", e.getMessage());
-        return ErrorResponse.of(ErrorCode.API_METHOD_NOT_ALLOWED);
+        return ResponseEntity
+                .status(API_METHOD_NOT_ALLOWED.getHttpStatus())
+                .body(ErrorResponse.of(API_METHOD_NOT_ALLOWED));
     }
 
     // 요청 파라미터가 유효하지 않은 경우
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         log.error("[MethodArgumentNotValidExceptionHandler] {}", e.getMessage());
-        return ErrorResponse.of(ErrorCode.API_INVALID_PARAM);
+        // 첫 번째 유효성 검사 실패 메시지만 가져오기
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(error -> error.getDefaultMessage())
+                .orElse("Validation failed");
+
+        return ResponseEntity
+                .status(API_INVALID_PARAM.getHttpStatus())
+                .body(ErrorResponse.of(API_INVALID_PARAM, errorMessage));
     }
 
     // 요청 파라미터의 타입이 맞지 않는 경우
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ErrorResponse methodArgumentTypeMismatchExceptionHandler(MethodArgumentTypeMismatchException e) {
+    public ResponseEntity<ErrorResponse> methodArgumentTypeMismatchExceptionHandler(MethodArgumentTypeMismatchException e) {
         log.error("[MethodArgumentTypeMismatchExceptionHandler] {}", e.getMessage());
-        return ErrorResponse.of(ErrorCode.API_INVALID_TYPE);
+
+        return ResponseEntity
+                .status(API_INVALID_TYPE.getHttpStatus())
+                .body(ErrorResponse.of(API_INVALID_TYPE, e.getName() + "는 " + e.getRequiredType() + " 타입이어야 합니다."));
     }
 
     // 요청 파라미터가 누락된 경우
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ErrorResponse missingServletRequestParameterExceptionHandler(MissingServletRequestParameterException e) {
+    public ResponseEntity<ErrorResponse> missingServletRequestParameterExceptionHandler(MissingServletRequestParameterException e) {
         log.error("[MissingServletRequestParameterExceptionHandler] {}", e.getMessage());
-        return ErrorResponse.of(ErrorCode.API_MISSING_PARAM);
+        return ResponseEntity
+                .status(API_MISSING_PARAM.getHttpStatus())
+                .body(ErrorResponse.of(API_MISSING_PARAM, e.getParameterName() + "를 추가해서 요청해주세요."));
     }
 
     // 비즈니스 로직에서 발생한 예외 처리
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BusinessException.class)
-    public ErrorResponse businessExceptionHandler(BusinessException e) {
+    public ResponseEntity<ErrorResponse> businessExceptionHandler(BusinessException e) {
         log.error("[BusinessExceptionHandler] {}", e.getMessage());
-        return ErrorResponse.of(e.getErrorCode());
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(ErrorResponse.of(e.getErrorCode()));
     }
 
     // 서버 내부 오류 예외 처리
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({RuntimeException.class, IllegalStateException.class})
-    public ErrorResponse runtimeExceptionHandler(Exception e) {
+    public ResponseEntity<ErrorResponse> runtimeExceptionHandler(Exception e) {
         log.error("[RuntimeExceptionHandler] {}", e.getMessage());
-        return ErrorResponse.of(ErrorCode.API_SERVER_ERROR);
+        return ResponseEntity
+                .status(API_SERVER_ERROR.getHttpStatus())
+                .body(ErrorResponse.of(API_SERVER_ERROR));
     }
 
 }
