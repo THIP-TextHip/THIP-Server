c0686c26d82eae6ab9ac84c6dcb58a9c67ac093b
seongjunnoh <joon1754@naver.com>
2025-06-26 17:34:02 +0900

    [refactor] : User 에 email 필드 추가 (#28)

--- src/main/java/konkuk/thip/user/adapter/out/mapper/UserMapper.java
+++ src/main/java/konkuk/thip/user/adapter/out/mapper/UserMapper.java
@@ -11,6 +11,7 @@ public class UserMapper {
 
     public UserJpaEntity toJpaEntity(User user, AliasJpaEntity aliasJpaEntity) {
         return UserJpaEntity.builder()
+                .email(user.getEmail())
                 .nickname(user.getNickname())
                 .imageUrl(user.getImageUrl())
                 .role(UserRole.from(user.getUserRole()))
@@ -21,6 +22,7 @@ public class UserMapper {
     public User toDomainEntity(UserJpaEntity userJpaEntity) {
         return User.builder()
                 .id(userJpaEntity.getUserId())
+                .email(userJpaEntity.getEmail())
                 .nickname(userJpaEntity.getNickname())
                 .imageUrl(userJpaEntity.getImageUrl())
                 .userRole(userJpaEntity.getRole().getType())
