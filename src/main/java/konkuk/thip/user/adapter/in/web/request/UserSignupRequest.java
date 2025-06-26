4d9dfa9f6c8523303e72763886e9d08b039d5560
seongjunnoh <joon1754@naver.com>
2025-06-26 17:38:55 +0900

    [feat] : 회원가입 api controller 부분 개발 (#28)

--- src/main/java/konkuk/thip/user/adapter/in/web/request/UserSignupRequest.java
+++ src/main/java/konkuk/thip/user/adapter/in/web/request/UserSignupRequest.java
@@ -1,7 +1,28 @@
 package konkuk.thip.user.adapter.in.web.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSignupRequest {
+
+    @NotNull(message = "aliasId는 필수입니다.")
+    private Long aliasId;
+
+    @NotBlank(message = "닉네임은 공백일 수 없습니다.")
+    @Length(max = 10, message = "닉네임은 최대 10자 입니다.")
+    private String nickname;
+
+    @NotBlank(message = "이메일은 공백일 수 없습니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
 }
---
+++ src/main/java/konkuk/thip/user/adapter/in/web/response/UserSignupResponse.java
@@ -0,0 +1,11 @@
+package konkuk.thip.user.adapter.in.web.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserSignupResponse {
+
+    private Long userId;
+}
