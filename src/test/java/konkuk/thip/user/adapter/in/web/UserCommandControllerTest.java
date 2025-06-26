b1f93722b7ab87e83152071e6162e6cef6b007b4
seongjunnoh <joon1754@naver.com>
2025-06-26 17:40:31 +0900

    [test] : 회원가입 api 통합 테스트 코드, controller 테스트 코드 작성 (#28)

---
+++ src/test/java/konkuk/thip/user/adapter/in/web/UserCommandControllerTest.java
@@ -0,0 +1,174 @@
+package konkuk.thip.user.adapter.in.web;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import konkuk.thip.user.adapter.in.web.request.UserSignupRequest;
+import konkuk.thip.user.adapter.out.jpa.AliasJpaEntity;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.AliasJpaRepository;
+import konkuk.thip.user.adapter.out.persistence.UserJpaRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class UserCommandControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AliasJpaRepository aliasJpaRepository;
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @Test
+    @DisplayName("[칭호id, 닉네임, 이메일] 정보를 바탕으로 회원가입을 진행한다.")
+    void signup_success() throws Exception {
+        //given : alias 생성, 회원가입 request 생성
+        AliasJpaEntity aliasJpaEntity = AliasJpaEntity.builder()
+                .value("칭호")
+                .color("blue")
+                .imageUrl("http://image.url")
+                .build();
+        aliasJpaRepository.save(aliasJpaEntity);
+
+        UserSignupRequest request = new UserSignupRequest(
+                aliasJpaEntity.getAliasId(),
+                "테스트 유저",
+                "test@test.com"
+        );
+
+        //when : 회원가입 api 호출
+        ResultActions result = mockMvc.perform(post("/users/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.userId").exists());
+
+        String json = result.andReturn().getResponse().getContentAsString();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        Long userId = jsonNode.path("data").path("userId").asLong();
+
+        UserJpaEntity userJpaEntity = userJpaRepository.findById(userId).orElse(null);
+
+        assertThat(userJpaEntity.getAliasForUserJpaEntity().getAliasId()).isEqualTo(request.getAliasId());
+        assertThat(userJpaEntity.getNickname()).isEqualTo(request.getNickname());
+        assertThat(userJpaEntity.getEmail()).isEqualTo(request.getEmail());
+    }
+
+    @Test
+    @DisplayName("[칭호id]값이 null일 경우, 400 error가 발생한다.")
+    void signup_whenAliasIdNull_thenBadRequest() throws Exception {
+        //given: aliasId null
+        UserSignupRequest request = new UserSignupRequest(
+                null,
+                "테스트유저",
+                "test@test.com"
+        );
+
+        //when //then
+        mockMvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("40002"))
+                .andExpect(jsonPath("$.message", containsString("aliasId는 필수입니다.")));
+    }
+
+    @Test
+    @DisplayName("[닉네임]값이 공백일 경우, 400 error가 발생한다.")
+    void signup_whenNicknameBlank_thenBadRequest() throws Exception {
+        //given: nickname blank
+        UserSignupRequest request = new UserSignupRequest(
+                1L,
+                "",
+                "test@test.com"
+        );
+
+        //when //then
+        mockMvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("40002"))
+                .andExpect(jsonPath("$.message", containsString("닉네임은 공백일 수 없습니다.")));
+    }
+
+    @Test
+    @DisplayName("[닉네임]값이 11자 이상일 경우, 400 error가 발생한다.")
+    void signup_whenNicknameTooLong_thenBadRequest() throws Exception {
+        //given: nickname blank
+        UserSignupRequest request = new UserSignupRequest(
+                1L,
+                "11자_닉네임_입니다",
+                "test@test.com"
+        );
+
+        //when //then
+        mockMvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("40002"))
+                .andExpect(jsonPath("$.message", containsString("닉네임은 최대 10자 입니다.")));
+    }
+
+    @Test
+    @DisplayName("[이메일]값이 공백일 경우, 400 error가 발생한다.")
+    void signup_whenEmailBlank_thenBadRequest() throws Exception {
+        //given
+        UserSignupRequest request = new UserSignupRequest(
+                1L,
+                "테스트유저",
+                ""
+        );
+
+        //when //then
+        mockMvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("40002"))
+                .andExpect(jsonPath("$.message", containsString("이메일은 공백일 수 없습니다.")));
+    }
+
+    @Test
+    @DisplayName("[이메일]값이 유효한 이메일 형식이 아닐 경우, 400 error가 발생한다.")
+    void signup_whenEmailInvalidFormat_thenBadRequest() throws Exception {
+        //given
+        UserSignupRequest request = new UserSignupRequest(
+                1L,
+                "테스트유저",
+                "invalid-email-format"
+        );
+
+        //when //then
+        mockMvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("40002"))
+                .andExpect(jsonPath("$.message", containsString("이메일 형식이 올바르지 않습니다.")));
+    }
+}
