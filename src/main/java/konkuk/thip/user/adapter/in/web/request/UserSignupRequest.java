bad9a59d25e42191abdc71b019d991033ceaa9a2
seongjunnoh <joon1754@naver.com>
2025-06-26 21:36:35 +0900

    [refactor] : setter 삭제 (#28)

--- src/main/java/konkuk/thip/user/adapter/in/web/request/UserSignupRequest.java
+++ src/main/java/konkuk/thip/user/adapter/in/web/request/UserSignupRequest.java
@@ -7,11 +7,9 @@ import konkuk.thip.user.application.port.in.dto.UserSignupCommand;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSignupRequest {
