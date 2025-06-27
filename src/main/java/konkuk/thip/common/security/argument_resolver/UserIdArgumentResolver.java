b23a6fa8fa240b6ecab12c721df060b49918d7ac
janghyunjun <buzz03312@naver.com>
2025-06-28 03:31:19 +0900

    [feat] 필요한 어노테이션 및 argument resolver (#30)

---
+++ src/main/java/konkuk/thip/common/security/argument_resolver/UserIdArgumentResolver.java
@@ -0,0 +1,38 @@
+package konkuk.thip.common.security.argument_resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import konkuk.thip.common.exception.AuthException;
+import konkuk.thip.common.security.annotation.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static konkuk.thip.common.exception.code.ErrorCode.AUTH_TOKEN_NOT_FOUND;
+
+@Component
+@RequiredArgsConstructor
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(UserId.class)
+                && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+
+        Object userId = ((HttpServletRequest) webRequest.getNativeRequest()).getAttribute("userId");
+        if (userId == null) {
+            throw new AuthException(AUTH_TOKEN_NOT_FOUND);
+        }
+        return userId;
+    }
+}
