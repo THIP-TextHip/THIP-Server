a785e947f906e77fd0cf303359591414840cf2ac
강희진 <kanghuijin12@gmail.com>
2025-06-28 01:38:07 +0900

    [feat] BookQueryController 구현 (#32)

--- src/main/java/konkuk/thip/book/adapter/in/web/BookQueryController.java
+++ src/main/java/konkuk/thip/book/adapter/in/web/BookQueryController.java
@@ -1,10 +1,20 @@
 package konkuk.thip.book.adapter.in.web;
 
+import konkuk.thip.book.adapter.in.web.response.GetBookSearchListResponse;
+import konkuk.thip.book.application.port.in.BookSearchUseCase;
+import konkuk.thip.common.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class BookQueryController {
 
+    private final BookSearchUseCase bookSearchUseCase;
+
+    @GetMapping("/books")
+    public BaseResponse<GetBookSearchListResponse> getBookSearchList(@RequestParam final String keyword,
+                                                                     @RequestParam final int page) {
+        return BaseResponse.ok(bookSearchUseCase.searchBooks(keyword, page));
+    }
 }
