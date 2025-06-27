54df077e0167f99fc60d8490372ee85a3cc24b3e
강희진 <kanghuijin12@gmail.com>
2025-06-28 01:39:24 +0900

    [refactor] BusinessException 에러메세지 리펙 (#32)

--- src/main/java/konkuk/thip/common/exception/BusinessException.java
+++ src/main/java/konkuk/thip/common/exception/BusinessException.java
@@ -8,11 +8,12 @@ public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
     public BusinessException(ErrorCode errorCode, Exception e) {
-        super(e);
+        super(errorCode.getMessage(), e);
         this.errorCode = errorCode;
     }
 }
