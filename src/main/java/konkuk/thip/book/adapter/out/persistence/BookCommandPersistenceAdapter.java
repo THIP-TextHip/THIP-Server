c3c27a0312a406f5fc681e4cf3c7668ef3182306
강희진 <kanghuijin12@gmail.com>
2025-06-30 03:57:43 +0900

    [feat] BookCommandPersistenceAdapter.findByIsbn 작성 (#34)

--- src/main/java/konkuk/thip/book/adapter/out/persistence/BookCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/book/adapter/out/persistence/BookCommandPersistenceAdapter.java
@@ -1,15 +1,28 @@
 package konkuk.thip.book.adapter.out.persistence;
 
+import konkuk.thip.book.adapter.out.jpa.BookJpaEntity;
 import konkuk.thip.book.adapter.out.mapper.BookMapper;
 import konkuk.thip.book.application.port.out.BookCommandPort;
+import konkuk.thip.book.domain.Book;
+import konkuk.thip.common.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static konkuk.thip.common.exception.code.ErrorCode.BOOK_NOT_FOUND;
 
 @Repository
 @RequiredArgsConstructor
 public class BookCommandPersistenceAdapter implements BookCommandPort {
 
-    private final BookJpaRepository jpaRepository;
-    private final BookMapper userMapper;
+    private final BookJpaRepository bookJpaRepository;
+    private final BookMapper bookMapper;
 
+    @Override
+    public Book findByIsbn(String isbn) {
+
+        BookJpaEntity bookJpaEntity = bookJpaRepository.findByIsbn(isbn).orElseThrow(
+                () -> new EntityNotFoundException(BOOK_NOT_FOUND));
+
+        return bookMapper.toDomainEntity(bookJpaEntity);
+    }
 }
