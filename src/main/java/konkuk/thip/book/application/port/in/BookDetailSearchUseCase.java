dc9f160ed73bac9826adef3161a6fbff98c659cf
강희진 <kanghuijin12@gmail.com>
2025-06-30 03:58:18 +0900

    [remove] 사용하지않는 파일 삭제 (#34)

--- src/main/java/konkuk/thip/book/application/port/in/BookDetailSearchUseCase.java
+++
@@ -1,9 +0,0 @@
-package konkuk.thip.book.application.port.in;
-
-import konkuk.thip.book.application.port.in.dto.BookDetailSearchResult;
-
-public interface BookDetailSearchUseCase {
-
-    BookDetailSearchResult searchDetailBooks(String isbn);
-
-}
--- src/main/java/konkuk/thip/book/application/port/out/SearchDetailBookQueryPort.java
+++
@@ -1,7 +0,0 @@
-package konkuk.thip.book.application.port.out;
-
-import konkuk.thip.book.adapter.out.api.dto.NaverDetailBookParseResult;
-
-public interface SearchDetailBookQueryPort {
-    NaverDetailBookParseResult findDetailBookByKeyword(String isbn);
-}
