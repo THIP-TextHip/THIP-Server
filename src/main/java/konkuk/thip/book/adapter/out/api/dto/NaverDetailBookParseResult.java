3d0a5635dd69c5cfd8a61db9779c8d15dec6c6ca
강희진 <kanghuijin12@gmail.com>
2025-06-29 22:27:22 +0900

    [feat] NaverDetailBookParseResult 추가 (#34)

---
+++ src/main/java/konkuk/thip/book/adapter/out/api/dto/NaverDetailBookParseResult.java
@@ -0,0 +1,16 @@
+
+package konkuk.thip.book.adapter.out.api.dto;
+
+import lombok.Builder;
+
+@Builder
+public record NaverDetailBookParseResult(
+            String title,
+            String imageUrl,
+            String author,
+            String publisher,
+            String isbn,
+            String description
+    ) {
+
+}
