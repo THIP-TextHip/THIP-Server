4f5169c50eac60ebbb4b8b37785d808072edebdd
강희진 <kanghuijin12@gmail.com>
2025-06-29 22:12:58 +0900

    [feat] GetBookDetailSearchResponse dto 작성 (#34)

---
+++ src/main/java/konkuk/thip/book/adapter/in/web/response/GetBookDetailSearchResponse.java
@@ -0,0 +1,32 @@
+package konkuk.thip.book.adapter.in.web.response;
+
+import konkuk.thip.book.application.port.in.dto.BookDetailSearchResult;
+import lombok.Builder;
+
+@Builder
+public record GetBookDetailSearchResponse(
+        String title,
+        String imageUrl,
+        String authorName,
+        String publisher,
+        String isbn,
+        String description,
+        int recruitingRoomCount,
+        int recruitingReadCount,
+        boolean isSaved //회원가입 머지되면 추가
+) {
+    public static GetBookDetailSearchResponse of(BookDetailSearchResult result) {
+
+        return new GetBookDetailSearchResponse(
+                result.naverDetailBook().title(),
+                result.naverDetailBook().imageUrl(),
+                result.naverDetailBook().author(),
+                result.naverDetailBook().publisher(),
+                result.naverDetailBook().isbn(),
+                result.naverDetailBook().description(),
+                result.recruitingRoomCount(),
+                result.recruitingReadCount(),
+                result.isSaved());
+    }
+
+}
