bb38b45a093ceb04aeee2eef988aac977be93672
강희진 <kanghuijin12@gmail.com>
2025-06-30 04:04:38 +0900

    [feat] UserJpaRepository.findById (#34)

--- src/main/java/konkuk/thip/user/adapter/out/persistence/UserJpaRepository.java
+++ src/main/java/konkuk/thip/user/adapter/out/persistence/UserJpaRepository.java
@@ -3,7 +3,10 @@ package konkuk.thip.user.adapter.out.persistence;
 import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long>, UserQueryRepository {
 
     boolean existsByNickname(String nickname);
+    Optional<UserJpaEntity> findById(Long userId);
 }
