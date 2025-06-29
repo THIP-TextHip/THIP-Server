0e752fe450a88649b71eda0fb2ba9abee83fcad7
강희진 <kanghuijin12@gmail.com>
2025-06-29 17:44:46 +0900

    [refactor] NaverBookParseResult 반환값 NaverBook로 수정 (#32)

--- src/main/java/konkuk/thip/book/adapter/out/api/dto/NaverBookParseResult.java
+++ src/main/java/konkuk/thip/book/adapter/out/api/dto/NaverBookParseResult.java
@@ -1,15 +1,23 @@
 package konkuk.thip.book.adapter.out.api.dto;
 
-import konkuk.thip.book.domain.Book;
+import lombok.Builder;
 
 import java.util.List;
 
 
 public record NaverBookParseResult(
-        List<Book> books,
+        List<NaverBook> naverBooks,
         int total,
         int start) {
-    public static NaverBookParseResult of(List<Book> books, int total, int start) {
+    @Builder
+    public record NaverBook(
+            String title,
+            String imageUrl,
+            String author,
+            String publisher,
+            String isbn
+    ) {}
+    public static NaverBookParseResult of(List<NaverBook> books, int total, int start) {
         return new NaverBookParseResult(books, total, start);
     }
 }
