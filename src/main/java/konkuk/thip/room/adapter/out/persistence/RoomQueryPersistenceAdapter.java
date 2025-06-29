439c26db5200e713ea6c363b9a597c3c4394abd6
강희진 <kanghuijin12@gmail.com>
2025-06-30 04:03:12 +0900

    [feat] RoomQueryPersistenceAdapter.countRecruitingRoomsByBookAndStartDateAfter (#34)

--- src/main/java/konkuk/thip/room/adapter/out/persistence/RoomQueryPersistenceAdapter.java
+++ src/main/java/konkuk/thip/room/adapter/out/persistence/RoomQueryPersistenceAdapter.java
@@ -5,11 +5,17 @@ import konkuk.thip.room.application.port.out.RoomQueryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 @RequiredArgsConstructor
 public class RoomQueryPersistenceAdapter implements RoomQueryPort {
 
-    private final RoomJpaRepository jpaRepository;
-    private final RoomMapper userMapper;
+    private final RoomJpaRepository roomJpaRepository;
+    private final RoomMapper roomMapper;
 
+    @Override
+    public int countRecruitingRoomsByBookAndStartDateAfter(Long bookId, LocalDate currentDate) {
+        return roomJpaRepository.countByBookJpaEntity_BookIdAndStartDateAfter(bookId, currentDate);
+    }
 }
