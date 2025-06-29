c2b00eaf92d9533cf43e26c87b06d249e98bbb81
seongjunnoh <joon1754@naver.com>
2025-06-29 21:45:16 +0900

    [refactor] : request, response dto 네이밍 수정 (#31)
    팀 컨벤션에 맞추어 수정

--- src/main/java/konkuk/thip/user/adapter/in/web/response/VerifyNicknameResponse.java
+++
@@ -1,7 +0,0 @@
-package konkuk.thip.user.adapter.in.web.response;
-
-public record VerifyNicknameResponse(boolean isVerified) {
-    public static VerifyNicknameResponse of(boolean isVerified) {
-        return new VerifyNicknameResponse(isVerified);
-    }
-}
