9c24c979164dbb6694f2b3a2c2f0ac956e68ef6b
강희진 <kanghuijin12@gmail.com>
2025-06-30 15:59:10 +0900

    [feat] RecentSearchCommandPersistenceAdapter.save 작성 (#41)

--- src/main/java/konkuk/thip/recentSearch/adapter/out/persistence/RecentSearchCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/recentSearch/adapter/out/persistence/RecentSearchCommandPersistenceAdapter.java
@@ -1,15 +1,38 @@
 package konkuk.thip.recentSearch.adapter.out.persistence;
 
+import konkuk.thip.common.exception.EntityNotFoundException;
+import konkuk.thip.recentSearch.adapter.out.jpa.RecentSearchJpaEntity;
+import konkuk.thip.recentSearch.adapter.out.jpa.SearchType;
 import konkuk.thip.recentSearch.adapter.out.mapper.RecentSearchMapper;
 import konkuk.thip.recentSearch.application.port.out.RecentSearchCommandPort;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static konkuk.thip.common.exception.code.ErrorCode.ALIAS_NOT_FOUND;
 
 @Repository
 @RequiredArgsConstructor
 public class RecentSearchCommandPersistenceAdapter implements RecentSearchCommandPort {
 
     private final RecentSearchJpaRepository recentSearchJpaRepository;
+    private final UserJpaRepository userJpaRepository;
+
     private final RecentSearchMapper recentSearchMapper;
 
+    @Override
+    public void save(Long userId, String keyword, SearchType searchType) {
+
+        UserJpaEntity userJpaEntity = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ALIAS_NOT_FOUND));
+
+        RecentSearchJpaEntity entity = RecentSearchJpaEntity.builder()
+                .searchTerm(keyword)
+                .type(searchType)
+                .userJpaEntity(userJpaEntity)
+                .build();
+
+        recentSearchJpaRepository.save(entity);
+    }
 }
