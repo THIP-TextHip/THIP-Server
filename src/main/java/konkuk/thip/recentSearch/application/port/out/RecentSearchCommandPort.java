66ba6d425c92b98a1ec27593838bfe707bb0af37
강희진 <kanghuijin12@gmail.com>
2025-06-30 15:59:28 +0900

    [feat] RecentSearchCommandPort.save 작성 (#41)

--- src/main/java/konkuk/thip/recentSearch/application/port/out/RecentSearchCommandPort.java
+++ src/main/java/konkuk/thip/recentSearch/application/port/out/RecentSearchCommandPort.java
@@ -1,6 +1,8 @@
 package konkuk.thip.recentSearch.application.port.out;
 
 
-public interface RecentSearchCommandPort {
+import konkuk.thip.recentSearch.adapter.out.jpa.SearchType;
 
+public interface RecentSearchCommandPort {
+    void save(Long userId, String keyword, SearchType searchType);
 }
