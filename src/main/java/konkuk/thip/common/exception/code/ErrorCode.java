b3dda33d8145c2ffa5bd025bfa46a04f792d5146
강희진 <kanghuijin12@gmail.com>
2025-06-30 14:44:26 +0900

    [refactor] 에러코드 중복 수정 (#34)

--- src/main/java/konkuk/thip/common/exception/code/ErrorCode.java
+++ src/main/java/konkuk/thip/common/exception/code/ErrorCode.java
@@ -50,8 +50,8 @@ public enum ErrorCode implements ResponseCode {
     BOOK_SEARCH_PAGE_OUT_OF_RANGE(HttpStatus.BAD_REQUEST, 80006,"검색어 페이지가 범위를 벗어났습니다."),
     BOOK_KEYWORD_REQUIRED(HttpStatus.BAD_REQUEST, 80007, "검색어는 필수 입력값입니다."),
     BOOK_PAGE_NUMBER_INVALID(HttpStatus.BAD_REQUEST, 80008, "페이지 번호는 1 이상의 값이어야 합니다."),
-    BOOK_ISBN_NOT_FOUND(HttpStatus.BAD_REQUEST, 80008, "ISBN으로 검색한 결과가 존재하지 않습니다."),
-    BOOK_NOT_FOUND(HttpStatus.BAD_REQUEST, 80009, "존재하지 않는 BOOK 입니다.");
+    BOOK_ISBN_NOT_FOUND(HttpStatus.BAD_REQUEST, 80009, "ISBN으로 검색한 결과가 존재하지 않습니다."),
+    BOOK_NOT_FOUND(HttpStatus.BAD_REQUEST, 800010, "존재하지 않는 BOOK 입니다.");
 
 
 
