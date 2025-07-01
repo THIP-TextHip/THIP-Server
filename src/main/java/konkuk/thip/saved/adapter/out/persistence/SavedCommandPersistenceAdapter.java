3eec6842a6eb034228190d4976dab8ea28f5f3c4
강희진 <kanghuijin12@gmail.com>
2025-07-01 19:12:39 +0900

    [feat] SavedCommandPersistenceAdapter.saveBook (#42)

--- src/main/java/konkuk/thip/saved/adapter/out/persistence/SavedCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/saved/adapter/out/persistence/SavedCommandPersistenceAdapter.java
@@ -1,18 +1,56 @@
 package konkuk.thip.saved.adapter.out.persistence;
 
+import konkuk.thip.book.adapter.out.jpa.BookJpaEntity;
+import konkuk.thip.book.adapter.out.persistence.BookJpaRepository;
+import konkuk.thip.common.exception.BusinessException;
+import konkuk.thip.common.exception.EntityNotFoundException;
+import konkuk.thip.saved.adapter.out.jpa.SavedBookJpaEntity;
 import konkuk.thip.saved.adapter.out.mapper.SavedBookMapper;
 import konkuk.thip.saved.adapter.out.mapper.SavedFeedMapper;
 import konkuk.thip.saved.application.port.out.SavedCommandPort;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static konkuk.thip.common.exception.code.ErrorCode.*;
 
 @Repository
 @RequiredArgsConstructor
 public class SavedCommandPersistenceAdapter implements SavedCommandPort {
 
+    private final UserJpaRepository userJpaRepository;
+    private final BookJpaRepository bookJpaRepository;
     private final SavedBookJpaRepository savedBookJpaRepository;
     private final SavedFeedJpaRepository savedFeedJpaRepository;
     private final SavedBookMapper savedBookMapper;
     private final SavedFeedMapper savedFeedMapper;
 
+    @Override
+    public void saveBook(Long userId, Long bookId) {
+
+        if (savedBookJpaRepository.existsByUserJpaEntity_UserIdAndBookJpaEntity_BookId(userId, bookId))
+            throw new BusinessException(BOOK_ALREADY_SAVED);
+
+        UserJpaEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
+        BookJpaEntity book = bookJpaRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException(BOOK_NOT_FOUND));
+        SavedBookJpaEntity entity = SavedBookJpaEntity.builder()
+                .userJpaEntity(user)
+                .bookJpaEntity(book)
+                .build();
+        savedBookJpaRepository.save(entity);
+
+    }
+
+    //삭제 전략 도입 전
+    @Override
+    public void deleteBook(Long userId, Long bookId) {
+
+        if (!savedBookJpaRepository.existsByUserJpaEntity_UserIdAndBookJpaEntity_BookId(userId, bookId))
+            throw new BusinessException(BOOK_NOT_SAVED_CANNOT_DELETE);
+
+        savedBookJpaRepository.deleteByUserJpaEntity_UserIdAndBookJpaEntity_BookId(userId, bookId);
+    }
 }
