1c4700a5e855213a0f75c73fe25c7da8d11b1a47
강희진 <kanghuijin12@gmail.com>
2025-07-01 19:13:05 +0900

    [refactor] 공백 삭제 (#42)

--- src/main/java/konkuk/thip/saved/application/port/out/SavedCommandPort.java
+++ src/main/java/konkuk/thip/saved/application/port/out/SavedCommandPort.java
@@ -2,7 +2,6 @@ package konkuk.thip.saved.application.port.out;
 
 
 public interface SavedCommandPort {
-
     void saveBook(Long userId, Long bookId);
     void deleteBook(Long userId, Long bookId);
 }
