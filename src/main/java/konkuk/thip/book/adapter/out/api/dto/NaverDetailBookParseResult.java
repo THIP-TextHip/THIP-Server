d6328f0ed71b655b79fca3a9c964115606555905
강희진 <kanghuijin12@gmail.com>
2025-07-01 19:11:27 +0900

    [feat] NaverDetailBookParseResult.toBook (#42)

--- src/main/java/konkuk/thip/book/adapter/out/api/dto/NaverDetailBookParseResult.java
+++ src/main/java/konkuk/thip/book/adapter/out/api/dto/NaverDetailBookParseResult.java
@@ -1,6 +1,7 @@
 
 package konkuk.thip.book.adapter.out.api.dto;
 
+import konkuk.thip.book.domain.Book;
 import lombok.Builder;
 
 @Builder
@@ -12,5 +13,16 @@ public record NaverDetailBookParseResult(
             String isbn,
             String description
     ) {
-
+    public static Book toBook(NaverDetailBookParseResult naverDetailBookParseResult) {
+        return Book.builder()
+                .title(naverDetailBookParseResult.title)
+                .isbn(naverDetailBookParseResult.isbn)
+                .authorName(naverDetailBookParseResult.author)
+                .publisher(naverDetailBookParseResult.publisher)
+                .bestSeller(false) //베스트셀러 구현 어떻게 할지 확정되면 나중에 수정,
+                // 지금은 베스트셀러 조회안하고 무조건 저장되는 책은 베스트셀러가아닌걸로 가정
+                .imageUrl(naverDetailBookParseResult.imageUrl)
+                .description(naverDetailBookParseResult.description)
+                .build();
+    }
 }
