6fa794ce7dfced61aa929f058a5ced1d7fd01629
강희진 <kanghuijin12@gmail.com>
2025-07-01 19:12:14 +0900

    [feat] SavedBookJpaRepository.deleteByUserJpaEntity_UserIdAndBookJpaEntity_BookId (#42)

--- src/main/java/konkuk/thip/saved/adapter/out/persistence/SavedBookJpaRepository.java
+++ src/main/java/konkuk/thip/saved/adapter/out/persistence/SavedBookJpaRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SavedBookJpaRepository extends JpaRepository<SavedBookJpaEntity, Long> {
     boolean existsByUserJpaEntity_UserIdAndBookJpaEntity_BookId(Long userId, Long bookId);
+    void deleteByUserJpaEntity_UserIdAndBookJpaEntity_BookId(Long userId, Long bookId);
 }
