af0f1154de4de605499a164fa610ed17e15c826d
강희진 <kanghuijin12@gmail.com>
2025-07-04 20:44:29 +0900

    [refactor] 메서드명 변경 (#42)

--- src/main/java/konkuk/thip/book/application/port/in/BookSavedUseCase.java
+++ src/main/java/konkuk/thip/book/application/port/in/BookSavedUseCase.java
@@ -1,8 +1,7 @@
 package konkuk.thip.book.application.port.in;
 
-import konkuk.thip.book.adapter.in.web.request.PostBookIsSavedRequest;
 import konkuk.thip.book.application.port.in.dto.BookIsSavedResult;
 
 public interface BookSavedUseCase {
-    BookIsSavedResult isSavedBook(String isbn, PostBookIsSavedRequest postBookIsSavedRequest, Long userId);
+    BookIsSavedResult changeSavedBook(String isbn, boolean isSave, Long userId);
 }
