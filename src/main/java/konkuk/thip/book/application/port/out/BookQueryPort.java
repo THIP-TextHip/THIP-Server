abd294c2c9401e7f3805180b5e3253be61e825dd
강희진 <kanghuijin12@gmail.com>
2025-07-05 03:19:27 +0900

    [feat] BookQueryPort.findByIsbnIn (#48)

--- src/main/java/konkuk/thip/book/application/port/out/BookQueryPort.java
+++ src/main/java/konkuk/thip/book/application/port/out/BookQueryPort.java
@@ -1,5 +1,9 @@
 package konkuk.thip.book.application.port.out;
 
-public interface BookQueryPort {
+import konkuk.thip.book.domain.Book;
 
+import java.util.List;
+
+public interface BookQueryPort {
+    List<Book> findByIsbnIn(List<String> isbnList);
 }
