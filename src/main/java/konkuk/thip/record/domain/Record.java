2fd757d83489f7c087c5f74bf04502a527b42060
janghyunjun <buzz03312@naver.com>
2025-07-06 01:35:41 +0900

    [feat] Record 도메인 규칙 선언 (#45)

--- src/main/java/konkuk/thip/record/domain/Record.java
+++ src/main/java/konkuk/thip/record/domain/Record.java
@@ -1,8 +1,11 @@
 package konkuk.thip.record.domain;
 
 import konkuk.thip.common.entity.BaseDomainEntity;
+import konkuk.thip.common.exception.InvalidStateException;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+
+import static konkuk.thip.common.exception.code.ErrorCode.*;
 
 @Getter
 @SuperBuilder
@@ -19,4 +22,44 @@ public class Record extends BaseDomainEntity {
     private boolean isOverview;
 
     private Long roomId;
+
+    public static Record withoutId(
+            String content,
+            Long creatorId,
+            Integer page,
+            boolean isOverview,
+            Long roomId
+    ) {
+        return Record.builder()
+                .content(content)
+                .creatorId(creatorId)
+                .page(page)
+                .isOverview(isOverview)
+                .roomId(roomId)
+                .build();
+    }
+
+
+    public void validateOverview(int totalPageCount) {
+        double ratio = (double) page / totalPageCount;
+        if (isOverview && ratio < 0.8) {
+            String message = String.format(
+                    "총평(isOverview)은 진행률이 80%% 이상일 때만 가능합니다. 현재 진행률 = %.2f%% (%d/%d)",
+                    ratio * 100, page, totalPageCount
+            );
+            throw new InvalidStateException(RECORD_CANNOT_BE_OVERVIEW, new IllegalStateException(message));
+        }
+    }
+
+    public void validatePage(int totalPageCount) {
+        if (page < 1 || page > totalPageCount) {
+            String message = String.format(
+                    "페이지 범위가 잘못되었습니다. 현재 기록할 page = %d, 책 전체 page = %d",
+                    page, totalPageCount
+            );
+            throw new InvalidStateException(INVALID_RECORD_PAGE_RANGE,
+                    new IllegalArgumentException(message)
+            );
+        }
+    }
 }
