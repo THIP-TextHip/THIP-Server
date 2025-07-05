983492edd5d878940d47b334e4ed0219697872f1
janghyunjun <buzz03312@naver.com>
2025-07-05 12:03:40 +0900

    [feat] 기록 생성 Controller (#45)

--- src/main/java/konkuk/thip/record/adapter/in/web/RecordCommandController.java
+++ src/main/java/konkuk/thip/record/adapter/in/web/RecordCommandController.java
@@ -1,10 +1,28 @@
 package konkuk.thip.record.adapter.in.web;
 
+import jakarta.validation.Valid;
+import konkuk.thip.common.dto.BaseResponse;
+import konkuk.thip.common.security.annotation.UserId;
+import konkuk.thip.record.adapter.in.web.request.RecordCreateRequest;
+import konkuk.thip.record.adapter.in.web.response.RecordCreateResponse;
+import konkuk.thip.record.application.port.in.RecordCreateUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class RecordCommandController {
+    private final RecordCreateUseCase recordCreateUseCase;
+
+    @PostMapping("/rooms/{roomId}/records")
+    public BaseResponse<RecordCreateResponse> createRecord(
+            @UserId Long userId,
+            @PathVariable Long roomId,
+            @Valid @RequestBody RecordCreateRequest recordCreateRequest) {
+        return BaseResponse.ok(
+                RecordCreateResponse.of(
+                        recordCreateUseCase.createRecord(recordCreateRequest.toCommand(roomId, userId))
+                ));
+    }
 
 }
