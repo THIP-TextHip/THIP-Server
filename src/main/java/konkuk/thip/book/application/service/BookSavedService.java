6610801cd27cce725ab5eaf8e0e964aecd4ef857
강희진 <kanghuijin12@gmail.com>
2025-07-05 13:09:59 +0900

    [refactor] 서비스 로직 수정 (#42)

--- src/main/java/konkuk/thip/book/application/service/BookSavedService.java
+++ src/main/java/konkuk/thip/book/application/service/BookSavedService.java
@@ -8,13 +8,14 @@ import konkuk.thip.book.application.port.out.BookApiQueryPort;
 import konkuk.thip.book.application.port.out.BookCommandPort;
 import konkuk.thip.book.domain.Book;
 import konkuk.thip.common.exception.BusinessException;
+import konkuk.thip.common.exception.EntityNotFoundException;
 import konkuk.thip.saved.application.port.out.SavedCommandPort;
 import konkuk.thip.saved.application.port.out.SavedQueryPort;
 import konkuk.thip.book.domain.SavedBooks;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static konkuk.thip.common.exception.code.ErrorCode.BOOK_NOT_FOUND;
+import static konkuk.thip.common.exception.code.ErrorCode.BOOK_NOT_SAVED_DB_CANNOT_DELETE;
 
 @Service
 @RequiredArgsConstructor
@@ -29,40 +30,49 @@ public class BookSavedService implements BookSavedUseCase {
     @Transactional
     public BookIsSavedResult changeSavedBook(String isbn, boolean isSave, Long userId) {
 
-        // Book 조회 or 생성
-        Book book = bookCommandPort.findByIsbn(isbn)
-                .orElseGet(() -> {
-                    if (!isSave) {
-                        // 삭제 요청인데 책이 DB에 없으면 저장하지 않은 책이므로 예외처리
-                        throw new BusinessException(BOOK_NOT_FOUND);
-                    }
-                    // 저장 요청인데 책이 DB에 없으면 네이버 API로 저장
-                    NaverDetailBookParseResult naverResult = bookApiQueryPort.findDetailBookByKeyword(isbn);
-                    Book newBook = Book.withoutId(
-                            naverResult.title(),
-                            naverResult.isbn(),
-                            naverResult.author(),
-                            false,
-                            naverResult.publisher(),
-                            naverResult.imageUrl(),
-                            null,
-                            naverResult.description());
-                    Long newBookId = bookCommandPort.save(newBook);
-                    return newBook.withId(newBookId);
-                });
+        Book book;
 
-        //유저가 저장한 책 목록 조회
+        try {
+            // Book 조회 시도
+            book = bookCommandPort.findByIsbn(isbn);
+        } catch (EntityNotFoundException e) {
+            // 책이 DB에 없을 때 처리
+
+            if (!isSave) {
+                // 삭제 요청인데 책이 없으면 저장하지 않은 책이므로 예외 처리
+                throw new BusinessException(BOOK_NOT_SAVED_DB_CANNOT_DELETE);
+            }
+
+            // 저장 요청이면 네이버 API로 책 정보 조회 후 저장
+            NaverDetailBookParseResult naverResult = bookApiQueryPort.findDetailBookByKeyword(isbn);
+            Book newBook = Book.withoutId(
+                    naverResult.title(),
+                    naverResult.isbn(),
+                    naverResult.author(),
+                    false,
+                    naverResult.publisher(),
+                    naverResult.imageUrl(),
+                    null,
+                    naverResult.description());
+
+            Long newBookId = bookCommandPort.save(newBook);
+            book = newBook.withId(newBookId);
+        }
+
+        // 유저가 저장한 책 목록 조회
         SavedBooks savedBooks = savedQueryPort.findByUserId(userId);
 
         if (isSave) {
-            // 저장 요청일 떄는 사용자가 이미 저장하지 않았던 책이면 저장
+            // 저장 요청 시 이미 저장되어 있으면 예외 발생
             savedBooks.validateNotAlreadySaved(book);
             savedCommandPort.saveBook(userId, book.getId());
         } else {
-            // 삭제 요청일 때는 사용자가 저장한 책이면 삭제
+            // 삭제 요청 시 저장되어 있지 않으면 예외 발생
             savedBooks.validateCanDelete(book);
             savedCommandPort.deleteBook(userId, book.getId());
         }
+
         return BookIsSavedResult.of(isbn, isSave);
     }
+
 }
