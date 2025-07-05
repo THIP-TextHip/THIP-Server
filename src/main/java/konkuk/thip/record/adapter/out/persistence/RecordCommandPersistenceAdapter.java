7c70201cb3782634d7a61c4f28555f7a00e34cd4
janghyunjun <buzz03312@naver.com>
2025-07-06 01:36:57 +0900

    [feat] 기록 생성 관련 persistence 선언 (#45)

--- src/main/java/konkuk/thip/record/adapter/out/persistence/RecordCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/record/adapter/out/persistence/RecordCommandPersistenceAdapter.java
@@ -1,15 +1,41 @@
 package konkuk.thip.record.adapter.out.persistence;
 
+import konkuk.thip.common.exception.EntityNotFoundException;
 import konkuk.thip.record.adapter.out.mapper.RecordMapper;
 import konkuk.thip.record.application.port.out.RecordCommandPort;
+import konkuk.thip.record.domain.Record;
+import konkuk.thip.room.adapter.out.jpa.RoomJpaEntity;
+import konkuk.thip.room.adapter.out.persistence.RoomJpaRepository;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static konkuk.thip.common.exception.code.ErrorCode.ROOM_NOT_FOUND;
+import static konkuk.thip.common.exception.code.ErrorCode.USER_NOT_FOUND;
 
 @Repository
 @RequiredArgsConstructor
 public class RecordCommandPersistenceAdapter implements RecordCommandPort {
 
     private final RecordJpaRepository recordJpaRepository;
+    private final UserJpaRepository userJpaRepository;
+    private final RoomJpaRepository roomJpaRepository;
     private final RecordMapper recordMapper;
+
+    @Override
+    public Long saveRecord(Record record) {
+        UserJpaEntity userJpaEntity = userJpaRepository.findById(record.getCreatorId()).orElseThrow(
+                () -> new EntityNotFoundException(USER_NOT_FOUND)
+        );
+
+        RoomJpaEntity roomJpaEntity = roomJpaRepository.findById(record.getRoomId()).orElseThrow(
+                () -> new EntityNotFoundException(ROOM_NOT_FOUND)
+        );
+
+        return recordJpaRepository.save(
+                recordMapper.toJpaEntity(record, userJpaEntity, roomJpaEntity)
+        ).getPostId();
+    }
 
 }
