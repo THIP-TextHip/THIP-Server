41fdad15f3e7b7b435381a8e5c109d0b560baa3f
seongjunnoh <joon1754@naver.com>
2025-07-06 19:07:41 +0900

    [feat] value 값으로 Category 엔티티 조회하는 기능 구현 (#47)

--- src/main/java/konkuk/thip/room/adapter/out/persistence/CategoryJpaRepository.java
+++ src/main/java/konkuk/thip/room/adapter/out/persistence/CategoryJpaRepository.java
@@ -3,5 +3,9 @@ package konkuk.thip.room.adapter.out.persistence;
 import konkuk.thip.room.adapter.out.jpa.CategoryJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryJpaRepository extends JpaRepository<CategoryJpaEntity, Long> {
+
+    Optional<CategoryJpaEntity> findByValue(String value);
 }
