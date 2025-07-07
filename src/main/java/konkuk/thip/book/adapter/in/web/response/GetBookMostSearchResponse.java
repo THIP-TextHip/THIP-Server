9acf60f811d29c882ff1c7766b06797a6c502f18
강희진 <kanghuijin12@gmail.com>
2025-07-08 04:53:14 +0900

    [refactor] 이너클래스 위치 수정 (#48)

--- src/main/java/konkuk/thip/book/adapter/in/web/response/GetBookMostSearchResponse.java
+++ src/main/java/konkuk/thip/book/adapter/in/web/response/GetBookMostSearchResponse.java
@@ -6,16 +6,8 @@ import lombok.Builder;
 import java.util.List;
 
 public record GetBookMostSearchResponse(
-        List<BookRankInfo> bookList
+        List<BookMostSearchResult.BookRankInfo> bookList
 ) {
-    @Builder
-    public record BookRankInfo(
-            int rank,
-            String title,
-            String imageUrl,
-            String isbn
-    ) {}
-
     public static GetBookMostSearchResponse of(BookMostSearchResult bookMostSearchResult) {
         return new GetBookMostSearchResponse(bookMostSearchResult.bookList());
     }
--- src/main/java/konkuk/thip/book/application/port/in/dto/BookMostSearchResult.java
+++ src/main/java/konkuk/thip/book/application/port/in/dto/BookMostSearchResult.java
@@ -1,13 +1,22 @@
 package konkuk.thip.book.application.port.in.dto;
 
 import konkuk.thip.book.adapter.in.web.response.GetBookMostSearchResponse;
+import lombok.Builder;
 
 import java.util.List;
 
 public record BookMostSearchResult(
-        List<GetBookMostSearchResponse.BookRankInfo> bookList
+        List<BookRankInfo> bookList
 ) {
-    public static BookMostSearchResult of(List<GetBookMostSearchResponse.BookRankInfo> bookList) {
+    @Builder
+    public record BookRankInfo(
+            int rank,
+            String title,
+            String imageUrl,
+            String isbn
+    ) {}
+
+    public static BookMostSearchResult of(List<BookRankInfo> bookList) {
         return new BookMostSearchResult(bookList);
     }
 }
