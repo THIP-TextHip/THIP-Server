671ca86f2278e9c317d5fd3fd1fa88e249cd129a
강희진 <kanghuijin12@gmail.com>
2025-07-08 04:53:36 +0900

    [refactor] 안쓰는 함수 정리 (#48)

--- src/main/java/konkuk/thip/book/application/port/out/BookQueryPort.java
+++ src/main/java/konkuk/thip/book/application/port/out/BookQueryPort.java
@@ -1,9 +1,4 @@
 package konkuk.thip.book.application.port.out;
 
-import konkuk.thip.book.domain.Book;
-
-import java.util.List;
-
 public interface BookQueryPort {
-    List<Book> findByIsbnIn(List<String> isbnList);
 }
