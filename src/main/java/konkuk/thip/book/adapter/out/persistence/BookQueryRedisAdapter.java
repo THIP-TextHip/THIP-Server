80eb90e88d3be1e6bf66396d6570253478d05a0b
강희진 <kanghuijin12@gmail.com>
2025-07-08 04:53:52 +0900

    [refactor] 서비스 로직 수정 (#48)

--- src/main/java/konkuk/thip/book/adapter/out/persistence/BookQueryRedisAdapter.java
+++ src/main/java/konkuk/thip/book/adapter/out/persistence/BookQueryRedisAdapter.java
@@ -1,6 +1,12 @@
 package konkuk.thip.book.adapter.out.persistence;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import konkuk.thip.book.application.port.in.dto.BookMostSearchResult;
 import konkuk.thip.book.application.port.out.BookRedisQueryPort;
+import konkuk.thip.common.exception.ExternalApiException;
+import konkuk.thip.common.exception.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -29,6 +35,10 @@ public class BookQueryRedisAdapter implements BookRedisQueryPort {
     @Value("${app.redis.search-rank-prefix}")
     private String searchRankPrefix;
 
+    @Value("${app.redis.search-rank-detail-prefix}")
+    private String searchRankDetailPrefix;
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public List<Map.Entry<String, Double>> getBookSearchRank(LocalDate date, int topN) {
@@ -52,6 +62,22 @@ public class BookQueryRedisAdapter implements BookRedisQueryPort {
                 .collect(Collectors.toList());
     }
 
+    @Override
+    public List<BookMostSearchResult.BookRankInfo> getYesterdayBookRankInfos(LocalDate date) {
+        String redisKey = searchRankDetailPrefix + date.format(DAILY_KEY_FORMATTER);
+        String json = redisTemplate.opsForValue().get(redisKey);
 
+        if (json == null || json.isBlank()) {
+            return List.of();
+        }
+        try {
+            return objectMapper.readValue(
+                    json,
+                    new TypeReference<List<BookMostSearchResult.BookRankInfo>>() {}
+            );
+        } catch (JsonProcessingException e) {
+            throw new ExternalApiException(ErrorCode.JSON_PROCESSING_ERROR);
+        }
+    }
 
 }
--- src/main/java/konkuk/thip/book/application/port/out/BookRedisQueryPort.java
+++ src/main/java/konkuk/thip/book/application/port/out/BookRedisQueryPort.java
@@ -1,5 +1,7 @@
 package konkuk.thip.book.application.port.out;
 
+import konkuk.thip.book.application.port.in.dto.BookMostSearchResult;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -7,4 +9,5 @@ import java.util.Map;
 public interface BookRedisQueryPort {
     List<Map.Entry<String, Double>> getBookSearchRank(LocalDate date, int topN);
     List<Map.Entry<String, Double>> getBookSearchCountTopN(LocalDate date, int topN);
+    List<BookMostSearchResult.BookRankInfo> getYesterdayBookRankInfos(LocalDate date);
 }
