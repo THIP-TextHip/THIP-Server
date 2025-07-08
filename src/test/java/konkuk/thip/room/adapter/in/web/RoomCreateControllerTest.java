11dffd2cbaf935115c4237f5a946750c945331a0
seongjunnoh <joon1754@naver.com>
2025-07-08 17:26:21 +0900

    [fix] test yml을 활용해 테스트 실행하도록 명시 (#47)

--- src/test/java/konkuk/thip/room/adapter/in/web/RoomCreateControllerTest.java
+++ src/test/java/konkuk/thip/room/adapter/in/web/RoomCreateControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @DisplayName("방 생성 api controller 단위 테스트")
 class RoomCreateControllerTest {
