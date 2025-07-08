5cd9e04111e57842aa8c7d7f602f5a317969672b
강희진 <kanghuijin12@gmail.com>
2025-07-08 11:30:38 +0900

    [refactor] 레디스 커맨드/쿼리 어댑터 하나로 합침 (#48)

--- src/main/java/konkuk/thip/book/adapter/out/persistence/BookRedisAdapter.java
+++ src/main/java/konkuk/thip/book/adapter/out/persistence/BookRedisAdapter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import konkuk.thip.book.application.port.in.dto.BookMostSearchResult;
+import konkuk.thip.book.application.port.out.BookRedisCommandPort;
 import konkuk.thip.book.application.port.out.BookRedisQueryPort;
 import konkuk.thip.common.exception.ExternalApiException;
 import konkuk.thip.common.exception.code.ErrorCode;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
@@ -21,9 +23,11 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static konkuk.thip.common.exception.code.ErrorCode.JSON_PROCESSING_ERROR;
+
 @Component
 @RequiredArgsConstructor
-public class BookQueryRedisAdapter implements BookRedisQueryPort {
+public class BookRedisAdapter implements BookRedisQueryPort, BookRedisCommandPort {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final DateTimeFormatter DAILY_KEY_FORMATTER =
@@ -78,6 +82,54 @@ public class BookQueryRedisAdapter implements BookRedisQueryPort {
         } catch (JsonProcessingException e) {
             throw new ExternalApiException(ErrorCode.JSON_PROCESSING_ERROR);
         }
+    }
+
+    @Override
+    public void incrementBookSearchCount(String isbn, LocalDate date) {
+        String redisKey = makeRedisKey(searchCountPrefix, date);
+        redisTemplate.opsForZSet().incrementScore(redisKey, isbn, 1.0);
+    }
+
+    @Override
+    public void saveBookSearchRank(List<String> isbns, List<Double> scores, LocalDate date) {
+        String redisKey = makeRedisKey(searchRankPrefix, date);
+        for (int i = 0; i < isbns.size(); i++) {
+            redisTemplate.opsForZSet().add(redisKey, isbns.get(i), scores.get(i));
+        }
+        redisTemplate.expire(redisKey, Duration.ofDays(7));
+    }
+
+    @Override
+    public void saveBookSearchRankDetail(List<BookMostSearchResult.BookRankInfo> bookRankDetails, LocalDate date) {
+        String redisKey = makeRedisKey(searchRankDetailPrefix, date);
+        String detailJson;
+        try {
+            detailJson = objectMapper.writeValueAsString(bookRankDetails);
+        } catch (JsonProcessingException e) {
+            throw new ExternalApiException(JSON_PROCESSING_ERROR);
+        }
+        redisTemplate.opsForValue().set(redisKey, detailJson);
+    }
+
+    @Override
+    public void deleteBookSearchRank(LocalDate date) {
+        deleteZSetKey(searchRankPrefix, date);
+    }
+
+    @Override
+    public void deleteBookSearchCount(LocalDate date) { deleteZSetKey(searchCountPrefix, date); }
+
+    @Override
+    public void deleteBookSearchRankDetail(LocalDate date) { deleteZSetKey(searchRankDetailPrefix, date); }
+
+    private void deleteZSetKey(String prefix, LocalDate date) {
+        String redisKey = makeRedisKey(prefix, date);
+        redisTemplate.delete(redisKey);
+    }
+
+    private String makeRedisKey(String prefix, LocalDate date) {
+        String dateStr = date.format(DAILY_KEY_FORMATTER);
+        return prefix + dateStr;
     }
 
 }
