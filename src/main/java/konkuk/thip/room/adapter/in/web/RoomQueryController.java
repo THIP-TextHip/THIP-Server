eaed72a2dc3f736178c8d2114d044ef1b44a6eb4
강희진 <kanghuijin12@gmail.com>
2025-07-09 00:03:12 +0900

    [feat] 비공개 방 비밀번호 입력 검증 컨트롤러 작성 (#54)

--- src/main/java/konkuk/thip/room/adapter/in/web/RoomQueryController.java
+++ src/main/java/konkuk/thip/room/adapter/in/web/RoomQueryController.java
@@ -1,10 +1,26 @@
 package konkuk.thip.room.adapter.in.web;
 
+import jakarta.validation.Valid;
+import konkuk.thip.common.dto.BaseResponse;
+import konkuk.thip.common.security.annotation.UserId;
+import konkuk.thip.room.adapter.in.web.request.RoomVerifyPasswordRequest;
+import konkuk.thip.room.application.port.in.RoomVerifyPasswordUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class RoomQueryController {
+
+    private final RoomVerifyPasswordUseCase roomVerifyPasswordUseCase;
+
+    //비공개 방 비밀번호 입력 검증
+    @PostMapping("/rooms/{roomId}/password")
+    public BaseResponse<Void> verifyRoomPassword(@UserId final Long userId,
+                                                 @PathVariable("roomId") final Long roomId,
+                                                 @Valid @RequestBody final RoomVerifyPasswordRequest roomVerifyPasswordRequest
+                                                 ) {
+        return BaseResponse.ok(roomVerifyPasswordUseCase.verifyRoomPassword(roomVerifyPasswordRequest.toQuery(userId,roomId)));
+    }
 
 }
