6677793a66a8a1e44b3cb1517a5d826c0468eb54
강희진 <kanghuijin12@gmail.com>
2025-07-09 00:03:46 +0900

    [feat] request dto 작성 (#54)

---
+++ src/main/java/konkuk/thip/room/adapter/in/web/request/RoomVerifyPasswordRequest.java
@@ -0,0 +1,19 @@
+package konkuk.thip.room.adapter.in.web.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import konkuk.thip.room.application.port.in.dto.RoomVerifyPasswordQuery;
+
+public record RoomVerifyPasswordRequest(
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Pattern(regexp = "\\d{4}", message = "비밀번호는 숫자 4자리여야 합니다.")
+        String password
+) {
+    public RoomVerifyPasswordQuery toQuery(Long userId,Long roomId) {
+        return new RoomVerifyPasswordQuery(
+                userId,
+                roomId,
+                password);
+    }
+}
