8ed9260216072d56c40da9ef42ad1636550657b0
강희진 <kanghuijin12@gmail.com>
2025-07-09 00:04:03 +0900

    [feat] 서비스 로직 작성 (#54)

---
+++ src/main/java/konkuk/thip/room/application/service/RoomVerifyPasswordService.java
@@ -0,0 +1,33 @@
+package konkuk.thip.room.application.service;
+
+import konkuk.thip.room.application.port.in.RoomVerifyPasswordUseCase;
+import konkuk.thip.room.application.port.in.dto.RoomVerifyPasswordQuery;
+import konkuk.thip.room.application.port.out.RoomCommandPort;
+import konkuk.thip.room.domain.Room;
+import konkuk.thip.user.application.port.out.UserCommandPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RoomVerifyPasswordService implements RoomVerifyPasswordUseCase {
+
+    private final UserCommandPort userCommandPort;
+    private final RoomCommandPort roomCommandPort;
+
+    @Override
+    public Void verifyRoomPassword(RoomVerifyPasswordQuery query) {
+
+        //유저 검증
+        userCommandPort.findById(query.userId());
+
+        //방 검증
+        Room room = roomCommandPort.findById(query.roomId());
+
+        //도메인에서 비밀번호 검증 로직 수행
+        room.verifyPassword(query.password());
+        return null;
+    }
+}
