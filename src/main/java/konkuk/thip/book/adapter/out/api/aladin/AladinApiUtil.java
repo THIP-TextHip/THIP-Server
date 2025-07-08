77852a848caa56432220e8eac27051dfe4067c8e
seongjunnoh <joon1754@naver.com>
2025-07-08 15:42:17 +0900

    [refactor] AladinApiUtil 에서 사용하는 파라미터 enum 으로 분리 (#47)

--- src/main/java/konkuk/thip/book/adapter/out/api/aladin/AladinApiUtil.java
+++ src/main/java/konkuk/thip/book/adapter/out/api/aladin/AladinApiUtil.java
@@ -11,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
+import static konkuk.thip.book.adapter.out.api.aladin.AladinApiParam.*;
 import static konkuk.thip.common.exception.code.ErrorCode.BOOK_ALADIN_API_ISBN_NOT_FOUND;
 import static konkuk.thip.common.exception.code.ErrorCode.BOOK_ALADIN_API_PARSING_ERROR;
 
@@ -25,17 +26,18 @@ public class AladinApiUtil {
     @Value("${aladin.ttbKey}")
     private String ttbKey;
 
-    private static final String BASE_URL = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?";
-    private static final String ITEM_ID_TYPE = "ISBN";
-    private static final String OUTPUT = "js";
-    private static final String API_VERSION = "20131101";
-    private static final String SUB_INFO_PARSING_KEY = "subInfo";
-    private static final String PAGE_COUNT_PARSING_KEY = "itemPage";
+    @Value("${aladin.baseUrl}")
+    private String baseUrl;
+
 
     private String buildLookupUrl(String isbn) {
         return String.format(
-                BASE_URL + "ttbkey=%s&itemIdType=%s&itemId=%s&output=%s&Version=%s",
-                ttbKey, ITEM_ID_TYPE, isbn, OUTPUT, API_VERSION
+                baseUrl + "ttbkey=%s&itemIdType=%s&itemId=%s&output=%s&Version=%s",
+                ttbKey,
+                ITEM_ID_TYPE.getValue(),
+                isbn,
+                OUTPUT.getValue(),
+                API_VERSION.getValue()
         );
     }
 
@@ -55,9 +57,9 @@ public class AladinApiUtil {
                 throw new BusinessException(BOOK_ALADIN_API_ISBN_NOT_FOUND);
             }
 
-            JsonNode subInfo = items.get(0).path(SUB_INFO_PARSING_KEY);
+            JsonNode subInfo = items.get(0).path(SUB_INFO_PARSING_KEY.getValue());
 
-            return subInfo.path(PAGE_COUNT_PARSING_KEY).asInt();
+            return subInfo.path(PAGE_COUNT_PARSING_KEY.getValue()).asInt();
         } catch (IOException e) {
             throw new BusinessException(BOOK_ALADIN_API_PARSING_ERROR);
         }
