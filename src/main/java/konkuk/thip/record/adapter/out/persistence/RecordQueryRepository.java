71c37a910e9baf57d635a989e9454680f352648e
janghyunjun <buzz03312@naver.com>
2025-07-09 15:08:11 +0900

    [feat] Room으로 Record를 조회하는 쿼리 저으이 (#52)

--- src/main/java/konkuk/thip/record/adapter/out/persistence/RecordQueryRepository.java
+++ src/main/java/konkuk/thip/record/adapter/out/persistence/RecordQueryRepository.java
@@ -1,11 +1,11 @@
 package konkuk.thip.record.adapter.out.persistence;
 
-import konkuk.thip.post.adapter.out.jpa.PostJpaEntity;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import konkuk.thip.record.adapter.out.jpa.RecordJpaEntity;
+
+import java.util.List;
 
 public interface RecordQueryRepository {
 
-    Slice<PostJpaEntity> findPostsByRoom(Long roomId, String type, String sort, Integer pageStart, Integer pageEnd, Long userId, Pageable pageable);
+    List<RecordJpaEntity> findRecordsByRoom(Long roomId, String type, Integer pageStart, Integer pageEnd, Long userId);
 
 }
--- src/main/java/konkuk/thip/record/adapter/out/persistence/RecordQueryRepositoryImpl.java
+++ src/main/java/konkuk/thip/record/adapter/out/persistence/RecordQueryRepositoryImpl.java
@@ -2,19 +2,13 @@ package konkuk.thip.record.adapter.out.persistence;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import konkuk.thip.comment.adapter.out.jpa.QCommentJpaEntity;
-import konkuk.thip.post.adapter.out.jpa.PostJpaEntity;
-import konkuk.thip.post.adapter.out.jpa.QPostJpaEntity;
-import konkuk.thip.post.adapter.out.jpa.QPostLikeJpaEntity;
 import konkuk.thip.record.adapter.out.jpa.QRecordJpaEntity;
-import konkuk.thip.vote.adapter.out.jpa.QVoteJpaEntity;
+import konkuk.thip.record.adapter.out.jpa.RecordJpaEntity;
+import konkuk.thip.user.adapter.out.jpa.QUserJpaEntity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-import static com.querydsl.core.types.dsl.Expressions.numberTemplate;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,72 +17,30 @@ public class RecordQueryRepositoryImpl implements RecordQueryRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Slice<PostJpaEntity> findPostsByRoom(Long roomId, String type, String sort, Integer pageStart, Integer pageEnd, Long userId, Pageable pageable) {
-        QPostJpaEntity post = QPostJpaEntity.postJpaEntity;
+    public List<RecordJpaEntity> findRecordsByRoom(Long roomId, String type, Integer pageStart, Integer pageEnd, Long userId) {
         QRecordJpaEntity record = QRecordJpaEntity.recordJpaEntity;
-        QVoteJpaEntity vote = QVoteJpaEntity.voteJpaEntity;
-        QCommentJpaEntity comment = QCommentJpaEntity.commentJpaEntity;
-        QPostLikeJpaEntity postLike = QPostLikeJpaEntity.postLikeJpaEntity;
+        QUserJpaEntity user = QUserJpaEntity.userJpaEntity;
 
-        var query = jpaQueryFactory
-                .selectFrom(post)
-                .leftJoin(record).on(record.postId.eq(post.postId))
-                .leftJoin(vote).on(vote.postId.eq(post.postId))
+        return jpaQueryFactory
+                .select(record)
+                .from(record)
+                .leftJoin(record.userJpaEntity, user).fetchJoin()
                 .where(
-                        filterByRoom(roomId, record, vote),
-                        filterByType(type, post, userId),
-                        filterByPage(pageStart, pageEnd, record, vote)
-                );
-
-        if ("like".equalsIgnoreCase(sort)) {
-            query.leftJoin(postLike).on(postLike.postJpaEntity.postId.eq(post.postId));
-        } else if ("comment".equalsIgnoreCase(sort)) {
-            query.leftJoin(comment).on(comment.postJpaEntity.postId.eq(post.postId));
-        }
-
-        query.groupBy(post.postId);
-
-        if ("like".equalsIgnoreCase(sort)) {
-            query.orderBy(numberTemplate(Long.class, "count({0})", postLike.likeId).desc());
-        } else if ("comment".equalsIgnoreCase(sort)) {
-            query.orderBy(numberTemplate(Long.class, "count({0})", comment.commentId).desc());
-        } else {
-            query.orderBy(post.createdAt.desc());
-        }
-
-        int pageSize = pageable.getPageSize();
-        int offset = (int) pageable.getOffset();
-
-        List<PostJpaEntity> content = query
-                .offset(offset)
-                .limit(pageSize + 1) // hasNext 판단을 위한 초과 조회
+                        record.roomJpaEntity.roomId.eq(roomId),
+                        filterByType(type, record, userId),
+                        (startEndNull(pageStart, pageEnd) ? record.isOverview.isTrue() : record.page.between(pageStart, pageEnd))
+                )
                 .fetch();
-
-        boolean hasNext = content.size() > pageSize;
-        if (hasNext) {
-            content.remove(pageSize);
-        }
-
-        return new SliceImpl<>(content, pageable, hasNext);
     }
 
-    private BooleanExpression filterByRoom(Long roomId, QRecordJpaEntity record, QVoteJpaEntity vote) {
-        return record.roomJpaEntity.roomId.eq(roomId)
-                .or(vote.roomJpaEntity.roomId.eq(roomId));
+    private boolean startEndNull(Integer start, Integer end) {
+        return start == null || end == null;
     }
 
-    private BooleanExpression filterByType(String type, QPostJpaEntity post, Long userId) {
+    private BooleanExpression filterByType(String type, QRecordJpaEntity post, Long userId) {
         if ("mine".equalsIgnoreCase(type)) {
             return post.userJpaEntity.userId.eq(userId);
         }
         return null;
-    }
-
-    private BooleanExpression filterByPage(Integer start, Integer end, QRecordJpaEntity record, QVoteJpaEntity vote) {
-        if (start == null || end == null) {
-            return record.isOverview.isTrue().or(vote.isOverview.isTrue());
-        }
-        return record.page.between(start, end)
-                .or(vote.page.between(start, end));
     }
 }
