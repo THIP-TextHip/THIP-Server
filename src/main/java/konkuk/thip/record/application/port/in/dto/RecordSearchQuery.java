8157217ca7c2bc83831cfbd8a83d84836c8a3a69
janghyunjun <buzz03312@naver.com>
2025-07-09 15:08:35 +0900

    [feat] 쿼리 조회용 dto (#52)

--- src/main/java/konkuk/thip/record/application/port/in/dto/RecordSearchQuery.java
+++ src/main/java/konkuk/thip/record/application/port/in/dto/RecordSearchQuery.java
@@ -3,5 +3,12 @@ package konkuk.thip.record.application.port.in.dto;
 import lombok.Builder;
 
 @Builder
-public record RecordSearchQuery() {
+public record RecordSearchQuery(
+        Long roomId,
+        String type,
+        String sort,
+        Integer pageStart,
+        Integer pageEnd,
+        Integer pageNum,
+        Long userId) {
 }
