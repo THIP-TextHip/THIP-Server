33d392dc46cb42a18beac0513e6b5a3b5d63a51f
seongjunnoh <joon1754@naver.com>
2025-07-09 20:27:55 +0900

    [feat] 방 검색 api use case 개발 (#55)

--- src/main/java/konkuk/thip/room/application/port/out/RoomQueryPort.java
+++ src/main/java/konkuk/thip/room/application/port/out/RoomQueryPort.java
@@ -1,7 +1,13 @@
 package konkuk.thip.room.application.port.out;
 
+import konkuk.thip.room.adapter.in.web.response.RoomSearchResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.time.LocalDate;
 
 public interface RoomQueryPort {
     int countRecruitingRoomsByBookAndStartDateAfter(Long bookId, LocalDate currentDate);
+
+    Page<RoomSearchResponse.RoomSearchResult> searchRoom(String keyword, String category, Pageable pageable);
 }
