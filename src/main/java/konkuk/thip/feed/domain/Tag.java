168716ee3cf47ccfb8c8aa55352488016d70953a
seongjunnoh <joon1754@naver.com>
2025-07-10 15:22:20 +0900

    [refactor] tag에서 category 정보 제거 (#53)

--- src/main/java/konkuk/thip/feed/domain/Tag.java
+++ src/main/java/konkuk/thip/feed/domain/Tag.java
@@ -3,34 +3,30 @@ package konkuk.thip.feed.domain;
 import konkuk.thip.common.exception.InvalidStateException;
 import konkuk.thip.room.domain.Category;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import static konkuk.thip.common.exception.code.ErrorCode.TAG_NAME_NOT_MATCH;
 
 @Getter
+@RequiredArgsConstructor
 public enum Tag {
-    BOOK_RECOMMEND("책추천", Category.LITERATURE),
-    TODAYS_BOOK("오늘의책", Category.LITERATURE),
-    READING_LOG("독서기록", Category.LITERATURE),
-    BOOK_REVIEW("책리뷰", Category.LITERATURE),
-    QUOTE("책속한줄", Category.LITERATURE),
-    BOOK_REPORT("독후감", Category.LITERATURE),
-    LIFE_BOOK("내인생책", Category.LITERATURE),
-    RE_READ("다시읽고싶은책", Category.LITERATURE),
-    BOOK_TALK("북토크", Category.LITERATURE),
-    BOOKSTAGRAM("책스타그램", Category.LITERATURE),
-    NOVEL("소설추천", Category.LITERATURE),
-    SELF_IMPROVEMENT("자기계발서", Category.HUMANITY),
-    PHILOSOPHY("인문학책", Category.HUMANITY),
-    SCIENCE("과학책", Category.SCIENCE_IT),
-    ECONOMY("경제책", Category.SOCIAL_SCIENCE);
+    BOOK_RECOMMEND("책추천"),
+    TODAYS_BOOK("오늘의책"),
+    READING_LOG("독서기록"),
+    BOOK_REVIEW("책리뷰"),
+    QUOTE("책속한줄"),
+    BOOK_REPORT("독후감"),
+    LIFE_BOOK("내인생책"),
+    RE_READ("다시읽고싶은책"),
+    BOOK_TALK("북토크"),
+    BOOKSTAGRAM("책스타그램"),
+    NOVEL("소설추천"),
+    SELF_IMPROVEMENT("자기계발서"),
+    PHILOSOPHY("인문학책"),
+    SCIENCE("과학책"),
+    ECONOMY("경제책");
 
     private final String value;
-    private final Category category;
-
-    Tag(String value, Category category) {
-        this.value = value;
-        this.category = category;
-    }
 
     public static Tag from(String value) {
         for (Tag tagName : Tag.values()) {
