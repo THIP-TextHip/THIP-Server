c02de47622b176b9232b364107b8ec2b4859b482
janghyunjun <buzz03312@naver.com>
2025-07-10 13:31:54 +0900

    [refactor] TagName과 Tag를 병합 (#53)

--- src/main/java/konkuk/thip/feed/domain/Tag.java
+++ src/main/java/konkuk/thip/feed/domain/Tag.java
@@ -1,18 +1,40 @@
 package konkuk.thip.feed.domain;
 
-import lombok.Builder;
+import konkuk.thip.common.exception.InvalidStateException;
 import lombok.Getter;
 
+import static konkuk.thip.common.exception.code.ErrorCode.TAG_NAME_NOT_MATCH;
+
 @Getter
-@Builder
-public class Tag {
+public enum Tag {
+    BOOK_RECOMMEND("책추천"),
+    TODAYS_BOOK("오늘의책"),
+    READING_LOG("독서기록"),
+    BOOK_REVIEW("책리뷰"),
+    QUOTE("책속한줄"),
+    BOOK_REPORT("독후감"),
+    LIFE_BOOK("내인생책"),
+    RE_READ("다시읽고싶은책"),
+    BOOK_TALK("북토크"),
+    BOOKSTAGRAM("책스타그램"),
+    NOVEL("소설추천"),
+    SELF_IMPROVEMENT("자기계발서"),
+    PHILOSOPHY("인문학책"),
+    SCIENCE("과학책"),
+    ECONOMY("경제책");
 
-    private TagName value;
+    private final String tag;
 
-    public static Tag from(String tagName) {
-        return Tag.builder()
-                .value(TagName.from(tagName))
-                .build();
+    Tag(String tag) {
+        this.tag = tag;
     }
 
+    public static Tag from(String tag) {
+        for (Tag tagName : Tag.values()) {
+            if (tagName.tag.equalsIgnoreCase(tag)) {
+                return tagName;
+            }
+        }
+        throw new InvalidStateException(TAG_NAME_NOT_MATCH);
+    }
 }
