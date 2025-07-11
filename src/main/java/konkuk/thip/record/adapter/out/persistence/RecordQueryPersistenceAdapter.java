13dffdfd801b98f626d417c926a4541bfd6630e8
janghyunjun <buzz03312@naver.com>
2025-07-12 03:46:39 +0900

    [refactor] 필요없는 DI 삭제 (#53)

--- src/main/java/konkuk/thip/record/adapter/out/persistence/RecordQueryPersistenceAdapter.java
+++ src/main/java/konkuk/thip/record/adapter/out/persistence/RecordQueryPersistenceAdapter.java
@@ -1,10 +1,7 @@
 package konkuk.thip.record.adapter.out.persistence;
 
 import konkuk.thip.record.adapter.in.web.response.RecordSearchResponse;
-import konkuk.thip.record.adapter.out.mapper.RecordMapper;
 import konkuk.thip.record.application.port.out.RecordQueryPort;
-import konkuk.thip.vote.adapter.out.mapper.VoteMapper;
-import konkuk.thip.vote.adapter.out.persistence.VoteJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,12 +12,6 @@ import org.springframework.stereotype.Repository;
 public class RecordQueryPersistenceAdapter implements RecordQueryPort {
 
     private final RecordJpaRepository recordJpaRepository;
-    private final VoteJpaRepository voteJpaRepository;
-    private final RecordMapper recordMapper;
-    private final VoteMapper voteMapper;
-
-    private static final Integer PAGE_SIZE = 10;
-
 
     @Override
     public Page<RecordSearchResponse.RecordSearchResult> findRecordsByRoom(Long roomId, String type, Integer pageStart, Integer pageEnd, Boolean isOverview, Long userId, Pageable pageable) {
