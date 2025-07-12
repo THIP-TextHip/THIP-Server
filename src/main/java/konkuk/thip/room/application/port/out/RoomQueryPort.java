385043bf55a0119a3e2ff7054a0151a66022a9af
강희진 <kanghuijin12@gmail.com>
2025-07-12 22:41:11 +0900

    [feat] RoomQueryPort.searchHomeJoinedRooms 작성 (#56)

--- src/main/java/konkuk/thip/room/application/port/out/RoomQueryPort.java
+++ src/main/java/konkuk/thip/room/application/port/out/RoomQueryPort.java
@@ -1,5 +1,6 @@
 package konkuk.thip.room.application.port.out;
 
+import konkuk.thip.room.adapter.in.web.response.RoomGetHomeJoinedListResponse;
 import konkuk.thip.room.adapter.in.web.response.RoomSearchResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,6 @@ import java.time.LocalDate;
 
 public interface RoomQueryPort {
     int countRecruitingRoomsByBookAndStartDateAfter(Long bookId, LocalDate currentDate);
-
     Page<RoomSearchResponse.RoomSearchResult> searchRoom(String keyword, String category, Pageable pageable);
+    Page<RoomGetHomeJoinedListResponse.RoomSearchResult> searchHomeJoinedRooms(Long userId, LocalDate today, Pageable pageable);
 }
