234176b068cbb01804d186218a7e867f21f79e5f
seongjunnoh <joon1754@naver.com>
2025-07-12 23:46:48 +0900

    [feat] 모집중인 방 상세조회 api controller 개발 (#64)

--- src/main/java/konkuk/thip/room/adapter/in/web/RoomQueryController.java
+++ src/main/java/konkuk/thip/room/adapter/in/web/RoomQueryController.java
@@ -1,10 +1,13 @@
 package konkuk.thip.room.adapter.in.web;
 
 import konkuk.thip.common.dto.BaseResponse;
+import konkuk.thip.common.security.annotation.UserId;
+import konkuk.thip.room.adapter.in.web.response.RoomRecruitingDetailViewResponse;
 import konkuk.thip.room.adapter.in.web.response.RoomSearchResponse;
 import konkuk.thip.room.application.port.in.RoomSearchUseCase;
 import jakarta.validation.Valid;
 import konkuk.thip.room.adapter.in.web.request.RoomVerifyPasswordRequest;
+import konkuk.thip.room.application.port.in.RoomShowRecruitingDetailViewUseCase;
 import konkuk.thip.room.application.port.in.RoomVerifyPasswordUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 public class RoomQueryController {
 
     private final RoomSearchUseCase roomSearchUseCase;
+    private final RoomVerifyPasswordUseCase roomVerifyPasswordUseCase;
+    private final RoomShowRecruitingDetailViewUseCase roomShowRecruitingDetailViewUseCase;
 
     @GetMapping("/rooms/search")
     public BaseResponse<RoomSearchResponse> searchRooms(
@@ -27,7 +32,6 @@ public class RoomQueryController {
     ) {
         return BaseResponse.ok(roomSearchUseCase.searchRoom(keyword, category, sort, page));
     }
-    private final RoomVerifyPasswordUseCase roomVerifyPasswordUseCase;
 
     //비공개 방 비밀번호 입력 검증
     @PostMapping("/rooms/{roomId}/password")
@@ -37,4 +41,10 @@ public class RoomQueryController {
         return BaseResponse.ok(roomVerifyPasswordUseCase.verifyRoomPassword(roomVerifyPasswordRequest.toQuery(roomId)));
     }
 
+    @GetMapping("/rooms/{roomId}/recruiting")
+    public BaseResponse<RoomRecruitingDetailViewResponse> getRecruitingRoomDetailView(
+            @UserId final Long userId,
+            @PathVariable("roomId") final Long roomId) {
+        return BaseResponse.ok(roomShowRecruitingDetailViewUseCase.getRecruitingRoomDetailView(userId, roomId));
+    }
 }
---
+++ src/main/java/konkuk/thip/room/adapter/in/web/response/RoomRecruitingDetailViewResponse.java
@@ -0,0 +1,33 @@
+package konkuk.thip.room.adapter.in.web.response;
+
+import java.util.List;
+
+public record RoomRecruitingDetailViewResponse(
+        boolean isHost,
+        boolean isJoining,
+        Long roomId,
+        String roomName,
+        String roomImageUrl,
+        boolean isPublic,
+        String progressStartDate,
+        String progressEndDate,
+        String recruitEndDate,
+        String category,
+        String roomDescription,
+        int memberCount,
+        int recruitCount,
+        String isbn,
+        String bookImageUrl,
+        String bookTitle,
+        String authorName,
+        String bookDescription,
+        List<RecommendRoom> recommendRooms
+) {
+    public record RecommendRoom(
+            String roomImageUrl,
+            String roomName,
+            int memberCount,
+            int recruitCount,
+            String recruitEndDate
+    ) {}
+}
