c73a9ee4f6314184d120969ded92fba29804480f
janghyunjun <buzz03312@naver.com>
2025-07-15 02:42:39 +0900

    [feat] 팔로워 리스트 조회를 위해 필요한 인터페이스 정의(#77)

--- src/main/java/konkuk/thip/user/application/port/out/FollowingQueryPort.java
+++ src/main/java/konkuk/thip/user/application/port/out/FollowingQueryPort.java
@@ -1,9 +1,13 @@
 package konkuk.thip.user.application.port.out;
 
+import konkuk.thip.user.adapter.in.web.response.UserFollowersResponse;
+
 import java.util.List;
 import java.util.Map;
 
 public interface FollowingQueryPort {
     Map<Long, Integer> countByFollowingUserIds(List<Long> userIds);
+
+    UserFollowersResponse getFollowersByUserId(Long userId, String cursor, int size);
 }
 
