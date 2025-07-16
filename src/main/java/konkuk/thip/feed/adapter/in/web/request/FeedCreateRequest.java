b20773977b3cfcb156bb809b75314bb1e9c3652a
강희진 <kanghuijin12@gmail.com>
2025-07-16 19:22:55 +0900

    [feat] FeedCreateRequest dto 작성 (#73)

---
+++ src/main/java/konkuk/thip/feed/adapter/in/web/request/FeedCreateRequest.java
@@ -0,0 +1,35 @@
+package konkuk.thip.feed.adapter.in.web.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import konkuk.thip.feed.application.port.in.dto.FeedCreateCommand;
+
+import java.util.List;
+
+public record FeedCreateRequest(
+
+        @NotBlank(message = "ISBN은 필수입니다.")
+        String isbn,
+
+        @NotBlank(message = "콘텐츠 내용은 필수입니다.")
+        String contentBody,
+
+        @NotNull(message = "방 공개 설정 여부는 필수입니다.")
+        Boolean isPublic,
+
+        String category,
+
+        List<String> tagList
+) {
+        public FeedCreateCommand toCommand(List<String> imageUrls,Long userId) {
+                return new FeedCreateCommand(
+                        isbn,
+                        contentBody,
+                        imageUrls,
+                        isPublic,
+                        category,
+                        tagList,
+                        userId
+                );
+        }
+}
