7fc67a36aedc40d7252db9bfafa405b8a4797305
janghyunjun <buzz03312@naver.com>
2025-07-18 15:43:47 +0900

    [feat] 방 참여/취소 api 핸들러 추가 (#84)

--- src/main/java/konkuk/thip/room/adapter/in/web/RoomCommandController.java
+++ src/main/java/konkuk/thip/room/adapter/in/web/RoomCommandController.java
@@ -4,9 +4,12 @@ import jakarta.validation.Valid;
 import konkuk.thip.common.dto.BaseResponse;
 import konkuk.thip.common.security.annotation.UserId;
 import konkuk.thip.room.adapter.in.web.request.RoomCreateRequest;
+import konkuk.thip.room.adapter.in.web.request.RoomJoinRequest;
 import konkuk.thip.room.adapter.in.web.response.RoomCreateResponse;
 import konkuk.thip.room.application.port.in.RoomCreateUseCase;
+import konkuk.thip.room.application.port.in.RoomJoinUsecase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomCommandController {
 
     private final RoomCreateUseCase roomCreateUseCase;
+    private final RoomJoinUsecase roomJoinUsecase;
 
+    /**
+     * 방 생성 요청
+     */
     @PostMapping("/rooms")
     public BaseResponse<RoomCreateResponse> createRoom(@Valid @RequestBody RoomCreateRequest request, @UserId Long userId) {
         return BaseResponse.ok(RoomCreateResponse.of(
                 roomCreateUseCase.createRoom(request.toCommand(), userId)
         ));
+    }
+
+    /**
+     * 방 참여하기/취소하기 요청
+     */
+    @PostMapping("/rooms/{roomId}/join")
+    public BaseResponse joinRoom(@Valid @RequestBody final RoomJoinRequest request,
+                                 @UserId final Long userId,
+                                 @PathVariable final Long roomId) {
+
+        roomJoinUsecase.changeJoinState(request.toCommand(userId, roomId));
+        return BaseResponse.ok(null);
     }
 }
