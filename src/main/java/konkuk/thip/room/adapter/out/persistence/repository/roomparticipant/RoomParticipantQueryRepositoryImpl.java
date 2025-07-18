73bf17478ff6317c92f2ac7d59ae8fec9e1d7bea
janghyunjun <buzz03312@naver.com>
2025-07-18 15:40:32 +0900

    [feat] RoomParticipantJpaRepository 추가 (#84)

---
+++ src/main/java/konkuk/thip/room/adapter/out/persistence/repository/roomparticipant/RoomParticipantQueryRepositoryImpl.java
@@ -0,0 +1,10 @@
+package konkuk.thip.room.adapter.out.persistence.repository.roomparticipant;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RoomParticipantQueryRepositoryImpl implements RoomParticipantQueryRepository{
+
+}
