a3116a4ce35a52f066acd5e2a4188c44ba366753
janghyunjun <buzz03312@naver.com>
2025-07-20 19:00:58 +0900

    [refactor] Optional 반환하도록 수정 (#84)

--- src/main/java/konkuk/thip/room/application/service/RoomJoinService.java
+++ src/main/java/konkuk/thip/room/application/service/RoomJoinService.java
@@ -1,13 +1,11 @@
 package konkuk.thip.room.application.service;
 
-import konkuk.thip.common.exception.EntityNotFoundException;
-import konkuk.thip.common.exception.InvalidStateException;
+import konkuk.thip.common.exception.BusinessException;
 import konkuk.thip.common.exception.code.ErrorCode;
 import konkuk.thip.room.application.port.in.RoomJoinUseCase;
 import konkuk.thip.room.application.port.in.dto.RoomJoinCommand;
 import konkuk.thip.room.application.port.out.RoomCommandPort;
 import konkuk.thip.room.application.port.out.RoomParticipantCommandPort;
-import konkuk.thip.room.application.port.out.RoomParticipantQueryPort;
 import konkuk.thip.room.domain.Room;
 import konkuk.thip.room.domain.RoomJoinType;
 import konkuk.thip.room.domain.RoomParticipant;
@@ -23,7 +21,6 @@ import static konkuk.thip.room.adapter.out.jpa.RoomParticipantRole.MEMBER;
 @RequiredArgsConstructor
 public class RoomJoinService implements RoomJoinUseCase {
 
-    private final RoomParticipantQueryPort roomParticipantQueryPort;
     private final RoomCommandPort roomCommandPort;
     private final RoomParticipantCommandPort roomParticipantCommandPort;
 
@@ -33,12 +30,8 @@ public class RoomJoinService implements RoomJoinUseCase {
         RoomJoinType type = RoomJoinType.from(roomJoinCommand.type());
 
         // 방이 존재하지 않거나 만료된 경우
-        Room room;
-        try {
-            room = roomCommandPort.findById(roomJoinCommand.roomId());
-        } catch (EntityNotFoundException e) {
-            throw new InvalidStateException(ErrorCode.USER_CANNOT_JOIN_OR_CANCEL);
-        }
+        Room room = roomCommandPort.findByIdOptional(roomJoinCommand.roomId())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_CANNOT_JOIN_OR_CANCEL));
 
         validateRoom(room);
 
@@ -55,17 +48,17 @@ public class RoomJoinService implements RoomJoinUseCase {
         }
 
         // 방의 상태 업데이트
-        roomCommandPort.updateMemberCount(room);
+        roomCommandPort.update(room);
     }
 
     private void handleCancel(RoomJoinCommand roomJoinCommand, Optional<RoomParticipant> participantOptional, Optional<RoomParticipant> roomParticipantOptional, Room room) {
         // 참여하지 않은 상태
         RoomParticipant participant = participantOptional.orElseThrow(() ->
-                new InvalidStateException(ErrorCode.USER_NOT_PARTICIPATED_CANNOT_CANCEL)
+                new BusinessException(ErrorCode.USER_NOT_PARTICIPATED_CANNOT_CANCEL)
         );
 
-        // 참여 취소
-        participant.cancelParticipation();
+        // 방장은 참여 취소를 할 수 없음
+        validateCancelable(participant);
 
         roomParticipantCommandPort.deleteByUserIdAndRoomId(roomJoinCommand.userId(), roomJoinCommand.roomId());
 
@@ -76,7 +69,7 @@ public class RoomJoinService implements RoomJoinUseCase {
     private void handleJoin(RoomJoinCommand roomJoinCommand, Optional<RoomParticipant> participantOptional, Room room) {
         // 이미 참여한 상태
         participantOptional.ifPresent(p -> {
-            throw new InvalidStateException(ErrorCode.USER_ALREADY_PARTICIPATE);
+            throw new BusinessException(ErrorCode.USER_ALREADY_PARTICIPATE);
         });
 
         RoomParticipant roomParticipant = RoomParticipant.withoutId(roomJoinCommand.userId(), roomJoinCommand.roomId(), MEMBER.getType());
@@ -89,6 +82,13 @@ public class RoomJoinService implements RoomJoinUseCase {
     private void validateRoom(Room room) {
         room.validateRoomRecruitExpired();
         room.validateRoomExpired();
+    }
+
+    // 방장이 참여 취소를 요청한 경우
+    private void validateCancelable(RoomParticipant roomParticipant) {
+        if (roomParticipant.isHost()) {
+            throw new BusinessException(ErrorCode.HOST_CANNOT_CANCEL);
+        }
     }
 
 
--- src/main/java/konkuk/thip/room/application/service/RoomRecruitCloseService.java
+++ src/main/java/konkuk/thip/room/application/service/RoomRecruitCloseService.java
@@ -1,7 +1,6 @@
 package konkuk.thip.room.application.service;
 
-import konkuk.thip.common.exception.EntityNotFoundException;
-import konkuk.thip.common.exception.InvalidStateException;
+import konkuk.thip.common.exception.BusinessException;
 import konkuk.thip.common.exception.code.ErrorCode;
 import konkuk.thip.room.application.port.in.RoomRecruitCloseUsecase;
 import konkuk.thip.room.application.port.out.RoomCommandPort;
@@ -24,20 +23,26 @@ public class RoomRecruitCloseService implements RoomRecruitCloseUsecase {
     @Transactional
     public void closeRoomRecruit(Long userId, Long roomId) {
         // 1. 방 참여자 조회
-        RoomParticipant roomParticipant;
-        try {
-            roomParticipant = roomParticipantCommandPort.findByUserIdAndRoomId(userId, roomId);
-        } catch (EntityNotFoundException e) {
-            throw new InvalidStateException(ErrorCode.ROOM_RECRUIT_CANNOT_CLOSED, new IllegalArgumentException("사용자가 방에 참여하지 않은 상태입니다."));
-        }
-        // 2. 방 모집 마감
-        roomParticipant.closeRoomRecruit();
+        RoomParticipant roomParticipant = roomParticipantCommandPort.findByUserIdAndRoomIdOptional(userId, roomId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ROOM_RECRUIT_CANNOT_CLOSED,
+                        new IllegalArgumentException("사용자가 방에 참여하지 않은 상태입니다.")));
+
+        // 2. 방 멤버는 방 모집 마감 불가
+        validateCloseable(roomParticipant);
 
         // 3. 모집 마감시 방 시작일을 현재 시간으로 변경
         Room room = roomCommandPort.findById(roomId);
         room.startRoomProgress();
 
         // 4. Room 테이블 업데이트
-        roomCommandPort.updateRoomStartDate(room);
+        roomCommandPort.update(room);
+    }
+
+
+    private void validateCloseable(RoomParticipant roomParticipant) {
+        if (roomParticipant.isMember()) {
+            throw new BusinessException(ErrorCode.ROOM_RECRUIT_CANNOT_CLOSED,
+                    new IllegalArgumentException("사용자는 방의 멤버입니다. 오직 호스트만 모집 마감이 가능합니다."));
+        }
     }
 }
