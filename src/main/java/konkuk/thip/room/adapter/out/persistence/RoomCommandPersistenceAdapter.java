9e34b75131244353c8b6e64a350609e12e77d761
janghyunjun <buzz03312@naver.com>
2025-07-20 18:48:44 +0900

    [refactor] findBy Optional 반환 메서드 추가 + update 통합 (#84)

--- src/main/java/konkuk/thip/room/adapter/out/persistence/RoomCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/room/adapter/out/persistence/RoomCommandPersistenceAdapter.java
@@ -6,13 +6,15 @@ import konkuk.thip.common.exception.EntityNotFoundException;
 import konkuk.thip.room.adapter.out.jpa.CategoryJpaEntity;
 import konkuk.thip.room.adapter.out.jpa.RoomJpaEntity;
 import konkuk.thip.room.adapter.out.mapper.RoomMapper;
-import konkuk.thip.room.adapter.out.persistence.repository.category.CategoryJpaRepository;
 import konkuk.thip.room.adapter.out.persistence.repository.RoomJpaRepository;
+import konkuk.thip.room.adapter.out.persistence.repository.category.CategoryJpaRepository;
 import konkuk.thip.room.application.port.out.RoomCommandPort;
 import konkuk.thip.room.domain.Category;
 import konkuk.thip.room.domain.Room;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 import static konkuk.thip.common.exception.code.ErrorCode.*;
 
@@ -32,6 +34,12 @@ public class RoomCommandPersistenceAdapter implements RoomCommandPort {
                 () -> new EntityNotFoundException(ROOM_NOT_FOUND)
         );
         return roomMapper.toDomainEntity(roomJpaEntity);
+    }
+
+    @Override
+    public Optional<Room> findByIdOptional(Long id) {
+        return roomJpaRepository.findById(id)
+                .map(roomMapper::toDomainEntity);
     }
 
     @Override
@@ -56,24 +64,11 @@ public class RoomCommandPersistenceAdapter implements RoomCommandPort {
     }
 
     @Override
-    public void updateMemberCount(Room room) {
+    public void update(Room room) {
         RoomJpaEntity roomJpaEntity = roomJpaRepository.findById(room.getId()).orElseThrow(
                 () -> new EntityNotFoundException(ROOM_NOT_FOUND)
         );
 
-        roomJpaEntity.updateMemberCount(room.getMemberCount());
-
-        roomJpaRepository.save(roomJpaEntity);
-    }
-
-    @Override
-    public void updateRoomStartDate(Room room) {
-        RoomJpaEntity roomJpaEntity = roomJpaRepository.findById(room.getId()).orElseThrow(
-                () -> new EntityNotFoundException(ROOM_NOT_FOUND)
-        );
-
-        roomJpaEntity.updateStartDate(room.getStartDate());
-
-        roomJpaRepository.save(roomJpaEntity);
+        roomJpaRepository.save(roomJpaEntity.updateFrom(room));
     }
 }
--- src/main/java/konkuk/thip/room/application/port/out/RoomCommandPort.java
+++ src/main/java/konkuk/thip/room/application/port/out/RoomCommandPort.java
@@ -3,15 +3,17 @@ package konkuk.thip.room.application.port.out;
 import konkuk.thip.room.domain.Category;
 import konkuk.thip.room.domain.Room;
 
+import java.util.Optional;
+
 public interface RoomCommandPort {
 
     Room findById(Long id);
+
+    Optional<Room> findByIdOptional(Long id);
 
     Long save(Room room);
 
     Category findCategoryByValue(String value);
 
-    void updateMemberCount(Room room);
-
-    void updateRoomStartDate(Room room);
+    void update(Room room);
 }
