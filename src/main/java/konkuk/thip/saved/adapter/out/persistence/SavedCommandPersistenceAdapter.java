cc24e321ce167582472ad0140598422aa6dbe353
강희진 <kanghuijin12@gmail.com>
2025-07-22 02:25:18 +0900

    [feat] 피드 저장 상태 변경 어댑터 코드 추가  (#86)

--- src/main/java/konkuk/thip/saved/adapter/out/persistence/SavedCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/saved/adapter/out/persistence/SavedCommandPersistenceAdapter.java
@@ -3,7 +3,10 @@ package konkuk.thip.saved.adapter.out.persistence;
 import konkuk.thip.book.adapter.out.jpa.BookJpaEntity;
 import konkuk.thip.book.adapter.out.persistence.repository.BookJpaRepository;
 import konkuk.thip.common.exception.EntityNotFoundException;
+import konkuk.thip.feed.adapter.out.jpa.FeedJpaEntity;
+import konkuk.thip.feed.adapter.out.persistence.repository.FeedJpaRepository;
 import konkuk.thip.saved.adapter.out.jpa.SavedBookJpaEntity;
+import konkuk.thip.saved.adapter.out.jpa.SavedFeedJpaEntity;
 import konkuk.thip.saved.adapter.out.mapper.SavedBookMapper;
 import konkuk.thip.saved.adapter.out.mapper.SavedFeedMapper;
 import konkuk.thip.saved.adapter.out.persistence.repository.SavedBookJpaRepository;
@@ -22,6 +25,7 @@ public class SavedCommandPersistenceAdapter implements SavedCommandPort {
 
     private final UserJpaRepository userJpaRepository;
     private final BookJpaRepository bookJpaRepository;
+    private final FeedJpaRepository feedJpaRepository;
     private final SavedBookJpaRepository savedBookJpaRepository;
     private final SavedFeedJpaRepository savedFeedJpaRepository;
     private final SavedBookMapper savedBookMapper;
@@ -40,10 +44,28 @@ public class SavedCommandPersistenceAdapter implements SavedCommandPort {
         savedBookJpaRepository.save(entity);
     }
 
-
-    //삭제 전략 도입 전
     @Override
     public void deleteBook(Long userId, Long bookId) {
         savedBookJpaRepository.deleteByUserJpaEntity_UserIdAndBookJpaEntity_BookId(userId, bookId);
     }
+
+    @Override
+    public void saveFeed(Long userId, Long feedId) {
+        UserJpaEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
+        FeedJpaEntity feed = feedJpaRepository.findById(feedId)
+                .orElseThrow(() -> new EntityNotFoundException(FEED_NOT_FOUND));
+        SavedFeedJpaEntity entity = SavedFeedJpaEntity.builder()
+                .userJpaEntity(user)
+                .feedJpaEntity(feed)
+                .build();
+        savedFeedJpaRepository.save(entity);
+    }
+
+    @Override
+    public void deleteFeed(Long userId, Long feedId) {
+        savedFeedJpaRepository.deleteByUserJpaEntity_UserIdAndFeedJpaEntity_PostId(userId, feedId);
+    }
+
+
 }
