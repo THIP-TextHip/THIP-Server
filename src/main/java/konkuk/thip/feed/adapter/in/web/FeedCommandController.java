24dced56ecab06ed3ab6c07304fe368cc52c2fbc
강희진 <kanghuijin12@gmail.com>
2025-07-21 13:50:56 +0900

    [feat] FeedCommandController.updateFeed (#86)

--- src/main/java/konkuk/thip/feed/adapter/in/web/FeedCommandController.java
+++ src/main/java/konkuk/thip/feed/adapter/in/web/FeedCommandController.java
@@ -4,13 +4,13 @@ import jakarta.validation.Valid;
 import konkuk.thip.common.dto.BaseResponse;
 import konkuk.thip.common.security.annotation.UserId;
 import konkuk.thip.feed.adapter.in.web.request.FeedCreateRequest;
-import konkuk.thip.feed.adapter.in.web.response.FeedCreateResponse;
+import konkuk.thip.feed.adapter.in.web.request.FeedUpdateRequest;
+import konkuk.thip.feed.adapter.in.web.response.FeedIdResponse;
 import konkuk.thip.feed.application.port.in.FeedCreateUseCase;
+import konkuk.thip.feed.application.port.in.FeedUpdateUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -21,11 +21,23 @@ import java.util.List;
 public class FeedCommandController {
 
     private final FeedCreateUseCase feedCreateUseCase;
+    private final FeedUpdateUseCase feedUpdateUseCase;
 
+    //피드 작성
     @PostMapping("/feeds")
-    public BaseResponse<FeedCreateResponse> createFeed(@RequestPart("request") @Valid final FeedCreateRequest request,
-                                                       @RequestPart(value = "images", required = false) final List<MultipartFile> images,
-                                                       @UserId final Long userId) {
-        return BaseResponse.ok(FeedCreateResponse.of(feedCreateUseCase.createFeed(request.toCommand(userId),images)));
+    public BaseResponse<FeedIdResponse> createFeed(@RequestPart("request") @Valid final FeedCreateRequest request,
+                                                   @RequestPart(value = "images", required = false) final List<MultipartFile> images,
+                                                   @UserId final Long userId) {
+        return BaseResponse.ok(FeedIdResponse.of(feedCreateUseCase.createFeed(request.toCommand(userId),images)));
+    }
+
+    // 피드 수정 (책 빼고 변경가능)
+    @PatchMapping("/feeds/{feedId}")
+    public BaseResponse<FeedIdResponse> updateFeed(@RequestBody @Valid FeedUpdateRequest request,
+                                                   @PathVariable("feedId") final Long feedId,
+                                                   @UserId Long userId) {
+
+        return BaseResponse.ok(FeedIdResponse.of(feedUpdateUseCase.updateFeed(request.toCommand(userId,feedId))));
+
     }
 }
