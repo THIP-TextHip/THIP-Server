280eeaf44e84ee921a9d4d6fae06d4e717a83cb7
강희진 <kanghuijin12@gmail.com>
2025-07-22 02:25:40 +0900

    [feat] SavedFeedJpaRepository 관련 메서드 추가 (#92)

--- src/main/java/konkuk/thip/saved/adapter/out/persistence/repository/SavedFeedJpaRepository.java
+++ src/main/java/konkuk/thip/saved/adapter/out/persistence/repository/SavedFeedJpaRepository.java
@@ -3,5 +3,9 @@ package konkuk.thip.saved.adapter.out.persistence.repository;
 import konkuk.thip.saved.adapter.out.jpa.SavedFeedJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SavedFeedJpaRepository extends JpaRepository<SavedFeedJpaEntity, Long> {
+    void deleteByUserJpaEntity_UserIdAndFeedJpaEntity_PostId(Long userId, Long feedId);
+    List<SavedFeedJpaEntity> findByUserJpaEntity_UserId(Long userId);
 }
