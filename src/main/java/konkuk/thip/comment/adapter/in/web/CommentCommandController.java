e07ccf68fcfb2836dd6e5756174ea484bfdc7685
강희진 <kanghuijin12@gmail.com>
2025-07-24 01:46:57 +0900

    [feat] CommentCommandController.createComment 작성 (#97)

--- src/main/java/konkuk/thip/comment/adapter/in/web/CommentCommandController.java
+++ src/main/java/konkuk/thip/comment/adapter/in/web/CommentCommandController.java
@@ -1,10 +1,31 @@
 package konkuk.thip.comment.adapter.in.web;
 
+import jakarta.validation.Valid;
+import konkuk.thip.comment.adapter.in.web.request.CommentCreateRequest;
+import konkuk.thip.comment.adapter.in.web.response.CommentIdResponse;
+import konkuk.thip.comment.application.port.in.CommentCreateUseCase;
+import konkuk.thip.common.dto.BaseResponse;
+import konkuk.thip.common.security.annotation.UserId;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentCommandController {
+
+    private final CommentCreateUseCase commentCreateUseCase;
+
+
+    /**
+     * 댓글/답글 작성
+     * parentId:{Long},isReplyRequest:true 답글
+     * parentId:null,isReplyRequest:false 댓글
+     */
+    @PostMapping("/comments/{postId}")
+    public BaseResponse<CommentIdResponse> createComment(@RequestBody @Valid final CommentCreateRequest request,
+                                                         @PathVariable("postId") final Long postId,
+                                                         @UserId final Long userId) {
+        return BaseResponse.ok(CommentIdResponse.of(commentCreateUseCase.createComment(request.toCommand(userId,postId))));
+    }
 
 }
