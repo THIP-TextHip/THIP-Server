18fdd7f59367bdcfa3f904925457bfbf23f385e4
강희진 <kanghuijin12@gmail.com>
2025-07-24 01:46:39 +0900

    [feat] Comment 도메인 생성 책임 코드 추가 (#97)

--- src/main/java/konkuk/thip/comment/domain/Comment.java
+++ src/main/java/konkuk/thip/comment/domain/Comment.java
@@ -1,8 +1,13 @@
 package konkuk.thip.comment.domain;
 
 import konkuk.thip.common.entity.BaseDomainEntity;
+import konkuk.thip.common.exception.InvalidStateException;
+import konkuk.thip.common.post.PostType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+
+import static konkuk.thip.common.exception.code.ErrorCode.INVALID_COMMENT_CREATE;
 
 @Getter
 @SuperBuilder
@@ -12,12 +17,83 @@ public class Comment extends BaseDomainEntity {
 
     private String content;
 
-    private int reportCount;
+    @Builder.Default
+    private Integer reportCount = 0;
+
+    @Builder.Default
+    private Integer likeCount = 0;
 
     private Long targetPostId;
 
     private Long creatorId;
 
     private Long parentCommentId;
+
+    private PostType postType;
+
+    public static Comment createComment(String content, Long postId, Long creatorId, String type,
+                                 boolean isReplyRequest, Long parentId, Comment parent) {
+
+        // 댓글/답글 생성 검증
+        validateCommentCreate(isReplyRequest,parentId);
+        PostType postType = PostType.from(type);
+
+        if (isReplyRequest) {
+            // 답글 생성 검증
+            validateReplyCommentCreate(postId, parent);
+            return withoutIdReplyComment(content, postId, creatorId, parent, postType);
+        }
+        return withoutIdRootComment(content, postId, creatorId, postType);
+    }
+
+
+    private static Comment withoutIdRootComment(String content, Long targetPostId, Long creatorId, PostType postType) {
+        return Comment.builder()
+                .id(null)
+                .content(content)
+                .targetPostId(targetPostId)
+                .creatorId(creatorId)
+                .parentCommentId(null)
+                .postType(postType)
+                .reportCount(0)
+                .likeCount(0)
+                .build();
+    }
+
+    private static Comment withoutIdReplyComment(String content, Long targetPostId, Long creatorId, Comment parentComment, PostType postType) {
+        return Comment.builder()
+                .id(null)
+                .content(content)
+                .targetPostId(targetPostId)
+                .creatorId(creatorId)
+                .parentCommentId(parentComment.getId())
+                .postType(postType)
+                .reportCount(0)
+                .likeCount(0)
+                .build();
+    }
+
+    private static void validateReplyCommentCreate(Long targetPostId, Comment parentComment) {
+        if (parentComment == null) {
+            throw new InvalidStateException(
+                    INVALID_COMMENT_CREATE,new IllegalArgumentException("parentId에 해당하는 부모 댓글이 존재해야 합니다."));
+        }
+        if (!targetPostId.equals(parentComment.getTargetPostId())) {
+            throw new InvalidStateException(
+                    INVALID_COMMENT_CREATE,new IllegalArgumentException("댓글과 부모 댓글의 게시글이 일치하지 않습니다."));
+        }
+    }
+
+    public static void validateCommentCreate(boolean isReplyRequest, Long parentId) {
+        if (isReplyRequest && parentId == null) {
+            throw new InvalidStateException(
+                    INVALID_COMMENT_CREATE, new IllegalArgumentException("답글 작성 시 parentId는 필수입니다."));
+
+        }
+        if (!isReplyRequest && parentId != null) {
+            throw new InvalidStateException(
+                    INVALID_COMMENT_CREATE, new IllegalArgumentException("일반 댓글에는 parentId가 없어야 합니다."));
+        }
+    }
 
 }
