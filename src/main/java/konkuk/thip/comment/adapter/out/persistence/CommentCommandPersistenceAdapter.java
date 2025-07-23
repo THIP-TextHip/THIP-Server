e83274342a80048b206c9175b3269a6d6c082188
강희진 <kanghuijin12@gmail.com>
2025-07-24 01:47:30 +0900

    [feat] CommentCommandPersistenceAdapter.save / findById  작성 (#97)

--- src/main/java/konkuk/thip/comment/adapter/out/persistence/CommentCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/comment/adapter/out/persistence/CommentCommandPersistenceAdapter.java
@@ -1,12 +1,74 @@
 package konkuk.thip.comment.adapter.out.persistence;
 
+import konkuk.thip.comment.adapter.out.jpa.CommentJpaEntity;
+import konkuk.thip.comment.adapter.out.mapper.CommentMapper;
+import konkuk.thip.comment.adapter.out.persistence.repository.CommentJpaRepository;
 import konkuk.thip.comment.application.port.out.CommentCommandPort;
+import konkuk.thip.comment.domain.Comment;
+import konkuk.thip.common.exception.EntityNotFoundException;
+import konkuk.thip.common.post.PostType;
+import konkuk.thip.feed.adapter.out.persistence.repository.FeedJpaRepository;
+import konkuk.thip.post.adapter.out.jpa.PostJpaEntity;
+import konkuk.thip.record.adapter.out.persistence.repository.RecordJpaRepository;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.repository.UserJpaRepository;
+import konkuk.thip.vote.adapter.out.persistence.repository.VoteJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static konkuk.thip.common.exception.code.ErrorCode.*;
 
 @Repository
 @RequiredArgsConstructor
 public class CommentCommandPersistenceAdapter implements CommentCommandPort {
 
+    private final CommentJpaRepository commentJpaRepository;
+    private final FeedJpaRepository feedJpaRepository;
+    private final RecordJpaRepository recordJpaRepository;
+    private final VoteJpaRepository voteJpaRepository;
+    private final UserJpaRepository userJpaRepository;
+    private final CommentMapper commentMapper;
+
+    @Override
+    public Long save(Comment comment) {
+
+        // 1. 작성자(User) 조회 및 존재 검증
+        UserJpaEntity userJpaEntity = userJpaRepository.findById(comment.getCreatorId()).orElseThrow(
+                () -> new EntityNotFoundException(USER_NOT_FOUND)
+        );
+
+        // 2. 게시물(Post) 조회 및 존재 검증
+        PostJpaEntity postJpaEntity = findPostJpaEntity(comment.getPostType(), comment.getTargetPostId());
+
+        // 3. 부모 댓글 조회 (있을 경우)
+        CommentJpaEntity parentCommentJpaEntity = null;
+        if (comment.getParentCommentId() != null) {
+            parentCommentJpaEntity = commentJpaRepository.findById(comment.getParentCommentId())
+                    .orElseThrow(() -> new EntityNotFoundException(COMMENT_NOT_FOUND));
+        }
+
+        return commentJpaRepository.save(
+                commentMapper.toJpaEntity(comment, postJpaEntity, userJpaEntity,parentCommentJpaEntity)
+        ).getCommentId();
+    }
+
+    private PostJpaEntity findPostJpaEntity(PostType postType, Long postId) {
+        return switch (postType) {
+            case FEED -> feedJpaRepository.findById(postId)
+                    .orElseThrow(() -> new EntityNotFoundException(FEED_NOT_FOUND));
+            case RECORD -> recordJpaRepository.findById(postId)
+                    .orElseThrow(() -> new EntityNotFoundException(RECORD_NOT_FOUND));
+            case VOTE -> voteJpaRepository.findById(postId)
+                    .orElseThrow(() -> new EntityNotFoundException(VOTE_NOT_FOUND));
+        };
+    }
+
+    @Override
+    public Optional<Comment> findById(Long id) {
+        return commentJpaRepository.findById(id)
+                .map(commentMapper::toDomainEntity);
+    }
 
 }
