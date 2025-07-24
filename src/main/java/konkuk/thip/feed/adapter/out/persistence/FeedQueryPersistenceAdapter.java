139e1a23cf1a42b9be37315e2abeaa872b95b48f
seongjunnoh <joon1754@naver.com>
2025-07-24 23:41:29 +0900

    [feat] 피드 전체 조회 api adapter 메서드 정의 (#96)

--- src/main/java/konkuk/thip/feed/adapter/out/persistence/FeedQueryPersistenceAdapter.java
+++ src/main/java/konkuk/thip/feed/adapter/out/persistence/FeedQueryPersistenceAdapter.java
@@ -1,11 +1,15 @@
 package konkuk.thip.feed.adapter.out.persistence;
 
+import konkuk.thip.common.util.CursorBasedList;
 import konkuk.thip.feed.adapter.out.mapper.FeedMapper;
 import konkuk.thip.feed.adapter.out.persistence.repository.FeedJpaRepository;
 import konkuk.thip.feed.application.port.out.FeedQueryPort;
+import konkuk.thip.feed.application.port.out.dto.FeedQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 @Repository
@@ -18,5 +22,19 @@ public class FeedQueryPersistenceAdapter implements FeedQueryPort {
     @Override
     public Set<Long> findUserIdsByBookId(Long bookId) {
         return feedJpaRepository.findUserIdsByBookId(bookId);
+    }
+
+    @Override
+    public CursorBasedList<FeedQueryDto> findFeedsByFollowingPriority(Long userId, LocalDateTime cursorVal, int size) {
+        List<FeedQueryDto> feedQueryDtos = feedJpaRepository.findFeedsByFollowingPriority(userId, cursorVal, size);
+
+        return CursorBasedList.of(feedQueryDtos, size, feedQueryDto -> feedQueryDto.createdAt().toString());
+    }
+
+    @Override
+    public CursorBasedList<FeedQueryDto> findLatestFeedsByCreatedAt(Long userId, LocalDateTime cursorVal, int size) {
+        List<FeedQueryDto> feedQueryDtos = feedJpaRepository.findLatestFeedsByCreatedAt(userId, cursorVal, size);
+
+        return CursorBasedList.of(feedQueryDtos, size, feedQueryDto -> feedQueryDto.createdAt().toString());
     }
 }
--- src/main/java/konkuk/thip/feed/application/port/out/FeedQueryPort.java
+++ src/main/java/konkuk/thip/feed/application/port/out/FeedQueryPort.java
@@ -1,8 +1,16 @@
 package konkuk.thip.feed.application.port.out;
 
+import konkuk.thip.common.util.CursorBasedList;
+import konkuk.thip.feed.application.port.out.dto.FeedQueryDto;
+
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public interface FeedQueryPort {
 
     Set<Long> findUserIdsByBookId(Long bookId);
+
+    CursorBasedList<FeedQueryDto> findFeedsByFollowingPriority(Long userId, LocalDateTime cursorVal, int size);
+
+    CursorBasedList<FeedQueryDto> findLatestFeedsByCreatedAt(Long userId, LocalDateTime cursorVal, int size);
 }
