e692a3c4c26be0817d807083e17dc693f8eab882
seongjunnoh <joon1754@naver.com>
2025-07-24 23:32:09 +0900

    [feat] 피드 전체 조회 api controller 개발 (#96)

---
+++ src/main/java/konkuk/thip/feed/adapter/in/web/response/FeedShowAllResponse.java
@@ -0,0 +1,27 @@
+package konkuk.thip.feed.adapter.in.web.response;
+
+import java.util.List;
+
+public record FeedShowAllResponse(
+        List<Feed> feedList,
+        String nextCursor,
+        boolean isLast
+) {
+    public record Feed(
+            Long feedId,
+            Long creatorId,
+            String creatorNickname,
+            String creatorProfileImageUrl,
+            String alias,
+            String postDate,
+            String isbn,
+            String bookTitle,
+            String bookAuthor,
+            String contentBody,
+            String[] contentUrls,
+            int likeCount,
+            int commentCount,
+            boolean isSaved,
+            boolean isLiked
+    ) { }
+}
