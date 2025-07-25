d49d428678e2b1b42025134554aebe7bff7dabd1
janghyunjun <buzz03312@naver.com>
2025-07-25 16:02:19 +0900

    [feat] 특정 게시물을 사용자가 좋아요 눌렀는지 여부 조회 쿼리 (#119)

--- src/main/java/konkuk/thip/post/adapter/out/persistence/PostLikeJpaRepository.java
+++ src/main/java/konkuk/thip/post/adapter/out/persistence/PostLikeJpaRepository.java
@@ -2,11 +2,19 @@ package konkuk.thip.post.adapter.out.persistence;
 
 import konkuk.thip.post.adapter.out.jpa.PostLikeJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface PostLikeJpaRepository extends JpaRepository<PostLikeJpaEntity, Long> {
     int countByPostJpaEntity_PostId(Long postId);
 
     boolean existsByPostJpaEntity_PostIdAndUserJpaEntity_UserId(Long postId, Long userId);
+
+    @Query(value = "SELECT pl.post_id FROM post_likes pl WHERE pl.user_id = :userId AND pl.post_id IN (:postIds)", nativeQuery = true)
+    Set<Long> findPostIdsLikedByUser(@Param("postIds") Set<Long> postIds,
+                                     @Param("userId") Long userId);
 }
