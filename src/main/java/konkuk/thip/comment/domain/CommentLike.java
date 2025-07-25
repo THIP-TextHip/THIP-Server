04e17d15ad278bb471cb67e91fe0c91c25c20da6
강희진 <kanghuijin12@gmail.com>
2025-07-25 18:47:24 +0900

    [refactor] 댓글 좋아요  CommentLike 도메인이 하도록 검증(#99)

--- src/main/java/konkuk/thip/comment/domain/CommentLike.java
+++ src/main/java/konkuk/thip/comment/domain/CommentLike.java
@@ -1,8 +1,12 @@
 package konkuk.thip.comment.domain;
 
 import konkuk.thip.common.entity.BaseDomainEntity;
+import konkuk.thip.common.exception.InvalidStateException;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+
+import static konkuk.thip.common.exception.code.ErrorCode.COMMENT_ALREADY_LIKED;
+import static konkuk.thip.common.exception.code.ErrorCode.COMMENT_NOT_LIKED_CANNOT_CANCEL;
 
 @Getter
 @SuperBuilder
@@ -13,4 +17,19 @@ public class CommentLike extends BaseDomainEntity {
     private Long userId;
 
     private Long targetCommentId;
+
+    // 좋아요 생성 가능 여부 검증 (이미 좋아요한 상태면 예외)
+    public static void validateCanLike(boolean alreadyLiked) {
+        if (alreadyLiked) {
+            throw new InvalidStateException(COMMENT_ALREADY_LIKED);
+        }
+    }
+
+    // 좋아요 취소 가능 여부 검증 (좋아요 안 한 상태면 예외)
+    public static void validateCanUnlike(boolean alreadyLiked) {
+        if (!alreadyLiked) {
+            throw new InvalidStateException(COMMENT_NOT_LIKED_CANNOT_CANCEL);
+        }
+    }
+
 }
