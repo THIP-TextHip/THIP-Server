01a8f6bfad2787d7abd5c991f2b208f9de8aa6e1
강희진 <kanghuijin12@gmail.com>
2025-07-25 18:49:13 +0900

    [refactor] 일급컬렉션 조회 하지않고 단순 해당댓글에대해 좋아요여부만 조회한뒤 CommentLike가 검증하도록 리펙 (#99)

--- src/main/java/konkuk/thip/comment/application/port/out/CommentLikeQueryPort.java
+++ src/main/java/konkuk/thip/comment/application/port/out/CommentLikeQueryPort.java
@@ -1,7 +1,5 @@
 package konkuk.thip.comment.application.port.out;
 
-import konkuk.thip.comment.domain.LikedComments;
-
 public interface CommentLikeQueryPort {
-    LikedComments findLikedCommentsByUserId(Long userId);
+    boolean isLikedCommentByUser(Long userId, Long commentId);
 }
--- src/main/java/konkuk/thip/comment/application/service/CommentLikeService.java
+++ src/main/java/konkuk/thip/comment/application/service/CommentLikeService.java
@@ -8,7 +8,7 @@ import konkuk.thip.comment.application.port.out.CommentCommandPort;
 import konkuk.thip.comment.application.port.out.CommentLikeCommandPort;
 import konkuk.thip.comment.application.port.out.CommentLikeQueryPort;
 import konkuk.thip.comment.domain.Comment;
-import konkuk.thip.comment.domain.LikedComments;
+import konkuk.thip.comment.domain.CommentLike;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,21 +27,19 @@ public class CommentLikeService implements CommentLikeUseCase {
         // 1. 댓글 조회 및 검증 (존재 여부)
         Comment comment = commentCommandPort.getByIdOrThrow(command.commentId());
 
-        // 2. 유저가 좋아요 한 댓글 목록 조회
-        LikedComments likedComments = commentLikeQueryPort.findLikedCommentsByUserId(command.userId());
+        // 2. 유저가 해당 댓글에 대해 좋아요 했는지 조회
+        boolean alreadyLiked = commentLikeQueryPort.isLikedCommentByUser(command.userId(), command.commentId());
 
         // 3. 좋아요 상태변경
         if (command.isLike()) {
-            // 좋아요 요청 시 이미 좋아요되어 있으면 예외 발생
-            likedComments.validateNotAlreadyLiked(comment);
-            commentLikeCommandPort.save(command.userId(),comment.getId());
+            CommentLike.validateCanLike(alreadyLiked); // 좋아요 가능 여부 검증
+            commentLikeCommandPort.save(command.userId(), command.commentId());
         } else {
-            // 좋아요 취소 요청 시 좋아요되어 있지 않으면 예외 발생
-            likedComments.validateCanUnlike(comment);
-            commentLikeCommandPort.delete(command.userId(),comment.getId());
+            CommentLike.validateCanUnlike(alreadyLiked); // 좋아요 취소 가능 여부 검증
+            commentLikeCommandPort.delete(command.userId(), command.commentId());
         }
 
-        // 4. 댓글 좋아요 수 업데이트
+        // 5. 댓글 좋아요 수 업데이트
         comment.updateLikeCount(command.isLike());
         commentCommandPort.update(comment);
 
