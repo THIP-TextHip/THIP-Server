791fee4e0b52a575c8e6748e2dcadc74638806f2
강희진 <kanghuijin12@gmail.com>
2025-07-26 18:22:05 +0900

    [test] 테스트마다 독립성 보장 어노테이션 추가 (#97)

--- src/test/java/konkuk/thip/comment/adapter/in/web/CommentControllerTest.java
+++ src/test/java/konkuk/thip/comment/adapter/in/web/CommentControllerTest.java
@@ -30,6 +30,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,6 +49,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @DisplayName("[단위] 댓글 생성 api controller 단위 테스트")
 class CommentControllerTest {
 
--- src/test/java/konkuk/thip/comment/adapter/in/web/CommentCreateAPITest.java
+++ src/test/java/konkuk/thip/comment/adapter/in/web/CommentCreateAPITest.java
@@ -28,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,6 +45,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @DisplayName("[통합] 댓글 생성 api 통합 테스트")
 class CommentCreateAPITest {
 
