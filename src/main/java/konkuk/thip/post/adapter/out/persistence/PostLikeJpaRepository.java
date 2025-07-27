aa4c64f0bf54e6454482ff634f3a33aac100cfb2
seongjunnoh <joon1754@naver.com>
2025-07-28 03:19:03 +0900

    [refactor] no use 메서드 삭제 (#96)

--- src/main/java/konkuk/thip/post/adapter/out/persistence/PostLikeJpaRepository.java
+++ src/main/java/konkuk/thip/post/adapter/out/persistence/PostLikeJpaRepository.java
@@ -12,9 +12,6 @@ import java.util.Set;
 
 @Repository
 public interface PostLikeJpaRepository extends JpaRepository<PostLikeJpaEntity, Long> {
-    int countByPostJpaEntity_PostId(Long postId);
-
-    boolean existsByPostJpaEntity_PostIdAndUserJpaEntity_UserId(Long postId, Long userId);
 
     @Query("SELECT p.postJpaEntity.postId FROM PostLikeJpaEntity p WHERE p.userJpaEntity.userId = :userId AND p.postJpaEntity.postId IN :postIds")
     List<Long> findPostIdsLikedByUserIdAndPostIds(@Param("userId") Long userId, @Param("postIds") List<Long> postIds);
