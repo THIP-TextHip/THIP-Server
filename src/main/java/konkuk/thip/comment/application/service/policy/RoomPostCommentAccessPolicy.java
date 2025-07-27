e5a2507913cf827d95a4db82e19b20b853a63958
강희진 <kanghuijin12@gmail.com>
2025-07-27 15:27:20 +0900

    [refactor] RoomPostCommentAccessPolicy (#99)

--- src/main/java/konkuk/thip/comment/application/service/policy/RoomPostCommentAccessPolicy.java
+++ src/main/java/konkuk/thip/comment/application/service/policy/RoomPostCommentAccessPolicy.java
@@ -1,15 +1,11 @@
 package konkuk.thip.comment.application.service.policy;
 
-import konkuk.thip.common.exception.InvalidStateException;
 import konkuk.thip.common.post.CommentCountUpdatable;
 import konkuk.thip.common.post.PostType;
-import konkuk.thip.room.application.port.out.RoomParticipantQueryPort;
 import konkuk.thip.room.application.service.RoomParticipantValidator;
 import konkuk.thip.room.domain.RoomPost;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import static konkuk.thip.common.exception.code.ErrorCode.ROOM_ACCESS_FORBIDDEN;
 
 @Component
 @RequiredArgsConstructor
@@ -27,4 +23,5 @@ public class RoomPostCommentAccessPolicy implements CommentAccessPolicy {
         RoomPost roomPost = (RoomPost) post;
         roomParticipantValidator.validateUserIsRoomMember(roomPost.getRoomId(), userId);
     }
+
 }
