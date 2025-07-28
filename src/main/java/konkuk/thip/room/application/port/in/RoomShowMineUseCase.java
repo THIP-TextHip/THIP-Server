d3f4f939ccc677724089580efe74bc08d6e160a1
seongjunnoh <joon1754@naver.com>
2025-07-28 20:25:48 +0900

    [refactor] usecase 메서드 파라미터 수정 (#89)
    - String cursor 를 주입받도록 수정

--- src/main/java/konkuk/thip/room/application/port/in/RoomShowMineUseCase.java
+++ src/main/java/konkuk/thip/room/application/port/in/RoomShowMineUseCase.java
@@ -2,9 +2,7 @@ package konkuk.thip.room.application.port.in;
 
 import konkuk.thip.room.adapter.in.web.response.RoomShowMineResponse;
 
-import java.time.LocalDate;
-
 public interface RoomShowMineUseCase {
 
-    RoomShowMineResponse getMyRooms(Long userId, String type, LocalDate cursorDate, Long cursorId);
+    RoomShowMineResponse getMyRooms(Long userId, String type, String cursor);
 }
