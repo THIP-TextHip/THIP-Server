e98256d7506d237c2293db882fc34bf6a97f9cc7
seongjunnoh <joon1754@naver.com>
2025-07-30 03:13:38 +0900

    [feat] 유저가 작성한 피드의 총 개수 구하는 jpql 코드 구현 (#110)

--- src/main/java/konkuk/thip/feed/adapter/out/persistence/repository/FeedJpaRepository.java
+++ src/main/java/konkuk/thip/feed/adapter/out/persistence/repository/FeedJpaRepository.java
@@ -1,7 +1,13 @@
 package konkuk.thip.feed.adapter.out.persistence.repository;
 
+import konkuk.thip.common.entity.StatusType;
 import konkuk.thip.feed.adapter.out.jpa.FeedJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FeedJpaRepository extends JpaRepository<FeedJpaEntity, Long>, FeedQueryRepository {
+
+    @Query("SELECT COUNT(f) FROM FeedJpaEntity f WHERE f.userJpaEntity.userId = :userId AND f.status = :status")
+    long countFeedsByUserId(@Param("userId") Long userId, @Param("status") StatusType status);
 }
