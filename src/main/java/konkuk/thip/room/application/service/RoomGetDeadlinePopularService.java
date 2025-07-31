966c81b5dff4404197b734a397c644638dc417f3
janghyunjun <buzz03312@naver.com>
2025-07-31 18:29:37 +0900

    [feat] 서비스 구현 (#117)

---
+++ src/main/java/konkuk/thip/room/application/service/RoomGetDeadlinePopularService.java
@@ -0,0 +1,31 @@
+package konkuk.thip.room.application.service;
+
+import konkuk.thip.room.adapter.in.web.response.RoomGetDeadlinePopularResponse;
+import konkuk.thip.room.application.mapper.RoomQueryMapper;
+import konkuk.thip.room.application.port.in.RoomGetDeadlinePopularUseCase;
+import konkuk.thip.room.application.port.out.RoomQueryPort;
+import konkuk.thip.room.domain.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoomGetDeadlinePopularService implements RoomGetDeadlinePopularUseCase {
+
+    private final RoomQueryPort roomQueryPort;
+    private final RoomQueryMapper roomQueryMapper;
+
+    private static final int DEFAULT_LIMIT = 4;
+
+    @Override
+    public RoomGetDeadlinePopularResponse getDeadlineAndPopularRoomList(String categoryStr) {
+        Category category = Category.from(categoryStr);
+
+        var deadlineRoomList = roomQueryMapper.toDeadlinePopularRoomDtoList(
+                roomQueryPort.findRoomsByCategoryOrderByDeadline(category, DEFAULT_LIMIT));
+        var popularRoomList = roomQueryMapper.toDeadlinePopularRoomDtoList(
+                roomQueryPort.findRoomsByCategoryOrderByPopular(category, DEFAULT_LIMIT));
+
+        return RoomGetDeadlinePopularResponse.of(deadlineRoomList, popularRoomList);
+    }
+}
