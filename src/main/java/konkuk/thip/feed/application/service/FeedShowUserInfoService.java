e3be4e256b31f7c430a56c45cac6c7c8d3db2214
seongjunnoh <joon1754@naver.com>
2025-07-31 18:09:34 +0900

    [feat] 피드 조회시 유저 정보 조회 api (2개) use case 개발 (#115)

---
+++ src/main/java/konkuk/thip/feed/application/service/FeedShowUserInfoService.java
@@ -0,0 +1,65 @@
+package konkuk.thip.feed.application.service;
+
+import konkuk.thip.feed.adapter.in.web.response.FeedShowUserInfoResponse;
+import konkuk.thip.feed.application.port.in.FeedShowUserInfoUseCase;
+import konkuk.thip.feed.application.port.out.FeedQueryPort;
+import konkuk.thip.user.application.port.out.FollowingQueryPort;
+import konkuk.thip.user.application.port.out.UserCommandPort;
+import konkuk.thip.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FeedShowUserInfoService implements FeedShowUserInfoUseCase {
+
+    private static final int FOLLOWER_DISPLAY_LIMIT = 5;
+    private final UserCommandPort userCommandPort;
+    private final FollowingQueryPort followingQueryPort;
+    private final FeedQueryPort feedQueryPort;
+
+    @Transactional(readOnly = true)
+    @Override
+    public FeedShowUserInfoResponse showMyInfoInFeeds(Long userId) {
+        // 1. User 찾기
+        User feedOwner = userCommandPort.findById(userId);
+
+        // 2. 해당 유저를 팔로우 하는 유저들을 프로필 이미지 정보 구하기
+        List<String> latestFollowerProfileImageUrls = followingQueryPort.getLatestFollowerProfileImageUrlsByUserId(userId, FOLLOWER_DISPLAY_LIMIT);
+
+        // 3. 내가 작성한 전체 피드 개수 구하기
+        int allFeedCount = feedQueryPort.countAllFeedsByUserId(userId);
+
+        return buildResponse(feedOwner, allFeedCount, latestFollowerProfileImageUrls);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public FeedShowUserInfoResponse showAnotherUserInfoInFeeds(Long anotherUserId) {
+        // 1. User 찾기
+        User feedOwner = userCommandPort.findById(anotherUserId);
+
+        // 2. 해당 유저를 팔로우 하는 유저들을 프로필 이미지 정보 구하기
+        List<String> latestFollowerProfileImageUrls = followingQueryPort.getLatestFollowerProfileImageUrlsByUserId(anotherUserId, FOLLOWER_DISPLAY_LIMIT);
+
+        // 3. 유저가 작성한 공개 피드 개수 구하기
+        int publicFeedCount = feedQueryPort.countPublicFeedsByUserId(anotherUserId);
+
+        return buildResponse(feedOwner, publicFeedCount, latestFollowerProfileImageUrls);
+    }
+
+    private FeedShowUserInfoResponse buildResponse(User feedOwner, int totalFeedCount, List<String> latestFollowerProfileImageUrls) {
+        return FeedShowUserInfoResponse.builder()
+                .profileImageUrl(feedOwner.getAlias().getImageUrl())
+                .nickname(feedOwner.getNickname())
+                .aliasName(feedOwner.getAlias().getValue())
+                .aliasColor(feedOwner.getAlias().getColor())
+                .followerCount(feedOwner.getFollowerCount())
+                .totalFeedCount(totalFeedCount)
+                .latestFollowerProfileImageUrls(latestFollowerProfileImageUrls)
+                .build();
+    }
+}
