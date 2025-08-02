322cd09d9e82a7b0abe883807b7a6a8e6fd178d6
seongjunnoh <joon1754@naver.com>
2025-08-02 16:09:17 +0900

    [feat] 특정 유저의 피드 전체 조회 api use case 개발 (#121)

--- src/main/java/konkuk/thip/feed/application/port/in/FeedShowMineUseCase.java
+++
@@ -1,8 +0,0 @@
-package konkuk.thip.feed.application.port.in;
-
-import konkuk.thip.feed.adapter.in.web.response.FeedShowMineResponse;
-
-public interface FeedShowMineUseCase {
-
-    FeedShowMineResponse showMyFeeds(Long userId, String cursor);
-}
