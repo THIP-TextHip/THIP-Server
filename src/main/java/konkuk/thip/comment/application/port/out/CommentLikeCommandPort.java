5340f2a96f0abc542b9834fa0c0667a881d98416
강희진 <kanghuijin12@gmail.com>
2025-08-03 03:00:18 +0900

    [feat] CommentLikeCommandPort.deleteAllByCommentId 추가 (#103)

--- src/main/java/konkuk/thip/comment/application/port/out/CommentLikeCommandPort.java
+++ src/main/java/konkuk/thip/comment/application/port/out/CommentLikeCommandPort.java
@@ -4,4 +4,5 @@ package konkuk.thip.comment.application.port.out;
 public interface CommentLikeCommandPort {
     void save(Long userId, Long commentId);
     void delete(Long userId, Long commentId);
+    void deleteAllByCommentId(Long commentId);
 }
