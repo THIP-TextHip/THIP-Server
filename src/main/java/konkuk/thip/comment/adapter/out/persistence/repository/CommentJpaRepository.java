19d2ffaf44b5d1725b2fe2ebf302683538b7fde3
강희진 <kanghuijin12@gmail.com>
2025-08-03 02:58:37 +0900

    [feat] findByCommentIdAndStatus 작성 (#103)

--- src/main/java/konkuk/thip/comment/adapter/out/persistence/repository/CommentJpaRepository.java
+++ src/main/java/konkuk/thip/comment/adapter/out/persistence/repository/CommentJpaRepository.java
@@ -1,8 +1,11 @@
 package konkuk.thip.comment.adapter.out.persistence.repository;
 
 import konkuk.thip.comment.adapter.out.jpa.CommentJpaEntity;
+import konkuk.thip.common.entity.StatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentJpaRepository extends JpaRepository<CommentJpaEntity, Long> {
-    int countByPostJpaEntity_PostId(Long postId);
+    Optional<CommentJpaEntity> findByCommentIdAndStatus(Long commentId, StatusType status);
 }
