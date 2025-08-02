76c08bce0f06d728b5b2a7ce48aad83ddf0fd4ad
강희진 <kanghuijin12@gmail.com>
2025-08-03 03:32:16 +0900

    [test] 댓글 삭제 통합 테스트 코드 작성 (#103)

---
+++ src/test/java/konkuk/thip/comment/adapter/in/web/CommentDeleteAPITest.java
@@ -0,0 +1,159 @@
+package konkuk.thip.comment.adapter.in.web;
+
+import konkuk.thip.book.adapter.out.jpa.BookJpaEntity;
+import konkuk.thip.book.adapter.out.persistence.repository.BookJpaRepository;
+import konkuk.thip.comment.adapter.out.jpa.CommentJpaEntity;
+import konkuk.thip.comment.adapter.out.persistence.repository.CommentJpaRepository;
+import konkuk.thip.comment.adapter.out.persistence.repository.CommentLikeJpaRepository;
+import konkuk.thip.common.util.TestEntityFactory;
+import konkuk.thip.feed.adapter.out.jpa.FeedJpaEntity;
+import konkuk.thip.feed.adapter.out.persistence.repository.FeedJpaRepository;
+import konkuk.thip.record.adapter.out.jpa.RecordJpaEntity;
+import konkuk.thip.record.adapter.out.persistence.repository.RecordJpaRepository;
+import konkuk.thip.room.adapter.out.jpa.CategoryJpaEntity;
+import konkuk.thip.room.adapter.out.jpa.RoomJpaEntity;
+import konkuk.thip.room.adapter.out.jpa.RoomParticipantRole;
+import konkuk.thip.room.adapter.out.persistence.repository.RoomJpaRepository;
+import konkuk.thip.room.adapter.out.persistence.repository.category.CategoryJpaRepository;
+import konkuk.thip.room.adapter.out.persistence.repository.roomparticipant.RoomParticipantJpaRepository;
+import konkuk.thip.user.adapter.out.jpa.AliasJpaEntity;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.repository.UserJpaRepository;
+import konkuk.thip.user.adapter.out.persistence.repository.alias.AliasJpaRepository;
+import konkuk.thip.vote.adapter.out.jpa.VoteJpaEntity;
+import konkuk.thip.vote.adapter.out.persistence.repository.VoteJpaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static konkuk.thip.common.entity.StatusType.INACTIVE;
+import static konkuk.thip.common.post.PostType.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
+@Transactional
+@DisplayName("[통합] 댓글 삭제 api 통합 테스트")
+class CommentDeleteAPITest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired private AliasJpaRepository aliasJpaRepository;
+    @Autowired private UserJpaRepository userJpaRepository;
+    @Autowired private CategoryJpaRepository categoryJpaRepository;
+    @Autowired private BookJpaRepository bookJpaRepository;
+    @Autowired private FeedJpaRepository feedJpaRepository;
+    @Autowired private VoteJpaRepository voteJpaRepository;
+    @Autowired private RecordJpaRepository recordJpaRepository;
+    @Autowired private CommentJpaRepository commentJpaRepository;
+    @Autowired private RoomJpaRepository roomJpaRepository;
+    @Autowired private RoomParticipantJpaRepository roomParticipantJpaRepository;
+    @Autowired private CommentLikeJpaRepository commentLikeJpaRepository;
+
+
+
+    private AliasJpaEntity alias;
+    private UserJpaEntity user;
+    private CategoryJpaEntity category;
+    private FeedJpaEntity feed;
+    private BookJpaEntity book;
+    private RecordJpaEntity record;
+    private VoteJpaEntity vote;
+    private RoomJpaEntity room;
+
+    @BeforeEach
+    void setUp() {
+        alias = aliasJpaRepository.save(TestEntityFactory.createLiteratureAlias());
+        user = userJpaRepository.save(TestEntityFactory.createUser(alias));
+        category = categoryJpaRepository.save(TestEntityFactory.createLiteratureCategory(alias));
+        book = bookJpaRepository.save(TestEntityFactory.createBookWithISBN("9788954682152"));
+        room = roomJpaRepository.save(TestEntityFactory.createRoom(book,category));
+        feed = feedJpaRepository.save(TestEntityFactory.createFeed(user,book, true));
+        record = recordJpaRepository.save(TestEntityFactory.createRecord(user,room));
+        vote = voteJpaRepository.save(TestEntityFactory.createVote(user,room));
+        roomParticipantJpaRepository.save(TestEntityFactory.createRoomParticipant(room, user, RoomParticipantRole.HOST, 0.0));
+    }
+
+
+    @Test
+    @DisplayName("루트댓글을 삭제하면 [soft delete 처리]된다")
+    void deleteRootComment_success() throws Exception {
+
+        // given
+        CommentJpaEntity comment = commentJpaRepository.save(TestEntityFactory.createComment(feed, user, FEED));
+        feed.updateCommentCount(1);
+        Long commentId = comment.getCommentId();
+
+        // when
+        mockMvc.perform(delete("/comments/{commentId}", commentId)
+                        .requestAttr("userId", user.getUserId()))
+                .andExpect(status().isOk());
+
+        // then
+        CommentJpaEntity found = commentJpaRepository.findById(commentId).get();
+        assertThat(found.getStatus()).isEqualTo(INACTIVE);
+    }
+
+    @Test
+    @DisplayName("대댓글을 삭제하면 [soft delete 처리]된다")
+    void deleteReplyComment_success() throws Exception {
+
+        // given: 부모 댓글/대댓글 생성
+        CommentJpaEntity parent = commentJpaRepository.save(TestEntityFactory.createComment(feed, user, FEED));
+        CommentJpaEntity reply = commentJpaRepository.save(TestEntityFactory.createReplyComment(feed, user, FEED, parent));
+        feed.updateCommentCount(2);
+        Long replyId = reply.getCommentId();
+
+
+        // when
+        mockMvc.perform(delete("/comments/{commentId}", replyId)
+                        .requestAttr("userId", user.getUserId()))
+                .andExpect(status().isOk());
+
+        // then
+        CommentJpaEntity found = commentJpaRepository.findById(replyId).get();
+        assertThat(found.getStatus()).isEqualTo(INACTIVE);
+    }
+
+    @Test
+    @DisplayName("댓글 삭제 시에 댓글을 성공적으로 삭제하면 댓글의 상태가 soft delete 처리되고" +
+            "게시물의 댓글 수가 1감소하고, 댓글에 달린 좋아요가 전체 삭제 된다.")
+    void deleteComment_success() throws Exception {
+
+        // given
+        CommentJpaEntity comment = commentJpaRepository.save(TestEntityFactory.createComment(feed, user, FEED));
+        commentLikeJpaRepository.save(TestEntityFactory.createCommentLike(comment, user));
+        feed.updateCommentCount(1);
+        Long commentId = comment.getCommentId();
+        int beforeCount = feed.getCommentCount();
+
+        // when
+        mockMvc.perform(delete("/comments/{commentId}", commentId)
+                        .requestAttr("userId", user.getUserId()))
+                .andExpect(status().isOk());
+
+        // then
+        CommentJpaEntity found = commentJpaRepository.findById(commentId).get();
+        assertThat(found.getStatus()).isEqualTo(INACTIVE);
+
+        // Feed 댓글수 감소 확인
+        FeedJpaEntity updatedFeed = feedJpaRepository.findById(feed.getPostId()).get();
+        assertThat(updatedFeed.getCommentCount()).isEqualTo(beforeCount - 1);
+
+        // 댓글 좋아요가 모두 삭제됐는지 확인
+        boolean like = (boolean) commentLikeJpaRepository.existsByUserIdAndCommentId(user.getUserId(), commentId);
+        assertThat(like).isFalse();
+    }
+
+
+}
