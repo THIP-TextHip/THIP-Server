ad53742b6cc4fda86e309d3a101735ca2675791f
janghyunjun <buzz03312@naver.com>
2025-08-05 19:31:01 +0900

    [refactor] count쿼리 jpql로 바꾸기 (ACTIVE 조건 추가) (#140)

--- src/main/java/konkuk/thip/room/adapter/out/persistence/repository/RoomJpaRepository.java
+++ src/main/java/konkuk/thip/room/adapter/out/persistence/repository/RoomJpaRepository.java
@@ -2,12 +2,17 @@ package konkuk.thip.room.adapter.out.persistence.repository;
 
 import konkuk.thip.room.adapter.out.jpa.RoomJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 
 public interface RoomJpaRepository extends JpaRepository<RoomJpaEntity, Long>, RoomQueryRepository {
 
-    int countByBookJpaEntity_BookIdAndStartDateAfter(Long bookId, LocalDate currentDate);
-
+    @Query("SELECT COUNT(r) FROM RoomJpaEntity r " +
+            "WHERE r.bookJpaEntity.bookId = :bookId " +
+            "AND r.startDate > :currentDate " +
+            "AND r.status = 'ACTIVE'")
+    int countActiveRoomsByBookIdAndStartDateAfter(@Param("bookId") Long bookId, @Param("currentDate") LocalDate currentDate);
 
 }
