d69eba8ced9f9b8f8b210fb3a1905c8487a96566
seongjunnoh <joon1754@naver.com>
2025-08-06 16:56:41 +0900

    [feat] 내 모임방 조회 response 에 String type 및 swagger description 추가 (#151)

--- src/main/java/konkuk/thip/room/adapter/in/web/response/RoomShowMineResponse.java
+++ src/main/java/konkuk/thip/room/adapter/in/web/response/RoomShowMineResponse.java
@@ -1,7 +1,10 @@
 package konkuk.thip.room.adapter.in.web.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
+@Schema(description = "내가 참여 중인 방 목록 조회 응답")
 public record RoomShowMineResponse(
         List<MyRoom> roomList,
         String nextCursor,
@@ -12,6 +15,11 @@ public record RoomShowMineResponse(
             String bookImageUrl,
             String roomName,
             int memberCount,
-            String endDate      // 방 진행 마감일 or 방 모집 마감일 (~ 뒤 형식)
+
+            @Schema(description = "방 진행 마감일 or 방 모집 마감일까지 남은 시간 (ex. \"3일 뒤\"), 완료된 방은 쓰레기값이 넘어갑니다. 무시해주세요.")
+            String endDate,     // 방 진행 마감일 or 방 모집 마감일 (~ 뒤 형식)
+
+            @Schema(description = "방 상태 : [모집중(=recruiting), 진행중(=playing), 완료된(=expired)] 중 하나")
+            String type
     ) {}
 }
