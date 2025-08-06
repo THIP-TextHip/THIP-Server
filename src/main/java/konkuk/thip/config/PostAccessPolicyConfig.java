142649f65dad5abcc24719a54d6214e5f6bcc8f7
강희진 <kanghuijin12@gmail.com>
2025-08-07 00:16:45 +0900

    [feat] 게시글 좋아요 접근 정책 config 작성 (#159)

--- src/main/java/konkuk/thip/config/PostAccessPolicyConfig.java
+++ src/main/java/konkuk/thip/config/PostAccessPolicyConfig.java
@@ -4,6 +4,9 @@ import konkuk.thip.comment.application.service.policy.CommentAccessPolicy;
 import konkuk.thip.comment.application.service.policy.FeedCommentAccessPolicy;
 import konkuk.thip.comment.application.service.policy.RoomPostCommentAccessPolicy;
 import konkuk.thip.common.post.PostType;
+import konkuk.thip.post.application.service.policy.FeedLikeAccessPolicy;
+import konkuk.thip.post.application.service.policy.PostLikeAccessPolicy;
+import konkuk.thip.post.application.service.policy.RoomPostLikeAccessPolicy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +15,13 @@ import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
-public class CommentAccessPolicyConfig {
+public class PostAccessPolicyConfig {
 
     private final FeedCommentAccessPolicy feedCommentPolicy;
     private final RoomPostCommentAccessPolicy roomCommentPolicy;
+
+    private final FeedLikeAccessPolicy feedLikePolicy;
+    private final RoomPostLikeAccessPolicy roomLikePolicy;
 
     @Bean
     public Map<PostType, CommentAccessPolicy> commentAccessPolicyMap() {
@@ -23,6 +29,15 @@ public class CommentAccessPolicyConfig {
                 PostType.FEED, feedCommentPolicy,
                 PostType.RECORD, roomCommentPolicy,
                 PostType.VOTE, roomCommentPolicy
+        );
+    }
+
+    @Bean
+    public Map<PostType, PostLikeAccessPolicy> roomPostAccessPolicyMap() {
+        return Map.of(
+                PostType.FEED, feedLikePolicy,
+                PostType.RECORD, roomLikePolicy,
+                PostType.VOTE, roomLikePolicy
         );
     }
 }
