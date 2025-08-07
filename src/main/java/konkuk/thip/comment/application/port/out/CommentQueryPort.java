33732a8f63cfdd145f89f68060b708aeb681c98e
seongjunnoh <joon1754@naver.com>
2025-08-07 20:19:10 +0900

    [feat] 댓글 조회용 영속성 port 추가 (#136)

--- src/main/java/konkuk/thip/comment/application/port/out/CommentQueryPort.java
+++ src/main/java/konkuk/thip/comment/application/port/out/CommentQueryPort.java
@@ -1,5 +1,14 @@
 package konkuk.thip.comment.application.port.out;
 
+import konkuk.thip.comment.application.port.out.dto.CommentQueryDto;
+import konkuk.thip.common.util.Cursor;
+import konkuk.thip.common.util.CursorBasedList;
+
+import java.util.List;
+
 public interface CommentQueryPort {
 
+    CursorBasedList<CommentQueryDto> findLatestRootCommentsWithDeleted(Long postId, Cursor cursor);
+
+    List<CommentQueryDto> findAllActiveChildrenComments(Long rootCommentId);
 }
