68401f17d7a463ec90ebc5590e664e2e86668bde
seongjunnoh <joon1754@naver.com>
2025-08-09 03:40:47 +0900

    [feat] 생성한 출석체크 도메인 저장 로직 구현 (#171)

--- src/main/java/konkuk/thip/attendancecheck/adapter/out/persistence/AttendanceCheckCommandPersistenceAdapter.java
+++ src/main/java/konkuk/thip/attendancecheck/adapter/out/persistence/AttendanceCheckCommandPersistenceAdapter.java
@@ -3,14 +3,39 @@ package konkuk.thip.attendancecheck.adapter.out.persistence;
 import konkuk.thip.attendancecheck.adapter.out.mapper.AttendanceCheckMapper;
 import konkuk.thip.attendancecheck.adapter.out.persistence.repository.AttendanceCheckJpaRepository;
 import konkuk.thip.attendancecheck.application.port.out.AttendanceCheckCommandPort;
+import konkuk.thip.attendancecheck.domain.AttendanceCheck;
+import konkuk.thip.common.exception.EntityNotFoundException;
+import konkuk.thip.room.adapter.out.jpa.RoomJpaEntity;
+import konkuk.thip.room.adapter.out.persistence.repository.RoomJpaRepository;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import static konkuk.thip.common.exception.code.ErrorCode.ROOM_NOT_FOUND;
+import static konkuk.thip.common.exception.code.ErrorCode.USER_NOT_FOUND;
 
 @Repository
 @RequiredArgsConstructor
 public class AttendanceCheckCommandPersistenceAdapter implements AttendanceCheckCommandPort {
 
     private final AttendanceCheckJpaRepository attendanceCheckJpaRepository;
+    private final RoomJpaRepository roomJpaRepository;
+    private final UserJpaRepository userJpaRepository;
     private final AttendanceCheckMapper attendanceCheckMapper;
 
+    @Override
+    public Long save(AttendanceCheck attendanceCheck) {
+        RoomJpaEntity roomJpaEntity = roomJpaRepository.findById(attendanceCheck.getRoomId()).orElseThrow(
+                () -> new EntityNotFoundException(ROOM_NOT_FOUND)
+        );
+
+        UserJpaEntity userJpaEntity = userJpaRepository.findById(attendanceCheck.getCreatorId()).orElseThrow(
+                () -> new EntityNotFoundException(USER_NOT_FOUND)
+        );
+
+        return attendanceCheckJpaRepository.save(
+                attendanceCheckMapper.toJpaEntity(attendanceCheck, roomJpaEntity, userJpaEntity)
+        ).getAttendanceCheckId();
+    }
 }
--- src/main/java/konkuk/thip/attendancecheck/application/port/out/AttendanceCheckCommandPort.java
+++ src/main/java/konkuk/thip/attendancecheck/application/port/out/AttendanceCheckCommandPort.java
@@ -1,6 +1,8 @@
 package konkuk.thip.attendancecheck.application.port.out;
 
+import konkuk.thip.attendancecheck.domain.AttendanceCheck;
 
 public interface AttendanceCheckCommandPort {
 
+    Long save(AttendanceCheck attendanceCheck);
 }
