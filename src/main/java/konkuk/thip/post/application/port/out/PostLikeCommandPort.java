64987888fdb74d4524b9e758c17c931c241ce85e
강희진 <kanghuijin12@gmail.com>
2025-08-10 17:35:37 +0900

    [feat] 게시물id로 게시물좋아요 일괄 삭제 함수 추가작성 (#160)

--- src/main/java/konkuk/thip/post/application/port/out/PostLikeCommandPort.java
+++ src/main/java/konkuk/thip/post/application/port/out/PostLikeCommandPort.java
@@ -5,4 +5,5 @@ import konkuk.thip.common.post.PostType;
 public interface PostLikeCommandPort {
     void save(Long userId, Long postId, PostType postType);
     void delete(Long userId, Long postId);
+    void deleteAllByPostId(Long postId);
 }
