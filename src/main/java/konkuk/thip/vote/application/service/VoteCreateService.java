dad52a6d5a3e17018d11d9f872499827e7dd7978
seongjunnoh <joon1754@naver.com>
2025-08-10 17:58:25 +0900

    [refactor] 투표 생성 시 userPercentage, roomPercentage 값 업데이트하도록 로직 수정 (#181)
    - 투표 생성한 사람의 userPercentage 값 수정
    - 해당 방의 roomPercentage 값 수정 (기록 생성 서비스 코드 참고하였습니다)

--- src/main/java/konkuk/thip/vote/application/service/VoteCreateService.java
+++ src/main/java/konkuk/thip/vote/application/service/VoteCreateService.java
@@ -3,7 +3,10 @@ package konkuk.thip.vote.application.service;
 import konkuk.thip.book.application.port.out.BookCommandPort;
 import konkuk.thip.book.domain.Book;
 import konkuk.thip.room.application.port.out.RoomCommandPort;
+import konkuk.thip.room.application.port.out.RoomParticipantCommandPort;
+import konkuk.thip.room.application.service.validator.RoomParticipantValidator;
 import konkuk.thip.room.domain.Room;
+import konkuk.thip.room.domain.RoomParticipant;
 import konkuk.thip.vote.application.port.in.VoteCreateUseCase;
 import konkuk.thip.vote.application.port.in.dto.VoteCreateCommand;
 import konkuk.thip.vote.application.port.in.dto.VoteCreateResult;
@@ -22,14 +25,17 @@ import java.util.List;
 @Slf4j
 public class VoteCreateService implements VoteCreateUseCase {
 
+    private final RoomParticipantValidator roomParticipantValidator;
+    private final RoomParticipantCommandPort roomParticipantCommandPort;
     private final VoteCommandPort voteCommandPort;
     private final RoomCommandPort roomCommandPort;
     private final BookCommandPort bookCommandPort;
 
     @Transactional
     @Override
-    //todo UserRoom 업데이트 로직 추가 필요!!
     public VoteCreateResult createVote(VoteCreateCommand command) {
+        roomParticipantValidator.validateUserIsRoomMember(command.roomId(), command.userId());
+
         // 1. validate
         Vote vote = Vote.withoutId(
                 command.content(),
@@ -39,7 +45,9 @@ public class VoteCreateService implements VoteCreateUseCase {
                 command.roomId()
         );
 
-        validateVote(vote);
+        Room room = roomCommandPort.getByIdOrThrow(vote.getRoomId());
+        Book book = bookCommandPort.findById(room.getBookId());
+        validateVote(vote, room, book);
 
         // 2. vote 저장
         Long savedVoteId = voteCommandPort.saveVote(vote);
@@ -54,13 +62,31 @@ public class VoteCreateService implements VoteCreateUseCase {
                 .toList();
         voteCommandPort.saveAllVoteItems(voteItems);
 
+        // 4. RoomParticipant 정보 update
+        updateRoomProgress(vote, room, book);
+
         return VoteCreateResult.of(savedVoteId, command.roomId());
     }
 
-    private void validateVote(Vote vote) {
-        Room room = roomCommandPort.getByIdOrThrow(vote.getRoomId());
-        Book book = bookCommandPort.findById(room.getBookId());
+    private void updateRoomProgress(Vote vote, Room room, Book book) {
+        RoomParticipant roomParticipant = roomParticipantCommandPort.getByUserIdAndRoomIdOrThrow(vote.getCreatorId(), vote.getRoomId());
 
+        if(roomParticipant.updateUserProgress(vote.getPage(), book.getPageCount())) {
+            // userPercentage가 업데이트되었으면 Room의 roomPercentage 업데이트
+            List<RoomParticipant> roomParticipantList = roomParticipantCommandPort.findAllByRoomId(vote.getRoomId());
+            Double totalUserPercentage = roomParticipantList.stream()
+                    .filter(participant -> !roomParticipant.getId().equals(participant.getId())) // 현재 업데이트 중인 사용자 제외
+                    .map(RoomParticipant::getUserPercentage)
+                    .reduce(0.0, Double::sum);
+            totalUserPercentage += roomParticipant.getUserPercentage();
+            room.updateRoomPercentage(totalUserPercentage / roomParticipantList.size());
+        }
+
+        roomCommandPort.update(room);
+        roomParticipantCommandPort.update(roomParticipant);
+    }
+
+    private void validateVote(Vote vote, Room room, Book book) {
         // 페이지 유효성 검증
         vote.validatePage(book.getPageCount());
 
