163ec4f36180652d9b718483755204a84b727e72
강희진 <kanghuijin12@gmail.com>
2025-08-10 17:33:36 +0900

    [refactor] @SQLDelete 어노테이션 삭제 및 명시적 status업데이트로 소프트 딜리트 전략 수정 (#160)

--- src/main/java/konkuk/thip/post/adapter/out/jpa/PostJpaEntity.java
+++ src/main/java/konkuk/thip/post/adapter/out/jpa/PostJpaEntity.java
@@ -3,14 +3,18 @@ package konkuk.thip.post.adapter.out.jpa;
 import jakarta.persistence.*;
 import konkuk.thip.comment.adapter.out.jpa.CommentJpaEntity;
 import konkuk.thip.common.entity.BaseJpaEntity;
+import konkuk.thip.common.entity.StatusType;
+import konkuk.thip.common.exception.InvalidStateException;
 import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static konkuk.thip.common.entity.StatusType.INACTIVE;
+import static konkuk.thip.common.exception.code.ErrorCode.POST_ALREADY_DELETED;
 
 @Entity
 @Table(name = "posts")
@@ -18,7 +22,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "dtype")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE posts SET status = 'INACTIVE' WHERE post_id = ?")
 public abstract class PostJpaEntity extends BaseJpaEntity {
 
     @Id
@@ -49,11 +52,17 @@ public abstract class PostJpaEntity extends BaseJpaEntity {
     @OneToMany(mappedBy = "postJpaEntity", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<PostLikeJpaEntity> postLikeList = new ArrayList<>();
 
-
     public PostJpaEntity(String content, Integer likeCount, Integer commentCount, UserJpaEntity userJpaEntity) {
         this.content = content;
         this.likeCount = likeCount;
         this.commentCount = commentCount;
         this.userJpaEntity = userJpaEntity;
+    }
+
+    public void softDelete() {
+        if(this.status.equals(INACTIVE)){
+            throw new InvalidStateException(POST_ALREADY_DELETED);
+        }
+        this.status = INACTIVE;
     }
 }
