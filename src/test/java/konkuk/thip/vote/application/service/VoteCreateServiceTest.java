d8674b95f56e7a6a973a390183ef76220cf206bd
seongjunnoh <joon1754@naver.com>
2025-08-10 17:59:39 +0900

    [test] 투표 생성 시 DB에 userPercentage, roomPercentage 값 잘 업데이트 되는지 확인하는 서비스 통합 테스트 코드 작성 (#181)

---
+++ src/test/java/konkuk/thip/vote/application/service/VoteCreateServiceTest.java
@@ -0,0 +1,114 @@
+package konkuk.thip.vote.application.service;
+
+import konkuk.thip.book.adapter.out.jpa.BookJpaEntity;
+import konkuk.thip.book.adapter.out.persistence.repository.BookJpaRepository;
+import konkuk.thip.common.util.TestEntityFactory;
+import konkuk.thip.room.adapter.out.jpa.CategoryJpaEntity;
+import konkuk.thip.room.adapter.out.jpa.RoomJpaEntity;
+import konkuk.thip.room.adapter.out.jpa.RoomParticipantJpaEntity;
+import konkuk.thip.room.adapter.out.jpa.RoomParticipantRole;
+import konkuk.thip.room.adapter.out.persistence.repository.RoomJpaRepository;
+import konkuk.thip.room.adapter.out.persistence.repository.category.CategoryJpaRepository;
+import konkuk.thip.room.adapter.out.persistence.repository.roomparticipant.RoomParticipantJpaRepository;
+import konkuk.thip.user.adapter.out.jpa.AliasJpaEntity;
+import konkuk.thip.user.adapter.out.jpa.UserJpaEntity;
+import konkuk.thip.user.adapter.out.persistence.repository.UserJpaRepository;
+import konkuk.thip.user.adapter.out.persistence.repository.alias.AliasJpaRepository;
+import konkuk.thip.vote.adapter.out.persistence.repository.VoteItemJpaRepository;
+import konkuk.thip.vote.adapter.out.persistence.repository.VoteJpaRepository;
+import konkuk.thip.vote.application.port.in.dto.VoteCreateCommand;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
+@DisplayName("[단위] 투표 생성 service 단위 테스트")
+class VoteCreateServiceTest {
+
+    @Autowired
+    private AliasJpaRepository aliasJpaRepository;
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @Autowired
+    private CategoryJpaRepository categoryJpaRepository;
+
+    @Autowired
+    private BookJpaRepository bookJpaRepository;
+
+    @Autowired
+    private RoomJpaRepository roomJpaRepository;
+
+    @Autowired
+    private RoomParticipantJpaRepository roomParticipantJpaRepository;
+
+    @Autowired
+    private VoteJpaRepository voteJpaRepository;
+
+    @Autowired
+    private VoteItemJpaRepository voteItemJpaRepository;
+
+    @Autowired
+    private VoteCreateService voteCreateService;
+
+    @AfterEach
+    void tearDown() {
+        voteItemJpaRepository.deleteAllInBatch();
+        voteJpaRepository.deleteAllInBatch();
+        roomParticipantJpaRepository.deleteAllInBatch();
+        roomJpaRepository.deleteAllInBatch();
+        bookJpaRepository.deleteAllInBatch();
+        userJpaRepository.deleteAllInBatch();
+        categoryJpaRepository.deleteAllInBatch();
+        aliasJpaRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("유저가 투표를 생성하면, 해당 유저의 [RoomParticipant의 currentPage, userPercentage]와 해당 방의 [Room의 roomPercentage] 값이 변경된다.")
+    void vote_create_room_participant_and_room_percentage_update() throws Exception {
+        //given
+        AliasJpaEntity alias = aliasJpaRepository.save(TestEntityFactory.createScienceAlias());
+        UserJpaEntity me = userJpaRepository.save(TestEntityFactory.createUser(alias, "me"));
+
+        BookJpaEntity book = bookJpaRepository.save(TestEntityFactory.createBook(369));
+        CategoryJpaEntity category = categoryJpaRepository.save(TestEntityFactory.createScienceCategory(alias));
+        RoomJpaEntity room = roomJpaRepository.save(TestEntityFactory.createRoom(book, category));
+
+        roomParticipantJpaRepository.save(TestEntityFactory.createRoomParticipant(room, me, RoomParticipantRole.MEMBER, 0.0));
+
+        UserJpaEntity user = userJpaRepository.save(TestEntityFactory.createUser(alias, "user"));
+        roomParticipantJpaRepository.save(TestEntityFactory.createRoomParticipant(room, user, RoomParticipantRole.MEMBER, 7.7));      // userPercentage = 7.7 인 RoomParticipant 생성
+
+        //when
+        List<VoteCreateCommand.VoteItemCreateCommand> voteItems = List.of(
+                new VoteCreateCommand.VoteItemCreateCommand("투표 항목1"),
+                new VoteCreateCommand.VoteItemCreateCommand("투표 항목2")
+        );
+
+        VoteCreateCommand command = new VoteCreateCommand(
+                me.getUserId(), room.getRoomId(), 89, false, "투표 내용", voteItems
+        );
+
+        //then
+        voteCreateService.createVote(command);
+
+        //then
+        RoomParticipantJpaEntity roomParticipant = roomParticipantJpaRepository.findByUserIdAndRoomId(me.getUserId(), room.getRoomId()).get();
+        RoomJpaEntity refreshRoom = roomJpaRepository.findAll().get(0);
+
+        // userPercentage, roomPercentage 값 update 확인
+        assertThat(roomParticipant.getUserPercentage()).isEqualTo((double) 89 / 369 * 100);
+        assertThat(refreshRoom.getRoomPercentage()).isEqualTo((7.7 + (double) 89 / 369 * 100) / 2);
+    }
+}
