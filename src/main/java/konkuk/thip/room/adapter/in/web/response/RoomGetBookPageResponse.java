38ccc33f9ea983bf3ddf49a78d848662e66a4134
janghyunjun <buzz03312@naver.com>
2025-08-11 01:01:23 +0900

    [hotfix] 총평 여부 api에서 최근 읽은 책 페이지 반환 (#184)

--- src/main/java/konkuk/thip/room/adapter/in/web/response/RoomGetBookPageResponse.java
+++ src/main/java/konkuk/thip/room/adapter/in/web/response/RoomGetBookPageResponse.java
@@ -2,10 +2,11 @@ package konkuk.thip.room.adapter.in.web.response;
 
 public record RoomGetBookPageResponse(
         int totalBookPage,
+        int recentBookPage,
         boolean isOverviewPossible,
         Long roomId
 ) {
-    public static RoomGetBookPageResponse of(int totalBookPage, boolean isOverviewPossible, Long roomId) {
-        return new RoomGetBookPageResponse(totalBookPage, isOverviewPossible, roomId);
+    public static RoomGetBookPageResponse of(int totalBookPage, int recentBookPage, boolean isOverviewPossible, Long roomId) {
+        return new RoomGetBookPageResponse(totalBookPage, recentBookPage, isOverviewPossible, roomId);
     }
 }
