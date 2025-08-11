9ace812d9d130262f7c50a30deac06bff14f8b14
강희진 <kanghuijin12@gmail.com>
2025-08-11 17:06:09 +0900

    [feat] VoteParticipantJpaRepository.deleteAllByVoteId 작성 (#188)

--- src/main/java/konkuk/thip/vote/adapter/out/persistence/repository/VoteParticipantJpaRepository.java
+++ src/main/java/konkuk/thip/vote/adapter/out/persistence/repository/VoteParticipantJpaRepository.java
@@ -2,7 +2,9 @@ package konkuk.thip.vote.adapter.out.persistence.repository;
 
 import konkuk.thip.vote.adapter.out.jpa.VoteParticipantJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,8 @@ import java.util.Optional;
 public interface VoteParticipantJpaRepository extends JpaRepository<VoteParticipantJpaEntity, Long>, VoteParticipantQueryRepository {
     @Query("SELECT vp FROM VoteParticipantJpaEntity vp WHERE vp.userJpaEntity.userId = :userId AND vp.voteItemJpaEntity.voteItemId = :voteItemId")
     Optional<VoteParticipantJpaEntity> findVoteParticipantByUserIdAndVoteItemId(Long userId, Long voteItemId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM VoteParticipantJpaEntity vp WHERE vp.voteItemJpaEntity.voteJpaEntity.postId = :voteId")
+    void deleteAllByVoteId(@Param("voteId") Long voteId);
 }
