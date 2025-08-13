d02b355c7db016abdb227f3237d13b2cecc2b24d
seongjunnoh <joon1754@naver.com>
2025-08-13 17:37:30 +0900

    [refactor] 바뀐 요구사항에 따라 오늘의 한마디 작성 api service 코드 수정 (#171)
    - 유저가 이미 작성한 오늘의 한마디 개수를 조회하는 코드 추가

--- src/main/java/konkuk/thip/room/application/service/AttendanceCheckCreateService.java
+++ src/main/java/konkuk/thip/room/application/service/AttendanceCheckCreateService.java
@@ -2,7 +2,9 @@ package konkuk.thip.room.application.service;
 
 import konkuk.thip.room.application.port.in.AttendanceCheckCreateUseCase;
 import konkuk.thip.room.application.port.in.dto.AttendanceCheckCreateCommand;
+import konkuk.thip.room.application.port.in.dto.AttendanceCheckCreateResult;
 import konkuk.thip.room.application.port.out.AttendanceCheckCommandPort;
+import konkuk.thip.room.application.port.out.AttendanceCheckQueryPort;
 import konkuk.thip.room.domain.AttendanceCheck;
 import konkuk.thip.room.application.service.validator.RoomParticipantValidator;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +17,25 @@ public class AttendanceCheckCreateService implements AttendanceCheckCreateUseCas
 
     private final RoomParticipantValidator roomParticipantValidator;
     private final AttendanceCheckCommandPort attendanceCheckCommandPort;
+    private final AttendanceCheckQueryPort attendanceCheckQueryPort;
 
     @Transactional
     @Override
-    public Long create(AttendanceCheckCreateCommand command) {
+    public AttendanceCheckCreateResult create(AttendanceCheckCreateCommand command) {
         // 1. 유저가 해당 방에 오늘의 한마디를 작성할 수 있는지 검증
         roomParticipantValidator.validateUserIsRoomMember(command.roomId(), command.creatorId());
 
-        // 2. 출석체크 도메인 생성 및 저장
-        AttendanceCheck attendanceCheck = AttendanceCheck.withoutId(command.roomId(), command.creatorId(), command.content());
-        return attendanceCheckCommandPort.save(attendanceCheck);
+        // 2. 유저가 오늘 이미 작성한 오늘의 한마디 개수 조회
+        int alreadyWrittenCountToday = attendanceCheckQueryPort.countAttendanceChecksOnTodayByUser(command.creatorId());
+
+        // 3. 출석체크 도메인 생성 및 저장
+        AttendanceCheck attendanceCheck = AttendanceCheck.withoutId(command.roomId(), command.creatorId(), command.content(), alreadyWrittenCountToday);
+        Long savedId = attendanceCheckCommandPort.save(attendanceCheck);
+
+        return AttendanceCheckCreateResult.builder()
+                .attendanceCheckId(savedId)
+                .roomId(command.roomId())
+                .todayWriteCountOfUser(alreadyWrittenCountToday + 1)
+                .build();
     }
 }
