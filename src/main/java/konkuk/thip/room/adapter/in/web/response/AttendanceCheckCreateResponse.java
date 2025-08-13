cdeff6fb703c9c461f93aac556ec24ce214dd1ac
seongjunnoh <joon1754@naver.com>
2025-08-13 17:40:54 +0900

    [refactor] 바뀐 요구사항에 따라 response dto 수정 (#171)
    - roomId, isFirstWrite 필드 추가

--- src/main/java/konkuk/thip/room/adapter/in/web/response/AttendanceCheckCreateResponse.java
+++ src/main/java/konkuk/thip/room/adapter/in/web/response/AttendanceCheckCreateResponse.java
@@ -1,9 +1,20 @@
 package konkuk.thip.room.adapter.in.web.response;
 
+import konkuk.thip.room.application.port.in.dto.AttendanceCheckCreateResult;
+
 public record AttendanceCheckCreateResponse(
-        Long attendanceCheckId
+        Long attendanceCheckId,
+        Long roomId,
+        boolean isFirstWrite
 ) {
-    public static AttendanceCheckCreateResponse of(Long attendanceCheckId) {
-        return new AttendanceCheckCreateResponse(attendanceCheckId);
+    public static AttendanceCheckCreateResponse of(AttendanceCheckCreateResult result) {
+        boolean isFirstWrite = false;
+        if (result.todayWriteCountOfUser() == 1) isFirstWrite = true;
+
+        return new AttendanceCheckCreateResponse(
+                result.attendanceCheckId(),
+                result.roomId(),
+                isFirstWrite
+        );
     }
 }
