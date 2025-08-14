de637f7df0c1a3c32e430051243e2924c1098e5d
janghyunjun <buzz03312@naver.com>
2025-08-14 15:44:23 +0900

    [fix] nickname nullable 불가 (#210)

--- src/main/java/konkuk/thip/user/adapter/in/web/request/UserSignupRequest.java
+++ src/main/java/konkuk/thip/user/adapter/in/web/request/UserSignupRequest.java
@@ -13,6 +13,7 @@ public record UserSignupRequest(
         String aliasName,
 
         @Schema(description = "사용자 닉네임", example = "홍길동_123")
+        @NotBlank(message = "닉네임은 필수입니다.")
         @Pattern(regexp = "[가-힣a-zA-Z0-9]+", message = "닉네임은 한글, 영어, 숫자로만 구성되어야 합니다.(공백불가)")
         @Size(max = 10, message = "닉네임은 최대 10자 입니다.")
         String nickname
