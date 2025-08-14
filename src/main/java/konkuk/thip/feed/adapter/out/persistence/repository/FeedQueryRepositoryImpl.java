acaf637bb6beac06c6068701c9ba64ae0db082be
seongjunnoh <joon1754@naver.com>
2025-08-15 00:28:06 +0900

    [refactor] 피드 관련 QueryDSL 코드에서 isWriter 관련 코드 삭제 (#220)

--- src/main/java/konkuk/thip/feed/adapter/out/persistence/repository/FeedQueryRepositoryImpl.java
+++ src/main/java/konkuk/thip/feed/adapter/out/persistence/repository/FeedQueryRepositoryImpl.java
@@ -79,7 +79,7 @@ public class FeedQueryRepositoryImpl implements FeedQueryRepository {
 
         // 3) DTO 변환
         return ordered.stream()
-                .map(e -> toDto(e, priorityMap.get(e.getPostId()),userId))
+                .map(e -> toDto(e, priorityMap.get(e.getPostId())))
                 .toList();
     }
 
@@ -101,7 +101,7 @@ public class FeedQueryRepositoryImpl implements FeedQueryRepository {
 
         // 3) DTO 변환 (priority 없음)
         return ordered.stream()
-                .map(e -> toDto(e, null, userId))
+                .map(e -> toDto(e, null))
                 .toList();
     }
 
@@ -192,12 +192,12 @@ public class FeedQueryRepositoryImpl implements FeedQueryRepository {
 
         // 3) DTO 변환 (priority 없음)
         return ordered.stream()
-                .map(e -> toDto(e, null, userId))
+                .map(e -> toDto(e, null))
                 .toList();
     }
 
     @Override
-    public List<FeedQueryDto> findSpecificUserFeedsByCreatedAt(Long userId, Long feedOwnerId, LocalDateTime lastCreatedAt, int size) {
+    public List<FeedQueryDto> findSpecificUserFeedsByCreatedAt(Long feedOwnerId, LocalDateTime lastCreatedAt, int size) {
         // 1. 특정 유저 피드 ID 목록만 최신순 페이징 조회
         List<Long> feedIds = fetchSpecificUserFeedIdsByCreatedAt(feedOwnerId, lastCreatedAt, size);
 
@@ -211,7 +211,7 @@ public class FeedQueryRepositoryImpl implements FeedQueryRepository {
 
         // 3) DTO 변환 (priority 없음)
         return ordered.stream()
-                .map(e -> toDto(e, null, userId))
+                .map(e -> toDto(e, null))
                 .toList();
     }
 
@@ -259,12 +259,11 @@ public class FeedQueryRepositoryImpl implements FeedQueryRepository {
                 .fetch();
     }
 
-    private FeedQueryDto toDto(FeedJpaEntity e, Integer priority, Long userId) {
+    private FeedQueryDto toDto(FeedJpaEntity e, Integer priority) {
         String[] urls = e.getContentList().stream()
                 .map(ContentJpaEntity::getContentUrl)
                 .toArray(String[]::new);
         boolean isPriorityFeed = (priority != null && priority == 1);
-        boolean isWriter = e.getUserJpaEntity().getUserId().equals(userId);
 
         return FeedQueryDto.builder()
                 .feedId(e.getPostId())
@@ -281,7 +280,6 @@ public class FeedQueryRepositoryImpl implements FeedQueryRepository {
                 .likeCount(e.getLikeCount())
                 .commentCount(e.getCommentCount())
                 .isPublic(e.getIsPublic())
-                .isWriter(isWriter)
                 .isPriorityFeed(isPriorityFeed)
                 .build();
     }
