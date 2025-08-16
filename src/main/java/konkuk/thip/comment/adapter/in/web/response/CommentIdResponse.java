7e18927e12bfa9114d59c0e94813462eb5c73a36
강희진 <kanghuijin12@gmail.com>
2025-08-16 19:30:48 +0900

    [hotfix] 댓글 생성시 댓글 조회와 똑같은 형식으로 response 반환 (#228)

--- src/main/java/konkuk/thip/comment/adapter/in/web/response/CommentIdResponse.java
+++
@@ -1,7 +0,0 @@
-package konkuk.thip.comment.adapter.in.web.response;
-
-public record CommentIdResponse(Long commentId) {
-    public static CommentIdResponse of(Long commentId) {
-        return new CommentIdResponse(commentId);
-    }
-}
