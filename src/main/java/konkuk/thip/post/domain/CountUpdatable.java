e29f42ba6b1ca61a4096aadf9e955bdc3bb5b48a
seongjunnoh <joon1754@naver.com>
2025-08-16 17:20:42 +0900

    [docs] todo 주석 삭제 (#227)

--- src/main/java/konkuk/thip/post/domain/CountUpdatable.java
+++ src/main/java/konkuk/thip/post/domain/CountUpdatable.java
@@ -2,7 +2,7 @@ package konkuk.thip.post.domain;
 
 import konkuk.thip.post.domain.service.PostCountService;
 
-public interface CountUpdatable { //TODO 패키지 구조 충돌안나게 한번에 옮기기
+public interface CountUpdatable {
     void increaseCommentCount();
     void decreaseCommentCount();
     void updateLikeCount(PostCountService postCountService, boolean isLike);
