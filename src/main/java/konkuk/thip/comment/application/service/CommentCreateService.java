1525d704ed3b4d8eab7b948584fb4c1579c59235
강희진 <kanghuijin12@gmail.com>
2025-08-16 19:30:33 +0900

    [hotfix] 댓글 생성시 댓글 조회와 똑같은 형식으로 response 반환 (#228)

--- src/main/java/konkuk/thip/comment/application/service/CommentCreateService.java
+++ src/main/java/konkuk/thip/comment/application/service/CommentCreateService.java
@@ -1,8 +1,13 @@
 package konkuk.thip.comment.application.service;
 
+import konkuk.thip.comment.adapter.in.web.response.CommentCreateResponse;
+import konkuk.thip.comment.application.mapper.CommentQueryMapper;
 import konkuk.thip.comment.application.port.in.CommentCreateUseCase;
 import konkuk.thip.comment.application.port.in.dto.CommentCreateCommand;
 import konkuk.thip.comment.application.port.out.CommentCommandPort;
+import konkuk.thip.comment.application.port.out.CommentLikeQueryPort;
+import konkuk.thip.comment.application.port.out.CommentQueryPort;
+import konkuk.thip.comment.application.port.out.dto.CommentQueryDto;
 import konkuk.thip.comment.application.service.validator.CommentAuthorizationValidator;
 import konkuk.thip.comment.domain.Comment;
 import konkuk.thip.common.exception.InvalidStateException;
@@ -21,13 +26,17 @@ import static konkuk.thip.common.exception.code.ErrorCode.INVALID_COMMENT_CREATE
 public class CommentCreateService implements CommentCreateUseCase {
 
     private final CommentCommandPort commentCommandPort;
+    private final CommentQueryPort commentQueryPort;
+    private final CommentLikeQueryPort commentLikeQueryPort;
+    private final CommentQueryMapper commentQueryMapper;
+
 
     private final PostHandler postHandler;
     private final CommentAuthorizationValidator commentAuthorizationValidator;
 
     @Override
     @Transactional
-    public Long createComment(CommentCreateCommand command) {
+    public CommentCreateResponse createComment(CommentCreateCommand command) {
 
         // 1. 댓글/답글 생성 선행검증 및 작성하려는 게시글 타입 검증
         Comment.validateCommentCreate(command.isReplyRequest(), command.parentId());
@@ -42,7 +51,7 @@ public class CommentCreateService implements CommentCreateUseCase {
         // TODO 기록 및 투표: 모임방의 내 게시글에 대한 댓글, 내 댓글에 대한 답글 알림 전송
 
         // 3. 댓글 생성
-        Long commentId = createCommentDomain(command);
+        Long savedCommentId = createCommentDomain(command);
 
         //TODO 게시물의 댓글 수 증가/감소 동시성 제어 로직 추가해야됨
 
@@ -52,7 +61,20 @@ public class CommentCreateService implements CommentCreateUseCase {
         // 4-2 Jpa엔티티 게시물 댓글 수 증가
         postHandler.updatePost(type, post);
 
-        return commentId;
+        // 5. 매퍼로 DTO 변환 후 반환
+        if (command.isReplyRequest()) {
+            // 부모 댓글 조회
+            CommentQueryDto parentCommentDto = commentQueryPort.findRootCommentById(command.parentId());
+            // 사용자 부모 댓글 좋아요 여부 조회
+            boolean isLikedParentComment = commentLikeQueryPort.isLikedCommentByUser(command.userId(),parentCommentDto.commentId());
+
+            CommentQueryDto savedReplyCommentDto = commentQueryPort.findChildCommentById(command.parentId(), savedCommentId);
+            return commentQueryMapper.toRootCommentResponseWithChildren(parentCommentDto, savedReplyCommentDto,isLikedParentComment,command.userId());
+        } else {
+            CommentQueryDto savedCommentDto = commentQueryPort.findRootCommentById(savedCommentId);
+            return commentQueryMapper.toRoot(savedCommentDto, false, command.userId());
+        }
+
     }
 
     private Long createCommentDomain(CommentCreateCommand command) {
