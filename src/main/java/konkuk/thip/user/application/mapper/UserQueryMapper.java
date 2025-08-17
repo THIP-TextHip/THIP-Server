6cb7f0a5cb612a82c7fd6be5a803f48c6328b882
seongjunnoh <joon1754@naver.com>
2025-08-18 04:13:44 +0900

    [refactor] 전체 피드 조회의 내 띱 목록 조회 api의 response 매핑을 위한 매퍼 코드 수정 (#213)

--- src/main/java/konkuk/thip/user/application/mapper/UserQueryMapper.java
+++ src/main/java/konkuk/thip/user/application/mapper/UserQueryMapper.java
@@ -1,9 +1,11 @@
 package konkuk.thip.user.application.mapper;
 
-import konkuk.thip.user.adapter.in.web.response.UserFollowingRecentWritersResponse;
 import konkuk.thip.user.adapter.in.web.response.UserSearchResponse;
+import konkuk.thip.user.adapter.in.web.response.UserShowFollowingsInFeedViewResponse;
+import konkuk.thip.user.application.port.out.dto.FollowingQueryDto;
 import konkuk.thip.user.application.port.out.dto.UserQueryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -13,12 +15,12 @@ public interface UserQueryMapper {
     // List<QueryDto> -> List<DTO>
     List<UserSearchResponse.UserSearchDto> toUserDtoList(List<UserQueryDto> userQueryDtos);
 
-    // 단건 매핑: UserQueryDto -> RecentWriter
-    UserFollowingRecentWritersResponse.RecentWriter toRecentWriter(UserQueryDto dto);
-    // 리스트 매핑: List<UserQueryDto> -> List<RecentWriter>
-    List<UserFollowingRecentWritersResponse.RecentWriter> toRecentWriterList(List<UserQueryDto> dtos);
-    // 래핑: List<UserQueryDto> -> UserFollowingRecentWritersResponse
-    default UserFollowingRecentWritersResponse toRecentWriterResponses(List<UserQueryDto> dtos) {
-        return new  UserFollowingRecentWritersResponse(toRecentWriterList(dtos));
-    }
+    @Mapping(target = "userId", source = "dto.followingTargetUserId")
+    @Mapping(target = "nickname", source = "dto.followingUserNickname")
+    @Mapping(target = "profileImageUrl", source = "dto.followingUserProfileImageUrl")
+    UserShowFollowingsInFeedViewResponse.UserShowFollowingsInFeedViewDto toFollowingFeedViewDto(FollowingQueryDto dto);
+
+    List<UserShowFollowingsInFeedViewResponse.UserShowFollowingsInFeedViewDto> toFollowingFeedViewDtos(
+            List<FollowingQueryDto> dtos
+    );
 }
