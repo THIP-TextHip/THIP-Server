24df5b855128cef3c7419915897870c240db2015
강희진 <kanghuijin12@gmail.com>
2025-08-19 16:32:24 +0900

    [refactor] 오늘의 한마디 작성 response 수정 (#256)

--- src/main/java/konkuk/thip/roompost/application/service/AttendanceCheckCreateService.java
+++ src/main/java/konkuk/thip/roompost/application/service/AttendanceCheckCreateService.java
@@ -1,5 +1,6 @@
 package konkuk.thip.roompost.application.service;
 
+import konkuk.thip.common.util.DateUtil;
 import konkuk.thip.roompost.application.port.in.AttendanceCheckCreateUseCase;
 import konkuk.thip.roompost.application.port.in.dto.attendancecheck.AttendanceCheckCreateCommand;
 import konkuk.thip.roompost.application.port.in.dto.attendancecheck.AttendanceCheckCreateResult;
@@ -7,6 +8,8 @@ import konkuk.thip.roompost.application.port.out.AttendanceCheckCommandPort;
 import konkuk.thip.roompost.application.port.out.AttendanceCheckQueryPort;
 import konkuk.thip.roompost.domain.AttendanceCheck;
 import konkuk.thip.room.application.service.validator.RoomParticipantValidator;
+import konkuk.thip.user.application.port.out.UserCommandPort;
+import konkuk.thip.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,12 +20,16 @@ public class AttendanceCheckCreateService implements AttendanceCheckCreateUseCas
 
     private final RoomParticipantValidator roomParticipantValidator;
     private final AttendanceCheckCommandPort attendanceCheckCommandPort;
+    private final UserCommandPort userCommandPort;
     private final AttendanceCheckQueryPort attendanceCheckQueryPort;
 
     @Transactional
     @Override
     public AttendanceCheckCreateResult create(AttendanceCheckCreateCommand command) {
-        // 1. 유저가 해당 방에 오늘의 한마디를 작성할 수 있는지 검증
+
+        // 1. 유저 검증 및 조회
+        User user = userCommandPort.findById(command.creatorId());
+        // 1-1. 유저가 해당 방에 오늘의 한마디를 작성할 수 있는지 검증
         roomParticipantValidator.validateUserIsRoomMember(command.roomId(), command.creatorId());
 
         // 2. 유저가 해당 방에서 오늘 이미 작성한 오늘의 한마디 개수 조회
@@ -30,12 +37,19 @@ public class AttendanceCheckCreateService implements AttendanceCheckCreateUseCas
 
         // 3. 출석체크 도메인 생성 및 저장
         AttendanceCheck attendanceCheck = AttendanceCheck.withoutId(command.roomId(), command.creatorId(), command.content(), alreadyWrittenCountToday);
-        Long savedId = attendanceCheckCommandPort.save(attendanceCheck);
+        AttendanceCheck SavedattendanceCheck = attendanceCheckCommandPort.getByIdOrThrow(attendanceCheckCommandPort.save(attendanceCheck));
 
         return AttendanceCheckCreateResult.builder()
-                .attendanceCheckId(savedId)
                 .roomId(command.roomId())
+                .attendanceCheckId(SavedattendanceCheck.getId())
+                .creatorId(command.creatorId())
+                .creatorNickname(user.getNickname())
+                .creatorProfileImageUrl(user.getAlias().getImageUrl())
+                .todayComment(command.content())
+                .postDate(DateUtil.formatBeforeTime(SavedattendanceCheck.getCreatedAt()))
+                .date(SavedattendanceCheck.getCreatedAt().toLocalDate())
                 .todayWriteCountOfUser(alreadyWrittenCountToday + 1)
+                .isWriter(true)
                 .build();
     }
 }
