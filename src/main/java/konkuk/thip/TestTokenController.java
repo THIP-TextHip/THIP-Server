e3cdaa47da0102815ad047ca5c4de5b233a9f896
seongjunnoh <joon1754@naver.com>
2025-08-24 18:03:32 +0900

    [chore] 로컬환경에서만 access token 발급을 위한 api를 활용할 수 있도록 property 설정 추가 (#276)
    - 테스트 api 용 controller는 property의 값이 true 인 경우에만 접근 가능

--- src/main/java/konkuk/thip/TestTokenController.java
+++ src/main/java/konkuk/thip/TestTokenController.java
@@ -2,12 +2,14 @@ package konkuk.thip;
 
 import konkuk.thip.common.security.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "thip.test-api.enabled", havingValue = "true")
 public class TestTokenController {
 
     private final JwtUtil jwtUtil;
