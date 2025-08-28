3f86f012ee5e78bf69d219c35bc9bbb7d6cc8c64
강희진 <kanghuijin12@gmail.com>
2025-08-28 23:04:14 +0900

    [refactor] [모임홈] 진행중인 방 조회시 페이지기반 무한스크롤을 커서기반 무한스크롤로 수정  RoomParticipantQueryDto 작성 (#283)

---
+++ src/main/java/konkuk/thip/room/application/port/out/dto/RoomParticipantQueryDto.java
@@ -0,0 +1,27 @@
+package konkuk.thip.room.application.port.out.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+
+@Builder
+public record RoomParticipantQueryDto(
+        Long roomId,
+        String bookImageUrl,
+        String roomTitle,
+        Integer memberCount,
+        Double userPercentage,
+        LocalDate startDate   // 방 진행 시작일
+) {
+    @QueryProjection
+    public RoomParticipantQueryDto {
+        Assert.notNull(roomId, "roomId must not be null");
+        Assert.notNull(bookImageUrl, "bookImageUrl must not be null");
+        Assert.notNull(roomTitle, "roomName must not be null");
+        Assert.notNull(memberCount, "memberCount must not be null");
+        Assert.notNull(userPercentage, "userPercentage must not be null");
+        Assert.notNull(startDate, "startDate must not be null");
+    }
+}
