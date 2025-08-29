1d39c2069e1a4e09dce93db754913b1fae928287
janghyunjun <buzz03312@naver.com>
2025-08-29 16:48:06 +0900

    [refactor] 쿼리 주석 변경 (#282)

--- src/main/resources/db/migration/V250828__Add_room_status.sql
+++ src/main/resources/db/migration/V250828__Add_room_status.sql
@@ -3,7 +3,7 @@ ALTER TABLE rooms
     ADD COLUMN room_status VARCHAR(32) NOT NULL DEFAULT 'RECRUITING'
         COMMENT 'Room 상태: RECRUITING/IN_PROGRESS/EXPIRED';
 
--- 2) start_date가 현재 날짜보다 "이후"이면 IN_PROGRESS로 업데이트
+-- 2) start_date가 현재 날짜보다 "이전"이면 IN_PROGRESS로 업데이트
 UPDATE rooms
 SET room_status = 'IN_PROGRESS'
 WHERE start_date <= CURDATE();
