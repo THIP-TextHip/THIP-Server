ce079e79ebed37e5532e3fcd55c6388da8883d06
강희진 <kanghuijin12@gmail.com>
2025-08-30 21:42:30 +0900

    [refactor] s3CommandPort 삭제 (#286)

--- src/main/java/konkuk/thip/feed/application/service/FeedUpdateService.java
+++ src/main/java/konkuk/thip/feed/application/service/FeedUpdateService.java
@@ -3,7 +3,6 @@ package konkuk.thip.feed.application.service;
 import konkuk.thip.feed.application.port.in.FeedUpdateUseCase;
 import konkuk.thip.feed.application.port.in.dto.FeedUpdateCommand;
 import konkuk.thip.feed.application.port.out.FeedCommandPort;
-import konkuk.thip.feed.application.port.out.S3CommandPort;
 import konkuk.thip.feed.domain.Feed;
 import konkuk.thip.feed.domain.value.Tag;
 import konkuk.thip.feed.domain.value.TagList;
@@ -12,17 +11,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class FeedUpdateService implements FeedUpdateUseCase {
 
-    private final S3CommandPort s3CommandPort;
     private final FeedCommandPort feedCommandPort;
 
     @Override
     @Transactional
+    //TODO 추후 이벤트 기반으로 트랜잭션 커밋후 S3 삭제하도록 리펙토링 or 사용하지 않는 이미지 배치 삭제방식 논의
     public Long updateFeed(FeedUpdateCommand command) {
 
         //1. 유효성 검증
@@ -52,16 +49,6 @@ public class FeedUpdateService implements FeedUpdateUseCase {
         }
         if (command.tagList() != null) {
             feed.updateTags(command.userId(), command.tagList());
-        }
-    }
-
-    //TODO 추후 이벤트 기반으로 트랜잭션 커밋후 S3 삭제하도록 리펙토링 or 사용하지 않는 이미지 배치 삭제방식 논의
-    private void handleFeedImageDelete(Feed feed, List<String> remainImageUrls) {
-        List<String> toDelete = feed.getContentList().stream()
-                .filter(url -> !remainImageUrls.contains(url))
-                .toList();
-        if (!toDelete.isEmpty()) {
-            s3CommandPort.deleteImages(toDelete);
         }
     }
 }
