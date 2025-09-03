1108eaca89c6ca87277e5b6e41212ef38a522935
janghyunjun <buzz03312@naver.com>
2025-09-03 15:47:08 +0900

    [feat] 푸쉬알림 여부 활성/비활성화 설정 dto 정의 (#289)

--- src/main/java/konkuk/thip/notification/adapter/in/web/request/FcmTokenEnableStateChangeRequest.java
+++ src/main/java/konkuk/thip/notification/adapter/in/web/request/FcmTokenEnableStateChangeRequest.java
@@ -1,13 +1,21 @@
 package konkuk.thip.notification.adapter.in.web.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import konkuk.thip.notification.application.port.in.dto.FcmEnableStateChangeCommand;
 
 @Schema(description = "푸시 알림 설정 변경 요청 DTO")
 public record FcmTokenEnableStateChangeRequest(
         @Schema(description = "푸시 알림 수신 여부", example = "true")
-        boolean isEnable,
+        boolean enable,
 
         @Schema(description = "디바이스 고유 ID", example = "device12345")
         String deviceId
 ) {
+        public FcmEnableStateChangeCommand toCommand(Long userId) {
+                return new FcmEnableStateChangeCommand(
+                        userId,
+                        this.enable,
+                        this.deviceId
+                );
+        }
 }
