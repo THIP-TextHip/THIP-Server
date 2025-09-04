c678717d36539ee3de233f69dde0fb4f00846c60
강희진 <kanghuijin12@gmail.com>
2025-09-05 01:20:24 +0900

    [feat] 회원 탈퇴 시 softDelete메서드 추가 (#176)

--- src/main/java/konkuk/thip/user/adapter/out/jpa/UserJpaEntity.java
+++ src/main/java/konkuk/thip/user/adapter/out/jpa/UserJpaEntity.java
@@ -3,12 +3,16 @@ package konkuk.thip.user.adapter.out.jpa;
 
 import jakarta.persistence.*;
 import konkuk.thip.common.entity.BaseJpaEntity;
+import konkuk.thip.common.exception.InvalidStateException;
 import konkuk.thip.user.domain.value.Alias;
 import konkuk.thip.user.domain.User;
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 
 import java.time.LocalDate;
+
+import static konkuk.thip.common.entity.StatusType.INACTIVE;
+import static konkuk.thip.common.exception.code.ErrorCode.POST_ALREADY_DELETED;
 
 @Entity
 @Table(name = "users")
@@ -33,6 +37,11 @@ public class UserJpaEntity extends BaseJpaEntity {
     @Column(name = "oauth2_id", length = 50, nullable = false)
     private String oauth2Id;
 
+    /**
+     * -- SETTER --
+     *  회원 탈퇴용
+     */
+    @Setter
     @Builder.Default
     private Integer followerCount = 0; // 팔로워 수
 
@@ -58,4 +67,13 @@ public class UserJpaEntity extends BaseJpaEntity {
         this.role = UserRole.from(user.getUserRole());
         this.followerCount = user.getFollowerCount();
     }
+
+    public void softDelete(User user) {
+        if(this.status.equals(INACTIVE)){
+            throw new InvalidStateException(POST_ALREADY_DELETED);
+        }
+        this.status = INACTIVE;
+        this.oauth2Id = user.getOauth2Id();
+    }
+
 }
