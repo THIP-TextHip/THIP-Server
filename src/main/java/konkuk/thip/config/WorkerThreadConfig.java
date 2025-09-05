24e001ad70a9680cb6a2f8b7e70b7ac55e5e7162
janghyunjun <buzz03312@naver.com>
2025-09-05 15:06:47 +0900

    [refactor] thread 설정 테스트 환경에서는 동기적으로 수정 (#289)

--- src/main/java/konkuk/thip/config/WorkerThreadConfig.java
+++ src/main/java/konkuk/thip/config/WorkerThreadConfig.java
@@ -4,6 +4,7 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,6 +13,7 @@ import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
+@Profile("!test")
 public class WorkerThreadConfig implements AsyncConfigurer {
 
     /**
