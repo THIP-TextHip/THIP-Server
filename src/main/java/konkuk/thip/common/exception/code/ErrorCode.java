267e1cc0811064afc1c212e767606047f29ed881
janghyunjun <buzz03312@naver.com>
2025-09-05 13:18:45 +0900

    [refactor] 에러코드 수정 (#289)

--- src/main/java/konkuk/thip/common/exception/code/ErrorCode.java
+++ src/main/java/konkuk/thip/common/exception/code/ErrorCode.java
@@ -215,8 +215,8 @@ public enum ErrorCode implements ResponseCode {
      * 200000 : Fcm error
      */
     FCM_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, 200000, "존재하지 않는 FCM TOKEN 입니다."),
-    FCM_TOKEN_ENABLED_STATE_ALREADY(HttpStatus.BAD_REQUEST, 200001, "요청한 상태로 이미 푸쉬알림 여부가 설정되어 있습니다."),
-    FIREBASE_SEND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 200003, "FCM 푸쉬알림 전송에 실패했습니다.")
+    FCM_TOKEN_ENABLED_STATE_ALREADY(HttpStatus.BAD_REQUEST, 200001, "요청한 상태로 이미 푸쉬 알림 여부가 설정되어 있습니다."),
+    FIREBASE_SEND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 200002, "FCM 푸쉬 알림 전송에 실패했습니다.")
 
     ;
 
