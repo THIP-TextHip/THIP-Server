ec18391608b4d15467ad5aa75b2c72fa8b67d922
seongjunnoh <joon1754@naver.com>
2025-09-12 11:51:09 +0900

    [feat] Notification 도메인 엔티티 내부에 정적 팩토리 메서드 추가 (#296)

--- src/main/java/konkuk/thip/notification/domain/Notification.java
+++ src/main/java/konkuk/thip/notification/domain/Notification.java
@@ -17,4 +17,13 @@ public class Notification extends BaseDomainEntity {
     private boolean isChecked;
 
     private Long targetUserId;
+
+    public static Notification withoutId (String title, String content, Long targetUserId) {
+        return Notification.builder()
+                .title(title)
+                .content(content)
+                .isChecked(false)
+                .targetUserId(targetUserId)
+                .build();
+    }
 }
