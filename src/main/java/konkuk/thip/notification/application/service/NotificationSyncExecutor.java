4c68d4c612fbbe558c12e281536a7940402b6875
seongjunnoh <joon1754@naver.com>
2025-09-14 01:49:30 +0900

    [refactor] 푸시 알림 이벤트 퍼블리시 중 예외가 발생하더라도 이를 외부로 던지지 않도록 try-catch 문 도입 (#296)

--- src/main/java/konkuk/thip/notification/application/service/NotificationSyncExecutor.java
+++ src/main/java/konkuk/thip/notification/application/service/NotificationSyncExecutor.java
@@ -5,14 +5,16 @@ import konkuk.thip.notification.application.port.out.NotificationCommandPort;
 import konkuk.thip.notification.application.service.template.NotificationTemplate;
 import konkuk.thip.notification.domain.Notification;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @HelperService
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationSyncExecutor {
 
     private final NotificationCommandPort notificationCommandPort;
 
-    public  <T> void execute(
+    public <T> void execute(
             NotificationTemplate<T> template,
             T args,
             Long targetUserId,
@@ -25,7 +27,14 @@ public class NotificationSyncExecutor {
         saveNotification(title, content, targetUserId);
 
         // 2. 이벤트 퍼블리시
-        invoker.publish(title, content);
+        try {
+            invoker.publish(title, content);
+        } catch (Exception e) {
+            // 이벤트 발행 실패 시, DB에 저장된 알림을 롤백하지는 않음
+            // -> 알림 저장은 비즈니스 트랜잭션과 동일한 경계 내에서 수행되므로, 알림 저장은 유지
+            // -> 푸시 알림 이벤트 발행이 실패한 경우, 일단 로깅만 추가
+            log.error("푸시 알림 이벤트 퍼블리시 실패 targetUserId = {}, title = {}", targetUserId, title, e);
+        }
     }
 
     private void saveNotification(String title, String content, Long targetUserId) {
