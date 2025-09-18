9d428ff94c32a17611124bba03a045ad6eb9fb85
janghyunjun <buzz03312@naver.com>
2025-09-18 22:52:59 +0900

    [refactor] 허용 도메인 properties 설정 파일 선언 (#303)

---
+++ src/main/java/konkuk/thip/config/properties/WebDomainProperties.java
@@ -0,0 +1,29 @@
+package konkuk.thip.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "server")
+public class WebDomainProperties {
+
+    private List<String> webDomainUrls = new ArrayList<>();
+
+    public boolean isAllowed(String target) {
+        try {
+            URI uri = URI.create(target);
+            String origin = uri.getScheme() + "://" + uri.getHost() + (uri.getPort() > 0 ? ":" + uri.getPort() : "");
+            return webDomainUrls.stream().anyMatch(o -> o.equalsIgnoreCase(origin));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
