2e3b64e42f19bc01da5be6549d16a7b6aaf30727
janghyunjun <buzz03312@naver.com>
2025-09-18 22:52:48 +0900

    [refactor] redirect_url 쿼리 파라미터 리졸버 구현 (#303)

---
+++ src/main/java/konkuk/thip/common/security/resolver/CustomAuthorizationRequestResolver.java
@@ -0,0 +1,58 @@
+package konkuk.thip.common.security.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import konkuk.thip.config.properties.WebDomainProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static konkuk.thip.common.security.constant.AuthParameters.REDIRECT_SESSION_KEY;
+import static konkuk.thip.common.security.constant.AuthParameters.REDIRECT_URL_KEY;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
+
+    private final OAuth2AuthorizationRequestResolver delegate;
+    private final WebDomainProperties webDomainProperties;
+
+    public CustomAuthorizationRequestResolver(ClientRegistrationRepository repo,
+                                              String authorizationRequestBaseUri,
+                                              WebDomainProperties props) {
+        this.delegate = new DefaultOAuth2AuthorizationRequestResolver(repo, authorizationRequestBaseUri);
+        this.webDomainProperties = props;
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request) {
+        OAuth2AuthorizationRequest base = delegate.resolve(request);
+        return customize(request, base);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request, String clientRegistrationId) {
+        OAuth2AuthorizationRequest base = delegate.resolve(request, clientRegistrationId);
+        return customize(request, base);
+    }
+
+    private OAuth2AuthorizationRequest customize(HttpServletRequest request, OAuth2AuthorizationRequest base) {
+        if (base == null) return null;
+
+        String redirectUrl = request.getParameter(REDIRECT_URL_KEY.getValue());
+        Map<String, Object> additional = new HashMap<>(base.getAdditionalParameters());
+        if (StringUtils.hasText(redirectUrl) && webDomainProperties.isAllowed(redirectUrl)) {
+            request.getSession(true).setAttribute(REDIRECT_SESSION_KEY.getValue(), redirectUrl);
+        }
+
+        return OAuth2AuthorizationRequest.from(base)
+                .additionalParameters(additional)
+                .build();
+    }
+}
