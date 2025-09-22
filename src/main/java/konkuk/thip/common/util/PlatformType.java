91f312adab561133033150e4c67b46f7c0d7c233
seongjunnoh <joon1754@naver.com>
2025-09-22 17:42:26 +0900

    [move] PlatformType enum 에 정적 팩토리 메서드 추가 및 패키지 구조 이동 (#308)
    - notifications 패키지 -> common/util 패키지로 이동

---
+++ src/main/java/konkuk/thip/common/util/PlatformType.java
@@ -0,0 +1,25 @@
+package konkuk.thip.common.util;
+
+import konkuk.thip.common.exception.InvalidStateException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static konkuk.thip.common.exception.code.ErrorCode.INVALID_FE_PLATFORM;
+
+@Getter
+@RequiredArgsConstructor
+public enum PlatformType {
+    ANDROID("ANDROID"),
+    WEB("WEB");
+
+    private final String value;
+
+    public static PlatformType from(String value) {
+        for (PlatformType type : PlatformType.values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new InvalidStateException(INVALID_FE_PLATFORM);
+    }
+}
--- src/main/java/konkuk/thip/notification/domain/value/PlatformType.java
+++
@@ -1,5 +0,0 @@
-package konkuk.thip.notification.domain.value;
-
-public enum PlatformType {
-    ANDROID, WEB
-}
