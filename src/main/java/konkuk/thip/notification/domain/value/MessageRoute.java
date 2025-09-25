6c5a62a15ba565a45a3562199108fe5553607a64
seongjunnoh <joon1754@naver.com>
2025-09-25 11:21:12 +0900

    [refactor] RoomNotificationOrchestrator 구현체 코드 수정 (#314)
    - 모임방 게시글 상세화면에 해당하는 MessageRoute 통일
    - FE 요청으로 댓글 모달창 오픈 여부를 나타내는 "openComments" 키 값을 RedirectSpec에 추가

--- src/main/java/konkuk/thip/notification/domain/value/MessageRoute.java
+++ src/main/java/konkuk/thip/notification/domain/value/MessageRoute.java
@@ -16,7 +16,5 @@ public enum MessageRoute {
     // ROOM
     ROOM_MAIN,              // 특정 모임방 메인 화면으로 이동
     ROOM_DETAIL,            // 특정 모임 상세정보 화면으로 이동
-    ROOM_POST_DETAIL,       // 특정 모임 게시글 상세 화면으로 이동 -> PostType으로 투표인지 기록인지 판단
-    ROOM_RECORD_DETAIL,     // 특정 모임 기록 상세 화면으로 이동 (기록장 조회 - 페이지 필터 걸린채로)
-    ROOM_VOTE_DETAIL;       // 특정 모임 투표 상세 화면으로 이동 (투표 조회 - 페이지 필터 걸린채로)
+    ROOM_POST_DETAIL,       // 특정 모임 게시글 상세 화면으로 이동 -> PostType으로 투표(VOTE)인지 기록(RECORD)인지 판단
 }
