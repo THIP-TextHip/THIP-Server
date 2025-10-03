d3cb4097000f14a6000c97ddfe7baa673079163f
janghyunjun <buzz03312@naver.com>
2025-10-03 16:35:37 +0900

    [chore] 응답에서 requestId 반환하도록 수정 (#319)

--- src/main/java/konkuk/thip/common/dto/ErrorResponse.java
+++ src/main/java/konkuk/thip/common/dto/ErrorResponse.java
@@ -3,9 +3,12 @@ package konkuk.thip.common.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
+import org.slf4j.MDC;
+
+import static konkuk.thip.common.logging.LoggingConstant.REQUEST_ID;
 
 @Getter
-@JsonPropertyOrder({"success", "code", "message"})
+@JsonPropertyOrder({"success", "code", "message", "requestId"})
 public class ErrorResponse {
 
     @JsonProperty("isSuccess")
@@ -15,14 +18,17 @@ public class ErrorResponse {
 
     private final String message;
 
-    private ErrorResponse(boolean success, int code, String message) {
+    private final String requestId;
+
+    private ErrorResponse(boolean success, int code, String message, String requestId) {
         this.success = success;
         this.code = code;
         this.message = message;
+        this.requestId = requestId;
     }
 
     private ErrorResponse(ResponseCode response) {
-        this(response.isSuccess(), response.getCode(), response.getMessage());
+        this(response.isSuccess(), response.getCode(), response.getMessage(), MDC.get(REQUEST_ID.getValue()));
     }
 
     public static ErrorResponse of(ResponseCode response) {
@@ -32,6 +38,6 @@ public class ErrorResponse {
     public static ErrorResponse of(ResponseCode response, String message) {
         StringBuilder sb =  new StringBuilder();
         sb.append(response.getMessage()).append(" ").append(message);
-        return new ErrorResponse(response.isSuccess(), response.getCode(), sb.toString());
+        return new ErrorResponse(response.isSuccess(), response.getCode(), sb.toString(), MDC.get(REQUEST_ID.getValue()));
     }
 }
