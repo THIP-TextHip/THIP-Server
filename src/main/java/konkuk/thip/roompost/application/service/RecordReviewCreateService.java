f1809fe35bb55893b291ba5a00261ec2553cdd93
janghyunjun <buzz03312@naver.com>
2025-10-15 00:15:20 +0900

    [refactor] 유효성 검증 순서 변경 (#321)

--- src/main/java/konkuk/thip/roompost/application/service/RecordReviewCreateService.java
+++ src/main/java/konkuk/thip/roompost/application/service/RecordReviewCreateService.java
@@ -40,11 +40,11 @@ public class RecordReviewCreateService implements RecordReviewCreateUseCase {
         Book book = bookCommandPort.findBookByRoomId(roomId);
 
         // 2. 유저가 독후감 생성이 가능한 상태인지 유효성 검증 (기록 개수는 2개 이상, 독후감 생성 횟수는 5회 이하)
-        user.increaseRecordReviewCount();
         if(records.size() < 2) {
             throw new BusinessException(ErrorCode.RECORD_REVIEW_NOT_ENOUGH_RECORDS,
                     new IllegalArgumentException("현재 기록 개수: " + records.size()));
         }
+        user.increaseRecordReviewCount();
 
         // 3. 독후감 생성
         String reviewContent = geminiQueryPort.generateRecordReview(user, records, book, MIN_REVIEW_LENGTH, MAX_REVIEW_LENGTH);
