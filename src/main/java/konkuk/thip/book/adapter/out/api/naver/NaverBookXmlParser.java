7632374c9670a3b718060f3c126ae048cd0dfdba
강희진 <kanghuijin12@gmail.com>
2025-10-20 14:36:54 +0900

    [refactor] 상수 추출 (#327)

--- src/main/java/konkuk/thip/book/adapter/out/api/naver/NaverBookXmlParser.java
+++ src/main/java/konkuk/thip/book/adapter/out/api/naver/NaverBookXmlParser.java
@@ -17,6 +17,7 @@ import static konkuk.thip.common.exception.code.ErrorCode.BOOK_NAVER_API_PARSING
 
 public class NaverBookXmlParser {
     private static final int MAX_DESCRIPTION_LENGTH = 3000;
+    private static final String DESCRIPTION_SUFFIX = "...";
 
     public static NaverBookParseResult parseBookList(String xml) {
         List<NaverBookParseResult.NaverBook> naverBooks = new ArrayList<>();
@@ -71,9 +72,8 @@ public class NaverBookXmlParser {
                     String isbn = getTagValue(item, "isbn");
                     String rawDescription =  StringEscapeUtils.unescapeHtml4(getTagValue(item, "description"));
                     String description;
-                    String suffix = "...";
                     if (rawDescription.length() > MAX_DESCRIPTION_LENGTH) {
-                        description = rawDescription.substring(0, MAX_DESCRIPTION_LENGTH - suffix.length()) + suffix;
+                        description = rawDescription.substring(0, MAX_DESCRIPTION_LENGTH - DESCRIPTION_SUFFIX.length()) + DESCRIPTION_SUFFIX;
                     } else {
                         description = rawDescription;
                     }
