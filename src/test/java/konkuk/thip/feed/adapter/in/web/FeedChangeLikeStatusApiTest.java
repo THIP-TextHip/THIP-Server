7677566cd04594a116513455c68276899c1249fe
강희진 <kanghuijin12@gmail.com>
2025-11-12 17:18:49 +0900

    [chore] 주석 오타 수정 (#322)

--- src/test/java/konkuk/thip/feed/adapter/in/web/FeedChangeLikeStatusApiTest.java
+++ src/test/java/konkuk/thip/feed/adapter/in/web/FeedChangeLikeStatusApiTest.java
@@ -136,7 +136,7 @@ class FeedChangeLikeStatusApiTest {
     @DisplayName("좋아요 하지 않은 피드를 좋아요 취소하면 [400 에러 발생]")
     void unlikeFeed_NotLiked_Fail() throws Exception {
 
-        // 다른 유저가 해당 게시글에 좋아요한 상태(좋아요 0 이하 오버플로우 예외 피하기위해서 해당피드에 이미 좋아요 1이상이라고 가정)
+        // 다른 유저가 해당 게시글에 좋아요한 상태(좋아요 0 이하 언더플로우 예외 피하기위해서 해당피드에 이미 좋아요 1이상이라고 가정)
         feed.updateLikeCount(1);
         // given
         FeedIsLikeRequest request = new FeedIsLikeRequest(false);
