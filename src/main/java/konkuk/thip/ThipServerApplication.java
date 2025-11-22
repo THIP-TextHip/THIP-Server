08247ea69ab24ec798a8cb1236b896bc0a305bbb
강희진 <kanghuijin12@gmail.com>
2025-11-23 01:16:41 +0900

    [refactor] @ EnableRetry 설정 (#322)

--- src/main/java/konkuk/thip/ThipServerApplication.java
+++ src/main/java/konkuk/thip/ThipServerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
 @ConfigurationPropertiesScan
+@EnableRetry
 @SpringBootApplication
 public class ThipServerApplication {
 
