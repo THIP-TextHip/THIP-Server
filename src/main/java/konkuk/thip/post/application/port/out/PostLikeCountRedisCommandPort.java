94d579a6259ba7c8b06329c54b65b2e914a4823f
강희진 <kanghuijin12@gmail.com>
2025-12-10 18:25:05 +0900

    [rename] PostLikeCountRedis*** -> PostLikeRedis 이름 변경r (#339)

--- src/main/java/konkuk/thip/post/application/port/out/PostLikeCountRedisCommandPort.java
+++
@@ -1,9 +0,0 @@
-package konkuk.thip.post.application.port.out;
-
-import java.util.Set;
-import konkuk.thip.post.domain.PostType;
-
-public interface PostLikeCountRedisCommandPort {
-    void updateLikeCount(PostType postType, Long postId, Integer likeCount, boolean isLike);
-    void bulkResetLikeCounts(Set<String> keysToReset);
-}
--- src/main/java/konkuk/thip/post/application/port/out/PostLikeCountRedisQueryPort.java
+++
@@ -1,9 +0,0 @@
-package konkuk.thip.post.application.port.out;
-
-import java.util.Map;
-import konkuk.thip.post.domain.PostType;
-
-public interface PostLikeCountRedisQueryPort {
-    Integer getLikeCount(PostType postType, Long postId, Integer dbLikeCount);
-    Map<String, Integer> getAllLikeCounts();
-}
