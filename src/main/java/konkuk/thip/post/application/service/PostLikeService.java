e54c50e2b7c07d7b58774dc53a436ae5a37275df
강희진 <kanghuijin12@gmail.com>
2025-12-10 20:06:07 +0900

    [refactor] 게시글 좋아요 Redis List 비동기 큐잉 도입 (DB Write 부하 분산)으로 인한 로직 수정 (#339)

--- src/main/java/konkuk/thip/post/application/service/PostLikeService.java
+++ src/main/java/konkuk/thip/post/application/service/PostLikeService.java
@@ -2,16 +2,14 @@ package konkuk.thip.post.application.service;
 
 import konkuk.thip.notification.application.port.in.FeedNotificationOrchestrator;
 import konkuk.thip.notification.application.port.in.RoomNotificationOrchestrator;
-import konkuk.thip.post.application.port.out.PostLikeCountRedisCommandPort;
-import konkuk.thip.post.application.port.out.PostLikeCountRedisQueryPort;
+import konkuk.thip.post.application.port.out.PostLikeEventCommandPort;
+import konkuk.thip.post.application.port.out.PostLikeRedisQueryPort;
 import konkuk.thip.post.application.port.out.dto.PostQueryDto;
 import konkuk.thip.post.application.service.handler.PostHandler;
 import konkuk.thip.post.domain.CountUpdatable;
 import konkuk.thip.post.application.port.in.dto.PostIsLikeCommand;
 import konkuk.thip.post.application.port.in.dto.PostIsLikeResult;
 import konkuk.thip.post.application.port.in.PostLikeUseCase;
-import konkuk.thip.post.application.port.out.PostLikeCommandPort;
-import konkuk.thip.post.application.port.out.PostLikeQueryPort;
 import konkuk.thip.post.application.service.validator.PostLikeAuthorizationValidator;
 import konkuk.thip.post.domain.service.PostCountService;
 import konkuk.thip.user.application.port.out.UserCommandPort;
@@ -24,11 +22,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PostLikeService implements PostLikeUseCase {
 
-    private final PostLikeQueryPort postLikeQueryPort;
-    private final PostLikeCommandPort postLikeCommandPort;
     private final UserCommandPort userCommandPort;
-    private final PostLikeCountRedisCommandPort postLikeCountRedisCommandPort;
-    private final PostLikeCountRedisQueryPort postLikeCountRedisQueryPort;
+    private final PostLikeRedisQueryPort postLikeRedisQueryPort;
 
     private final PostHandler postHandler;
     private final PostCountService postCountService;
@@ -36,6 +31,7 @@ public class PostLikeService implements PostLikeUseCase {
 
     private final FeedNotificationOrchestrator feedNotificationOrchestrator;
     private final RoomNotificationOrchestrator roomNotificationOrchestrator;
+    private final PostLikeEventCommandPort postLikeEventCommandPort;
 
     @Override
     @Transactional
@@ -47,26 +43,24 @@ public class PostLikeService implements PostLikeUseCase {
         postLikeAuthorizationValidator.validateUserCanAccessPostLike(command.postType(), post, command.userId());
 
         // 2. 유저가 해당 게시물에 대해 좋아요 했는지 조회
-        boolean alreadyLiked = postLikeQueryPort.isLikedPostByUser(command.userId(), command.postId());
+        boolean alreadyLiked = postLikeRedisQueryPort.isLikedPostByUser(command.userId(), command.postId());
 
-        // 3. 좋아요 상태변경
+        // 3. 좋아요 가능 여부 검증
         if (command.isLike()) {
-            postLikeAuthorizationValidator.validateUserCanLike(alreadyLiked); // 좋아요 가능 여부 검증
-            postLikeCommandPort.save(command.userId(), command.postId(),command.postType());
-
+            postLikeAuthorizationValidator.validateUserCanLike(alreadyLiked);
             // 좋아요 푸쉬알림 전송
             //sendNotifications(command);
         } else {
-            postLikeAuthorizationValidator.validateUserCanUnLike(alreadyLiked); // 좋아요 취소 가능 여부 검증
-            postLikeCommandPort.delete(command.userId(), command.postId());
+            postLikeAuthorizationValidator.validateUserCanUnLike(alreadyLiked);
         }
 
-        // 4. 게시물 좋아요 수
-        int redisLikeCount = postLikeCountRedisQueryPort.getLikeCount(command.postType(),post.getId(), post.getLikeCount());
-        post.updateLikeCount(postCountService,command.isLike(), redisLikeCount); // 도메인 상태 갱신 (외부에서 읽은 최신값 주입)
-        // 4-1. 좋아요 수를 Redis INCR/DECR 이용해 원자적으로 갱신
-        postLikeCountRedisCommandPort.updateLikeCount(command.postType(),post.getId(),post.getLikeCount(),command.isLike());
+        // 4. 도메인 상태 갱신 (게시물 좋아요 수)
+        int redisLikeCount = postLikeRedisQueryPort.getLikeCount(command.postType(), post.getId(), post.getLikeCount());
+        post.updateLikeCount(postCountService, command.isLike(), redisLikeCount); // 도메인 상태 갱신 (외부에서 읽은 최신값 주입)
 
+        // 5. 트랜잭션 후속 처리 (DB 커밋 성공 후 비동기 작업을 위한 이벤트 발행)
+        postLikeEventCommandPort.publishEvent(command.userId(), command.postId(), command.isLike(),
+                command.postType(), post.getLikeCount());
         return PostIsLikeResult.of(post.getId(), command.isLike());
     }
 
