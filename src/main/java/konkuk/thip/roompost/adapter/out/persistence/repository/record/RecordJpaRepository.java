95d14f61ee96b4ce07d5092b629abfee6aa1e1a0
강희진 <kanghuijin12@gmail.com>
2025-12-31 04:26:02 +0900

    [feat] record 조회시 락 타임아웃 옵션 추가 (#322)

--- src/main/java/konkuk/thip/roompost/adapter/out/persistence/repository/record/RecordJpaRepository.java
+++ src/main/java/konkuk/thip/roompost/adapter/out/persistence/repository/record/RecordJpaRepository.java
@@ -1,11 +1,13 @@
 package konkuk.thip.roompost.adapter.out.persistence.repository.record;
 
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import konkuk.thip.roompost.adapter.out.jpa.RecordJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -20,6 +22,7 @@ public interface RecordJpaRepository extends JpaRepository<RecordJpaEntity, Long
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT r FROM RecordJpaEntity r WHERE r.postId = :postId")
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
     Optional<RecordJpaEntity> findByPostIdForUpdate(@Param("postId") Long postId);
 
     @Query("SELECT r.postId FROM RecordJpaEntity r WHERE r.userJpaEntity.userId = :userId")
