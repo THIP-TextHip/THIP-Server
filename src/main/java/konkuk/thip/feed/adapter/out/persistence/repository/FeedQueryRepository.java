080c778111131b816df080c4539c896baa497e98
강희진 <kanghuijin12@gmail.com>
2026-02-24 22:32:01 +0900

    [refactor] 피드 캐싱 로직 추가관련 조회메서드 정의
    -findTopFeedIds
    -findFeedDetailById
    -findFeedDetailsByIds
    (#345)

--- src/main/java/konkuk/thip/feed/adapter/out/persistence/repository/FeedQueryRepository.java
+++ src/main/java/konkuk/thip/feed/adapter/out/persistence/repository/FeedQueryRepository.java
@@ -9,9 +9,9 @@ import java.util.Set;
 public interface FeedQueryRepository {
     Set<Long> findUserIdsByBookId(Long bookId);
 
-    List<FeedQueryDto> findFeedsByFollowingPriority(Long userId, Integer lastPriority, LocalDateTime lastCreatedAt, int size);
+    List<FeedQueryDto> findFeedsByFollowingPriority(Long userId, Integer lastPriority, Long lastPostId, int size);
 
-    List<FeedQueryDto> findLatestFeedsByCreatedAt(Long userId, LocalDateTime lastCreatedAt, int size);
+    List<FeedQueryDto> findLatestFeedsByFeedId(Long userId, Long lastPostId, int size);
 
     List<FeedQueryDto> findMyFeedsByCreatedAt(Long userId, LocalDateTime lastCreatedAt, int size);
 
@@ -24,4 +24,10 @@ public interface FeedQueryRepository {
     List<Long> findLatestPublicFeedCreatorsIn(Set<Long> userIds, int size);
 
     List<FeedQueryDto> findSavedFeedsByCreatedAt(Long userId, LocalDateTime lastCreatedAt, int size);
+
+    List<Long> findTopFeedIds(int size);
+
+    FeedQueryDto findFeedDetailById(Long feedId);
+
+    List<FeedQueryDto> findFeedDetailsByIds(List<Long> feedIds);
 }
